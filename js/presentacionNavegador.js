474d81f664f034f6b33348bf283757e6b0dbd693
Pedro Nicolas Perez <nicolas20824@iesmarenostrum.com>
2021-09-29 18:19:50 +0200

    cambiada alerta principal

--- js/presentacionNavegador.js
+++ js/presentacionNavegador.js
@@ -1,4 +1,4 @@
-alert("Hola Mundo");
+alert("Hola yo soy Pedro Nicolás Pérez");
 let nombre = prompt("Dime tu nombre, intruso: ");
 alert("Hola " + nombre + ", bienvenido al maravilloso mundo de Js.");
 document.getElementById("titulo").innerHTML = 'Hola ' + nombre;
