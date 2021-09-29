17e0a6797d32f6bf32cc7bc0b92598b58af519e3
Pedro Nicolas Perez <nicolas20824@iesmarenostrum.com>
2021-09-29 18:18:03 +0200

    anyadida funcionalidad js

---
+++ js/presentacionNavegador.js
@@ -0,0 +1,4 @@
+alert("Hola Mundo");
+let nombre = prompt("Dime tu nombre, intruso: ");
+alert("Hola " + nombre + ", bienvenido al maravilloso mundo de Js.");
+document.getElementById("titulo").innerHTML = 'Hola ' + nombre;
