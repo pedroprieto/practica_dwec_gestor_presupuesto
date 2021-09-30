73dd4742fa387792fb5801292e73d1c78c9ca638
LGilat <gilabert21505@iesmarenostrum.com>
2021-09-30 11:01:01 +0200

    nuevo Archivo html - nueva folder:js Archivos:presentacionNavegador.js presentacionNode.js

---
+++ js/presentacionNavegador.js
@@ -0,0 +1,3 @@
+let nombre="Luis";
+let apellidos="Gilabert Blanes"
+alert(`${nombre} ${apellidos}`);
---
+++ js/presentacionNode.js
@@ -0,0 +1,3 @@
+let nombre="Luis";
+let apellidos="Gilabert Blanes"
+console.log(`${nombre} ${apellidos}`);
