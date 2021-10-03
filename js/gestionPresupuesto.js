7d659fb0b5d6211e096e809a36b6b8f94de56a9c
Pedro Prieto <pedpral@gmail.com>
2021-10-04 01:08:04 +0200

    Esqueleto práctica Intro JS I

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,8 +1,19 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
+// TODO: Variable global
 
 
+function actualizarPresupuesto() {
+    // TODO
+}
 
+function mostrarPresupuesto() {
+    // TODO
+}
+
+function CrearGasto() {
+    // TODO
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
