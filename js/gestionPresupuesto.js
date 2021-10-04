dd8a4dfda3796c1ed2d9d7ea40e6478e98416cb7
Maria C. Mediavilla <mediavilla20800@iesmarenostrum.com>
2021-10-04 13:51:51 +0200

    actualizacion de gestionpresupuesto.js y actualizacion de los paquetes test.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,29 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
+var presupuesto = 0;
 // TODO: Variable global
 
 
-function actualizarPresupuesto() {
+function actualizarPresupuesto(int) {
     // TODO
+   /* Función de 1 parámetro que se encargará
+    de actualizar la variable global presupuesto
+    . Esta función comprobará que el valor introducido es un número
+     no negativo: en caso de que sea un dato válido, actualizará la 
+     variable presupuesto y devolverá el valor del mismo; en caso contrario,
+      mostrará un error por pantalla y devolverá el valor -1. */
 }
 
 function mostrarPresupuesto() {
     // TODO
+    /*Función sin parámetros que se encargará de devolver el texto siguiente:
+     Tu presupuesto actual es de X €, siendo X el valor de la variable global presupuesto.*/
 }
 
 function CrearGasto() {
     // TODO
+    /*Función constructora que se encargará de crear un objeto gasto. Esta función devolverá un objeto de tipo gasto.
+     Deberá comprobar que el valor introducido sea un núḿero no negativo; en caso contrario, asignará a la propiedad valor el valor 0.
+     */
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
