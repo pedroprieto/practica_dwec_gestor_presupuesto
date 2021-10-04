562a3e75bc30b2e7cb3c865f573d569c86fb6302
LGilat <gilabert21505@iesmarenostrum.com>
2021-10-04 12:43:06 +0200

    modificado fichero js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -51,8 +51,8 @@ function CrearGasto(descripcion, valor) {
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-// export   {
-//     mostrarPresupuesto,
-//     actualizarPresupuesto,
-//     CrearGasto
-// }
+export   {
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto
+}
