3990d0a9295be00ee56e7d7f8c75088760d31775
LGilat <gilabert21505@iesmarenostrum.com>
2021-10-05 11:10:54 +0200

    descomentar export (gestionpresupuesto.js)

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,11 +2,11 @@
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-// export   {
-//     mostrarPresupuesto,
-//     actualizarPresupuesto,
-//     CrearGasto
-// }
+export   {
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto
+}
 
 
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
@@ -62,15 +62,6 @@ function CrearGasto(descripcion, value) {
     this.descripcion=descripcion;
 
 
-
-    // Object.defineProperty(this,"valor",{
-    //     set(value){
-    //         this.valor=value;
-    //     }
-    //     get(){
-    //         return this.valor;
-    //     }
-    // });
 
 
 
