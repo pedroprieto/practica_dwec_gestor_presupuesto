f16d368a807398914ae6779dce8c8591963732a6
LGilat <gilabert21505@iesmarenostrum.com>
2021-10-05 15:27:35 +0200

    modificado 4 ficheros package-lock.json,package.json,testIntroJSI,gestionpresupuesto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,13 +1,4 @@
 
-// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
-// Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto
-}
-
 
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
@@ -27,7 +18,7 @@ function actualizarPresupuesto(valor) {
 
 function mostrarPresupuesto() {
     // TODO
-    return `tu presupuesto actual es de ${presupuesto} €`;
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
 function CrearGasto(descripcion, value) {
@@ -61,6 +52,14 @@ function CrearGasto(descripcion, value) {
     this.valor=actualizar(value);
     this.descripcion=descripcion;
 
-
-
 }
+
+
+// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
+// Las funciones y objetos deben tener los nombres que se indican en el enunciado
+// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+module.exports =  {
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto
+};
