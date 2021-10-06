03f920d620ffa346e0eb21f838e1ecfc29fdaace
Arantzazu Gomez <gomez20742@iesmarenostrum.com>
2021-10-06 11:05:27 +0200

    Objeto gasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,29 +4,38 @@
 let presupuesto = 0;
 
 function actualizarPresupuesto(cantidad) {
-    if(cantidad>=0){
+    if (cantidad >= 0) {
         presupuesto += cantidad;
-    }else{
-        cantidad =-1;
+    } else {
+        cantidad = -1;
         console.log("Error al introducir el valor")
     }
     return cantidad;
 }
 
 function mostrarPresupuesto() {
-    
-    let x= presupuesto;
+
+    let x = presupuesto;
     return `Tu presupuesto actual es de ${x} €`
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    if (valor < 0) {
+        valor = 0;
+    }
+    this.descripcion = descripcion;
+    this.valor = valor;
+    this.mostrarGasto = function () {
+        console.log(`Gasto correspondiente a ${descripcion} con valor ${valor}`);
+    }
+
+
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
