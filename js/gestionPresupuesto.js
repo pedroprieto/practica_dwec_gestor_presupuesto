12c89e089d7daa696d8d3938e814fde12e3d346a
Pau Garcia Gomez <garcia20139@iesmarenostrum.com>
2021-10-08 19:52:15 +0200

    primer intento de escritura de gestionPresupuesto.js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,70 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
+function actualizarPresupuesto(cantidad) {
+    let numError= -1;
 
-function actualizarPresupuesto() {
-    // TODO
+    if(cantidad >= 0) {
+        presupuesto = cantidad;
+        return (cantidad);
+    }else{
+        console.log("Cantidad incorrecta.");
+        return (numError);
+    }
 }
 
-function mostrarPresupuesto() {
+function mostrarPresupuesto(pres) {
     // TODO
+    let mensaje; 
+
+    let result = actualizarPresupuesto(pres);
+
+    mensaje ="Tu presupuesto actual es de " + result + " €" ;
+    console.log(mensaje);
 }
 
-function CrearGasto() {
+function CrearGasto(descrip,cantid) {
     // TODO
+    gasto.descripcion= descrip;
+    if(cantid >= 0)
+    {
+        gasto.valor = cantid;
+    }else
+        gasto.valor = 0;
+    
+    return gasto;
+
 }
+
+
+
+//Objeto gasto y sus métodos
+let gasto = {
+    descripcion: "",
+    valor: 0
+};
+
+function mostrarGasto(){
+    console.log("Gasto correspondiente a " + gasto.descripcion + "con valor" + gasto.valor + " €");
+}
+
+function actualizarDescripcion (descr) {
+    gasto.descripcion = descr;
+}
+
+function actualizarValor (val) {
+    if(val >= 0)
+        gasto.valor = val;
+    else
+        return;
+}
+
+gasto.mostrarGasto = mostrarGasto;
+gasto.actualizarDescripcion = actualizarDescripcion;
+gasto.actualizarValor = actualizarValor;
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
