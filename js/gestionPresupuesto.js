78241f16e8291959cecaf68d9d4199a1168f2515
Pedro Nicolas Perez <nicolas20824@iesmarenostrum.com>
2021-10-10 13:59:08 +0200

    Anyadidas funciones de mostrarpresupuesto y compruebacantidad

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -15,6 +15,20 @@ function actualizarPresupuesto(cantidad) {
     }
 }
 
+function mostrarPresupuesto() {
+    return `Tu presupuesto actual es de ${presupuesto} €`;
+}
+
+function CompruebaCantidad(cantidad){
+    if (cantidad < 0 || isNaN(cantidad)){
+        return -1;
+    }
+    else{
+        return cantidad;
+    }
+}
+
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
