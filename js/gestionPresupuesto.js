4497a91fa37cea5440e29da747d0010c511aff97
Pedro Nicolas Perez <nicolas20824@iesmarenostrum.com>
2021-10-10 13:59:38 +0200

    anyadida funcion creargasto con objeto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -27,7 +27,29 @@ function CompruebaCantidad(cantidad){
         return cantidad;
     }
 }
+function CrearGasto(descripcion, valorGasto) {
+    
+    
+    let gasto = {
+        descripcion: "",
+        valor : 0,
+        mostrarGasto: function(){
+            return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+        },
+        actualizarDescripcion: function(nuevaDescripcion){
+            this.descripcion = nuevaDescripcion;
+        },
+        actualizarValor: function(nuevoValor){
+            if(CompruebaCantidad(nuevoValor) > -1){
+                this.valor = nuevoValor;
+            }
+        }
+    }
 
+    gasto.actualizarDescripcion(descripcion);
+    gasto.actualizarValor(valorGasto);
+    return gasto;
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
