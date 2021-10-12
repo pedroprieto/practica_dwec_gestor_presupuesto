0bf5e62ea84a84aaf2b68ef1b1ee99a1dd47f014
Alejandro Martinez <martinez20793@iesmarenostrum.com>
2021-10-12 11:00:22 +0200

    variables globales y funciones

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,34 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(valorActual) {
     // TODO
+    
+    if (valorActual >= 0){
+        presupuesto =  valorActual;
+        console.log(`Valor de la variable presupuesto es de ${presupuesto} €`);
+        return presupuesto;
+    } else{
+        console.log("Valor no valido");
+        return -1;
+    }
+
 }
 
 function mostrarPresupuesto() {
     // TODO
+    let x = presupuesto;
+    return `Tu presupuesto actual es de ${x} €`;
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion, valor) {
     // TODO
+    
+     
 }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
