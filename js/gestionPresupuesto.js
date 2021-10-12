5ed38538a59f4ebbce63f30a15b4e821d79a28c7
Arantzazu Gomez <gomez20742@iesmarenostrum.com>
2021-10-12 15:58:04 +0200

    creacion variables globales, funciones vacias y export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,7 +1,6 @@
-
-
-
 let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
 function actualizarPresupuesto(cantidad) {
     if (cantidad >= 0) {
@@ -20,35 +19,58 @@ function mostrarPresupuesto() {
 }
 
 function CrearGasto(descripcion, valor) {
-    
+
     this.descripcion = descripcion;
-    if(valor>=0) {
+    if (valor >= 0) {
         this.valor = valor;
-    }else{
+    } else {
         this.valor = 0;
     }
-    
+
 
     this.mostrarGasto = function () {
         return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
     }
-    this.actualizarDescripcion = function(nuevaDescripcion) {
-        this.descripcion=nuevaDescripcion;
+    this.actualizarDescripcion = function (nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
     }
-    this.actualizarValor = function(nuevoValor){
-        if(nuevoValor>=0){
-            this.valor=nuevoValor;
+    this.actualizarValor = function (nuevoValor) {
+        if (nuevoValor >= 0) {
+            this.valor = nuevoValor;
         }
 
     }
-  
+
 }
 
+function listarGastos(){
+
+}
+
+function anyadirGasto(){
+
+}
+
+function borrarGasto(){
+
+}
+
+function calcularTotalGastos(){
+
+}
+function calcularBalance(){
+    
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
