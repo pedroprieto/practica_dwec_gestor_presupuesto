d3506d2dbedd7bad0f0cb2ff5e4983680af4f9b8
HiginioUrbano <urbano20894@iesmarenostrum.com>
2021-10-12 10:57:18 +0200

    Añadidas funciones vacías y metodos export.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 
 //Variable global
 let presupuesto = 0;
+let gasto = [];
+let idGasto= 0;
 
 function actualizarPresupuesto(x) {
 
@@ -51,6 +53,22 @@ function CrearGasto(descripcion, valor) {
         }
     }
 }
+function listarGastos() {
+        
+}
+
+function anyadirGasto() {
+
+}
+function borrarGasto() {
+
+}
+function calcularTotalGastos() {
+
+}
+function calcularBalance() {
+    
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -58,5 +76,10 @@ function CrearGasto(descripcion, valor) {
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
