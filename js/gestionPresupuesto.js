99da0fa0feafd4042f23fe16185116193dd1a7d0
Javier Marco <marco12853@iesmarenostrum.com>
2021-10-16 20:48:52 +0200

    2 de 10. Función listarGastos y variable global. Función añadirGasto. Preparación borrarGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,12 +1,15 @@
 "use strict";
-// No se si quieres que usemos la forma "moderna". En el manual habla de omitirlo cuando utilicemos todo clases y módulos, pero por ahora lo dejo si no me dices lo contrario. 
 
-// Variable global
+//Variables globales
 let presupuesto = 0;
 
-/* Función de 1 parámetro que se encargará de actualizar la variable global presupuesto.
-Esta función comprobará que el valor introducido es un número no negativo: en caso de que sea un dato válido, 
-actualizará la variable presupuesto y devolverá el valor del mismo; en caso contrario, mostrará un error por pantalla y devolverá el valor -1*/
+//Almacenará el listado de gastos que vaya introduciendo el usuario. Inicialmente contendrá un array vacío.
+let gastos = [];
+
+//Se utilizará para almacenar el identificador actual de cada gasto que se vaya añadiendo. Su valor inicial será 0. Se irá incrementando con cada gasto que se añada.
+let idGasto = 0;
+
+
 function actualizarPresupuesto(presupuestoActualizado) { 
 
     let presupuestoAuxiliar = presupuestoActualizado;
@@ -20,44 +23,87 @@ function actualizarPresupuesto(presupuestoActualizado) {
     return presupuestoAuxiliar;               
 }
 
-// Función sin parámetros que se encargará de devolver el texto siguiente: Tu presupuesto actual es de X €, siendo X el valor de la variable global presupuesto.
+
 function mostrarPresupuesto() {
     let x = presupuesto;
     return `Tu presupuesto actual es de ${x} €`;
 }
 
-/* Función constructora que se encargará de crear un objeto gasto. Esta función devolverá un objeto de tipo gasto. 
-Deberá comprobar que el valor introducido sea un número no negativo; en caso contrario, asignará a la propiedad valor el valor 0.*/
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, fecha, etiquetas) {
     this.descripcion = descripcion;
-    // Almacenará la descripción del gasto en formato cadena.
 
     if (valor >= 0) {
         this.valor = valor;
     } else {
         this.valor = 0;
     }
-    // Almacenará el valor del gasto en formato númerico.
 
-    // mostrarGasto - Función sin parámetros que muestre el texto: 
-    // Gasto correspondiente a DESCRIPCION con valor VALOR €, siendo VALOR y DESCRIPCION las propiedades del objeto correspondientes.
+    //Falta seguir las normas de la práctica
+    this.fecha = fecha;
+
+    //Falta seguir las normas de la práctica
+    this.etiquetas = etiquetas;
+
     this.mostrarGasto = function() {
         let gasto = (`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
         return gasto;
     }
 
-    // actualizarDescripcion - Función de 1 parámetro que actualizará la descripción del objeto.
     this.actualizarDescripcion = function (descripcionActualizada) {
         this.descripcion = descripcionActualizada;
     }
 
-    // actualizarValor - Función de 1 parámetro que actualizará el valor del objeto. 
-    // Se encargará de comprobar que el valor introducido sea un número no negativo; en caso contrario, dejará el valor como estaba.
     this.actualizarValor = function (valorActualizado) {
         if (valorActualizado > 0) {
             this.valor = valorActualizado;
         }
     }
+}
+
+//Función sin parametros que devolverá la variable global gastos.
+function listarGastos()
+{
+    return gastos;
+}
+
+//Función de 1 parámetro que realizará tres tareas:
+function anyadirGasto(gastoAnyadido)
+{
+    //Añadir al objeto gasto pasado como parámetro una propiedad id cuyo valor será el valor actual de la variable global idGasto.
+    gastoAnyadido.id = idGasto;
+
+    //Incrementar el valor de la variable global idGasto.
+    idGasto++;
+
+    //Añadir el objeto gasto pasado como parámetro a la variable global gastos. El gasto se debe añadir al final del array.
+    gastos.push(gastoAnyadido);
+}
+
+//Función de 1 parámetro que eliminará de la variable global gastos el objeto gasto cuyo id haya sido pasado como parámetro. Si no existe un gasto con el id proporcionado
+//no hará nada. 
+function borrarGasto(gastoBorrado)
+{
+        /* TODO. 
+
+    Seguir por aqui
+    
+    Para buscarlo algo similar del manual: let result = arr.find(function(item, index, array)
+
+    Para borrarlo algo similar del manual: arr.splice(start[, deleteCount, elem1, ..., elemN])
+
+    https://es.javascript.info/array-methods#splice
+     */
+}
+
+
+function calcularTotalGastos()
+{
+    // TODO
+}
+
+function calcularBalance()
+{
+    // TODO
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
@@ -66,5 +112,10 @@ function CrearGasto(descripcion, valor) {
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
