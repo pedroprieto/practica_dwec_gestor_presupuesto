742d24f8df3ab35299cdbc9a23c813b327bf9449
Pedro Nicolas Perez <nicolas20824@iesmarenostrum.com>
2021-10-17 13:52:01 +0200

    anyadidas funciones de listar gastos y anyadir gastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -88,6 +88,16 @@ function CrearGasto(descripcion, valorGasto, fecha, ...etiquetas) {
     }
 }
 
+function listarGastos(){
+    console.log(String(gastos));
+    return String(gastos);
+}
+
+function anyadirGasto(gasto){
+    gasto.id = idGasto;
+    idGasto++;
+    gastos.push(gasto);
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
