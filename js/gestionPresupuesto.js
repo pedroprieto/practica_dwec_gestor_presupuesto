2402e8930f133b5ccc945669e6adb25d51250206
Javier Marco <marco12853@iesmarenostrum.com>
2021-10-18 19:44:15 +0200

    7 de 10. Crear gasto con fecha y etiquetas. Metodo mostrarGastoCompleto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -29,7 +29,9 @@ function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${x} €`;
 }
 
-function CrearGasto(descripcion, valor, fecha, etiquetas) {
+//Actualiza la función constructora para que incluya la fecha y las etiquetas (ver apartado de Objeto gasto). Los parámetros adicionales de la función deben ir a continuación de los existentes.
+//Sección del manual Parámetros REST y operador SPREAD ... para expandir etiquetas.
+function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
     this.descripcion = descripcion;
 
     if (valor >= 0) {
@@ -38,11 +40,63 @@ function CrearGasto(descripcion, valor, fecha, etiquetas) {
         this.valor = 0;
     }
 
-    //Falta seguir las normas de la práctica
-    this.fecha = fecha;
+    //fecha - Almacenará la fecha en que se crea el gasto en forma de timestamp
+    if (fecha)
+    {
+        this.fecha = Date.parse(fecha);  
+        //El parámetro fecha deberá ser un string con formato válido que pueda entender la función Date.parse. Si la fecha no es válida (no sigue el formato indicado), 
+        //se deberá almacenar la fecha actual en su lugar.
+    }   
+    else
+    {
+        this.fecha = Date.now(fecha);  
+        //Si no se indica el parámetro fecha, se almacenará en la propiedad fecha la fecha actual.  
+    }
+    
+    //etiquetas - Almacenará en un array el listado de etiquetas (categorías) asociadas al gasto.
+    if (etiquetas === null)
+    {
+        this.etiquetas = [];
+        //Si no se indican los parámetros de etiquetas, se almacenará en la propiedad etiquetas un array vacío.
+    }
+    else
+    {
+        this.etiquetas = etiquetas;
+    }
+    
+    //Añade o modifica los siguientes métodos del objeto gasto:
+    //mostrarGastoCompleto - Función sin parámetros que devuelva el texto multilínea siguiente (ejemplo para un gasto con tres etiquetas)
+    //Para mostrar la fecha en formato localizado puedes utilizar el método toLocaleString() (referencia de toLocaleString()).
+    this.mostrarGastoCompleto = function()
+    {
+        let fechaFormatoLocalizado = new Date(fecha);
+        //Si utilizasemos solo fecha.toLocaleString() sin hacer new Date(fecha) nos mostrará la fecha pero no con el formato adecuado y no pasa la prueba. 
 
-    //Falta seguir las normas de la práctica
-    this.etiquetas = etiquetas;
+        let gasto = (`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.\nFecha: ${fechaFormatoLocalizado.toLocaleString()}\nEtiquetas:\n- ${etiquetas.join ("\n- ")}\n`);
+        //Podemos utilizar toLocaleString en diferentes localizaciones con los parentesis: event.toLocaleString('en-GB') y también cambiará el formato de las horas entre 12h y 24h.
+        //arr.join(glue). (contrario a split) Creamos un string de arr elementos unidos con glue (pegamento) entre ellos. Luego es ir ajustando para pasar la prueba, ya sea \n o todos seguidos ":"
+        //Como el ejemplo del manual
+
+        return gasto;
+    }
+
+    //actualizarFecha - Función de 1 parámetro que actualizará la propiedad fecha del objeto. 
+    //Deberá recibir la fecha en formato string que sea entendible por la función Date.parse. Si la fecha no es válida, se dejará sin modificar.
+    this.actualizarFecha = function()
+    {
+        // TODO
+    }
+
+    this.anyadirEtiquetas = function()
+    {
+        // TODO
+    }
+
+    this.borrarEtiquetas = function()
+    {
+        // TODO
+    }
+
 
     this.mostrarGasto = function() {
         let gasto = (`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
