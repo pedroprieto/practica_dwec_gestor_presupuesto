3e591f4aa2d0f30d7437419ee5c25287fcd3561c
Rafael Gandolfo Garcia <rafagandolfogarcia@gmail.com>
2021-10-19 11:51:10 +0200

    cambios realizados. Pasa los test

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -5,23 +5,50 @@ let presupuesto = 0;
 let gastos= [];
 let idGasto = 0;
 
+function mostrarPresupuesto() {
+    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`
+}
 function actualizarPresupuesto(numero) {
     if(numero > 0){
-        presupuesto = numero;
-        return numero;
+
+    
+    return presupuesto = numero;
     }
     else{
-        return -1;
+        return -1
+    }
+}
+function anyadirGasto(gasto){
 
+    gasto.id = idGasto;
+    idGasto = idGasto +1;
+    gastos.push(gasto);
+}
+function borrarGasto(id) {
+    let pos = gastos.findIndex(gasto => gasto.id === id);
+    if (pos != -1) {
+        gastos.splice(pos, 1);
     }
 }
 
-function mostrarPresupuesto() {
-    return `Tu presupuesto actual es de ${presupuesto} €`;
+function calcularTotalGastos (){
+    let suma = 0;
+    for (let gasto of gastos) {
+        suma += gasto.valor;
+    }
+    return suma
+}
+function calcularBalance (){
+    let gastosTotales = calcularTotalGastos();
+    let balance = presupuesto - gastosTotales;
+    return balance
+}
+function listarGastos(){
+    return gastos
 }
 
-
-function CrearGasto(descripcion,valor) {
+function CrearGasto(descripcion,valor,fecha,...etiquetas) {
 
     this.descripcion = descripcion;
     
@@ -47,18 +74,61 @@ function CrearGasto(descripcion,valor) {
     else {
         this.valor = 0;
     }
+    this.etiquetas = [];
+    if(fecha){
+        fecha = Date.parse(fecha)
+        this.fecha = fecha;
+    }
+    else{
+        fecha = Date.now();
+        this.fecha = fecha;
+    }
     
-}
-    
-    
-function listarGastos(){
-    return gastos
-}
-function calcularTotalGastos (){}
-function calcularBalance(){}
-function anyadirGasto(){}
-function borrarGasto(){}
 
+
+    this.actualizarFecha = function(fecha){
+    
+        fecha = Date.parse(fecha);
+        if (fecha){
+            this.fecha = fecha;
+        }
+    }
+    
+    
+    this.anyadirEtiquetas = function(...etiquetas){
+        let posicion;
+        for (let etiqueta of etiquetas){
+            posicion = this.etiquetas.lastIndexOf(etiqueta);
+            if(posicion == -1){
+                this.etiquetas.push(etiqueta)
+            }
+        }
+    } 
+    this.anyadirEtiquetas(...etiquetas) 
+    
+    this.borrarEtiquetas = function(...etiquetas){
+        let posicion;
+        for (let etiqueta of etiquetas){
+            posicion = this.etiquetas.indexOf(etiqueta);
+            if (posicion !=-1){
+                this.etiquetas.splice(posicion,1);
+            }
+        }
+        
+    }
+    this.mostrarGastoCompleto = function(){
+
+        let fecha = new Date(this.fecha);
+        let fechaTexto = fecha.toLocaleString();
+    
+        let etiqueta = "";
+        for (let i = 0; i < this.etiquetas.length; i++) {
+            etiqueta += `- ${this.etiquetas[i]}\n`;
+        }
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.\nFecha: ${fechaTexto}\nEtiquetas:\n${etiqueta}`;
+    
+    }
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
