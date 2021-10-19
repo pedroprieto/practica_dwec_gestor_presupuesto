8b12c954c51f0625662bb541dcb80599e8c9331c
Asencio Ortiz <ortiz19920@iesmarenostrum.com>
2021-10-19 11:41:26 +0200

    hago más cambios

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,12 +1,16 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+
+
 let presupuesto = 0;
+
+
 
 function actualizarPresupuesto(budget) {
     if(budget >=  0){
         presupuesto = budget;
-        console.log(presupuesto);
+        return budget;
     }
     else{
         console.log("Error");
@@ -16,13 +20,14 @@ function actualizarPresupuesto(budget) {
 
 function mostrarPresupuesto() {
     // TODO
-    return `Tu presupuesto actual es de ${presupuesto}€`;
+    return `Tu presupuesto actual es de ${presupuesto} €`;
     
 }
 
 function CrearGasto(descripcion,valor) {
     // TODO
-    let gasto={};
+    
+    
     this.descripcion=descripcion;
     this.valor = valor;
     if(valor < 0){
@@ -30,12 +35,12 @@ function CrearGasto(descripcion,valor) {
     }
     else{
         this.valor = valor;
+        
     }
-    return gasto;
+    
 
     this.mostrarGasto = function(){
-        return `Gasto correspondiente a ${this.descripcion} 
-        con valor ${this.valor}€`
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`
     }
 
     this.actualizarDescripcion = function(nuevaDescripcion){
@@ -43,17 +48,19 @@ function CrearGasto(descripcion,valor) {
         this.descripcion = nuevaDescripcion;
     }
 
+
     this.actualizarValor = function(nuevoValor){
 
         if(nuevoValor < 0 ){
             this.valor = valor;
-            
+
         }else{
             this.valor = nuevoValor;
         }
         
     }
 
+    
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
