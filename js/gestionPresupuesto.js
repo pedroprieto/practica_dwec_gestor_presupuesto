def7e02a606bca469f7e1150944ca6bcde75c7ab
llinares19462 <llinares19462@iesmarenostrum.com>
2021-10-19 11:56:32 +0200

    Modificados metodos practica 1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -5,10 +5,10 @@ let presupuesto = 0;
 
 function actualizarPresupuesto(cantidad) {
 
-    let x = presupuesto;
+    // let x = presupuesto;
 
-    if (cantidad > -1){
-        presupuesto = presupuesto + cantidad;
+    if (cantidad >= 0){
+        presupuesto = cantidad;
         return `Tu presupuesto actual es de ${presupuesto} €`
     } else{
         return -1;
@@ -17,22 +17,28 @@ function actualizarPresupuesto(cantidad) {
 
 function mostrarPresupuesto() {
     
-    let x = presupuesto;
-    return `Tu presupuesto actual es de ${x} €`
+    // let x = presupuesto;
+    return `Tu presupuesto actual es de ${presupuesto} €`
 }
 
 function CrearGasto(concepto, cantidad) {
     
-   
-        this.descripcion = concepto,
-        this.valor = cantidad
-   
-
-    gasto.mostrarGasto = function() {
+    this.descripcion = concepto;
+    this.valor = cantidad;
+    this.mostrarGasto = function() {
         return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
     };
+    this.actualizarDescripcion = function (nueva_decripcion){
+        this.descripcion = nueva_decripcion;
+    };
+    this.actualizaValor = function (nuevo_valor){
+        if (nuevo_valor >= 0){
+            this.valor = nuevo_valor;
+        }
+        
+    };
 
-}
+};
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
