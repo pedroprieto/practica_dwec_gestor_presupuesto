a77d6c5eb08d852388b9a1c769b38fb99d81abd7
llinares19462 <llinares19462@iesmarenostrum.com>
2021-10-19 18:21:24 +0200

    Practica 2 JS - Creadas nuevas variables globales. Creada funcion listarGastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -51,11 +51,36 @@ function CrearGasto(descripcion, valor) {
 
 };
 
+function listarGastos(){
+    return gastos;
+};
+
+function anyadirGasto(){
+    
+};
+
+function borrarGasto(){
+    
+};
+
+function calcularTotalGastos(){
+    
+};
+
+function calcularBalance(){
+    
+};
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
