1a726e79eadce20c7540dc48c5b9f1ddd0f84346
Maria Jose Gomez Pastor <gomez20740@iesmarenostrum.com>
2021-10-19 11:47:45 +0200

    Objeto gasto y funcion crear gasto. Pasan 9/9 tests

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,13 +1,29 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
 // Variable global
 let presupuesto = 0;
+
+let gasto = {
+    descripcion: "",
+    valor: 0,
+
+    mostrarGasto() {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    },
+    actualizarDescripcion(descripcion) {
+        this.descripcion = descripcion;
+    },
+
+    actualizarValor(valor) {
+        if (valor >= 0) {
+            this.valor = valor;
+        }
+    },
+}
 
 function actualizarPresupuesto(nuevoValor) {
 
     if (nuevoValor >= 0) {
         presupuesto = nuevoValor;
-        
+
         return presupuesto;
     } else {
         return -1;
@@ -18,8 +34,16 @@ function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €` ;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    gasto.descripcion = descripcion;
+    
+    if (valor >= 0) {
+        gasto.valor = valor;
+    } else {
+        gasto.valor = 0;
+    }
+
+    return gasto;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
