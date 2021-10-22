82ef7445d6850b77040b9b299150df3f73b97c27
Sonia Soriano <soriano20887@iesmarenostrum.com>
2021-10-22 19:48:41 +0200

    Variable global, actualizar presupuesto y mostrarlo. Sonia

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,24 +1,41 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(nuevoPresupuesto) 
+{
     // TODO
+    if (nuevoPresupuesto >= 0)
+    {
+        presupuesto = nuevoPresupuesto;
+    }
+    else
+    {
+        Presupuesto = -1;
+        console.log("Error en el presupuesto");
+    }
+
+    return presupuesto;
 }
 
-function mostrarPresupuesto() {
+function mostrarPresupuesto() 
+{
     // TODO
+    let x = presupuesto;
+    return `Tu presupuesto actual es de ${x} €`;
 }
 
-function CrearGasto() {
+function CrearGasto() 
+{
     // TODO
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export   
+{
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
