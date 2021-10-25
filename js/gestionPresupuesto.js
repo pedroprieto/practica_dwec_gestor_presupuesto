4ac70ef6ad60517383507b59e9cebb286c9cc4d3
Leidygemade <leidygemade@hotmail.es>
2021-10-25 18:58:56 +0200

    añadir variables globales, modificación export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,7 +1,9 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
+// Variables globales
     let presupuesto = 0;
+    let gastos = [];
+    let idGasto = 0;
 
 function actualizarPresupuesto(valor) {
     if (valor >= 0) {
@@ -45,11 +47,36 @@ function CrearGasto(descripcion, valor) {
     }
 }
 
+function listarGastos() {
+
+}
+
+function anyadirGasto() {
+
+}
+
+function borrarGasto() {
+
+}
+
+function calcularTotalGastos() {
+
+}
+
+function calcularBalance() {
+    
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
