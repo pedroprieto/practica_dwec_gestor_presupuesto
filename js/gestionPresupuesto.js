99514f27b800b2467baddf9175e54097064fea78
Susana Fernández <fernandez20131@iesmarenostrum.com>
2021-10-28 20:11:41 +0200

    Fundamentos JS II - esqueleto inicial

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,5 +1,7 @@
 
 let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
 
 function actualizarPresupuesto(valor) {
@@ -35,11 +37,37 @@ function CrearGasto(descripcion, valor) {
     };
 }
 
+function listarGastos() {
+    //TODO
+}
+
+function anyadirGasto() {
+    //TODO
+}
+
+function borrarGasto() {
+    //TODO
+}
+
+
+function calcularTotalGastos() {
+    //TODO
+}
+
+function calcularBalance() {
+    //TODO
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos, 
+    anyadirGasto, 
+    borrarGasto, 
+    calcularTotalGastos,
+    calcularBalance
 }
