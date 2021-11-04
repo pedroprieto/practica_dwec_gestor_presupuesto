5c372996ea3485c7bb8c460f1466a302b3b31bc6
HiginioUrbano <urbano20894@iesmarenostrum.com>
2021-11-04 12:57:05 +0100

    mostrarDatosWeb

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,9 +1,10 @@
 
-  import * as gesPresWeb from './gestionPresupuestoWeb';
+  import * as gesPresWeb from "./gestionPresupuestoWeb.js";
 
   import * as gesPres from "./gestionPresupuesto.js";
 
   gesPres.actualizarPresupuesto(1500);
+  gesPresWeb.mostrarDatoEnId("presupuesto", gesPres.mostrarPresupuesto());
 
   let g1 = new gesPres.CrearGasto("Compra carne", 23.44, "2021-10-06", "supermercado", "comida");
   let g2 = new gesPres.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
@@ -19,5 +20,6 @@
   gesPres.anyadirGasto(g5);
   gesPres.anyadirGasto(g6);
 
-  gesPresWeb.mostrarDatoEnId("presupuesto", gesPres.mostrarPresupuesto());
-  gesPresWeb.mostrarGastoWeb();
+
+
+  gesPresWeb.mostrarGastoWeb("listado-gastos-completo", gesPres.listarGastos());
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -5,8 +5,18 @@ function mostrarDatoEnId(idElemento, valor){
     document.getElementById(idElemento).innerHTML = valor;
 
 }
-function mostrarGastoWeb(){
+function mostrarGastoWeb(idElemento, gastos){
+    for (let gasto of gastos)
+    {
+        let div = document.createElement('div');
 
+        div.className = "gasto";
+
+        div.append('div', "gasto-descripcion", gasto.descripcion); 
+
+    }
+    document.getElementById(idElemento) = div;
+    document.body.append(div);
 }
 function mostrarGastoAgrupadosWeb(){
 
