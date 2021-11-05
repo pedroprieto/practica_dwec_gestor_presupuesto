df2c05052348b99719134923cc2b3092aebab79a
ferrer21033 <ferrer21033@iesmarenostrum.com>
2021-11-05 11:06:49 +0100

    Practica 4 - 1º commit

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -27,10 +27,12 @@
 
 // }
 // TODO: Variable global
-let gasto = new CrearGasto();
+//let gasto = new CrearGasto();
+let gastos = new Array();
+let idGasto = 0;
 let  presupuesto = 0;
-let descripcion='';
-let valor=0;
+//let descripcion='';
+//let valor=0;
 function actualizarPresupuesto(parametro) {
     // TODO
     if (isNaN(parametro)==false && parametro>0)
@@ -49,7 +51,7 @@ function mostrarPresupuesto() {
 
     return ("Tu presupuesto actual es de "+presupuesto+" €");
 }
-function CrearGasto(descripcion,valor) 
+function CrearGasto(descripcion,valor,fecha,...etiquetas) 
 {
     // TODO
     //var gasto = new Object();
@@ -57,13 +59,76 @@ function CrearGasto(descripcion,valor)
         valor = 0;
     this.descripcion=descripcion;
     this.valor=valor;
+    if(fecha=='' || isNaN(Date.parse(fecha))) // si la fecha está vacía o no tiene un formato válido
+        this.fecha=Date.now(); //devuelve la fecha actual
+    else
+        this.fecha=fecha; //fecha actual (timestamp) en valor numerico
+
+    if(etiquetas=='')
+        this.etiquetas=[];
+    else {
+        this.etiquetas=[];
+        for (let categoria of etiquetas) this.etiquetas.push(categoria);
+    }
+        
     this.actualizarDescripcion=function(desc) { this.descripcion=desc};
     this.actualizarValor=function(valor) {if (isNaN(valor)==false && valor>0) { this.valor=valor}};
     this.mostrarGasto=function() {return ("Gasto correspondiente a "+this.descripcion+" con valor "+this.valor+" €")};
-    return;
-    
+    this.anyadirEtiquetas = function(...etiquetas) { for (let categoria of etiquetas) this.etiquetas.push(categoria)};
+
+    // console.log("hoa");
+    //return;   
 }
 
+function listarGastos()
+{
+    return gastos;
+}
+
+function anyadirGasto(gasto)
+{
+    gasto.id = idGasto;
+    idGasto++;
+    gastos.push(gasto);
+}
+function borrarGasto(idGasto){
+    for (let gasto of gastos) {
+        if(gasto.id == idGasto) {
+            gastos.splice(gastos.indexOf(gasto), 1);
+            break;
+        }
+    }
+}
+function calcularBalance(){
+    let totalGasto = 0;
+    for (let gasto of gastos) {
+        totalGasto += gasto.valor;
+    }
+    return presupuesto - totalGasto;
+}
+function calcularTotalGastos(){
+ return gastos.length;
+}
+
+let gasto1 = new CrearGasto("Gasto 1");
+let gasto2 = new CrearGasto("Gasto 2", 23.55);
+let gasto3 = new CrearGasto("Gasto 3", 23.55, "2021-10-06T13:10" );
+let gasto4 = new CrearGasto("Gasto 4", 23.55, "2021-10-06T13:10", "casa" );
+let gasto5 = new CrearGasto("Gasto 5", 23.55, "2021-10-06T13:10", "casa", "supermercado", "comida" );
+anyadirGasto(gasto1);
+anyadirGasto(gasto2);
+anyadirGasto(gasto3);
+anyadirGasto(gasto4);
+anyadirGasto(gasto5);
+
+
+// borrarGasto(2);
+let numero = calcularTotalGastos();
+
+actualizarPresupuesto(500);
+let balance = calcularBalance();
+
+alert("hola");
 //let gasto = CrearGasto(descripcion,valor);//constructor de objeto gasto
 //gasto.actualizarDescripcion("Nueva descripción de gasto 1");
 //gasto.actualizarValor(15);
@@ -75,5 +140,10 @@ function CrearGasto(descripcion,valor)
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance,
     CrearGasto
 }
