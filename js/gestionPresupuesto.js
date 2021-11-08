cfbe3a3a2eb5686f4f398d34bea34a257da8d141
María del Carmen García Mora <garcia20726@iesmarenostrum.com>
2021-11-08 22:46:51 +0100

    Creo archivos .js 'generasDatosEstaticos, GestionPresupuestoWeb'

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,6 +1,8 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+"use strict";
 
-// TODO: Variable global
+// Crear las funciones, objetos y variables indicadas en el enunciado
+
+// Variable global
 let presupuesto = 0;
 let gastos = [];
 let idGasto = 0;
---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+
+export   { 
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+} 
