9facb5c552cdb56baac3d887112c47b495607901
María del Carmen García Mora <garcia20726@iesmarenostrum.com>
2021-11-08 23:07:53 +0100

    función mostrarDatoEnID

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -2,8 +2,34 @@
 
 
 
+function mostrarDatoEnId(idElemento, valor){
+    idElemento.append(valor);
+}
+
+function mostrarGastoWeb(idElemento, gasto){
+
+    let mostrarGasto = document.getElementById(idElemento);
+    let arrayEtiquetas = "";
+
+    for ( let etiqueta of gasto.etiquetas ) {
+        arrayEtiquetas += `
+            <span class="gasto-etiquetas-etiqueta">${etiqueta}</span>
+        `
+    }
+
+    mostrarGasto.innerHTML += `
+        <div class="gasto">
+            <div class="gasto-descripcion">${gasto.descripcion}</div>
+            <div class="gasto-fecha">${gasto.fecha}</div>
+            <div class="gasto-valor">${gasto.valor}</div>
+            <div class="gasto-etiquetas">${arrayEtiquetas}</div>
+        </div>
+    `;
+
+}
+
+
 export   { 
     mostrarDatoEnId,
-    mostrarGastoWeb,
-    mostrarGastosAgrupadosWeb
+    mostrarGastoWeb
 } 
