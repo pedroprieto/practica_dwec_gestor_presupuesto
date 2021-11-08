9408aface9ed1ead2ca01196811083a9932de825
María del Carmen García Mora <garcia20726@iesmarenostrum.com>
2021-11-08 23:24:18 +0100

    función mostrarGastosAgrupadosWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -2,21 +2,25 @@
 
 
 
-function mostrarDatoEnId(idElemento, valor){
-    idElemento.append(valor);
+function mostrarDatoEnId( idElemento, valor ){
+// Escribe el valor (texto) en el elemento HTML con id idElemento indicado
+    idElemento.append( valor );
 }
 
-function mostrarGastoWeb(idElemento, gasto){
+function mostrarGastoWeb( idElemento, gasto ){
+// Añade dentro del elemento HTML con id idElemento indicado una estructura HTML para el gasto que se pase como parámetro 
 
-    let mostrarGasto = document.getElementById(idElemento);
+    let mostrarGasto = document.getElementById( idElemento );
     let arrayEtiquetas = "";
 
+    // Recorro el array etiquetas y las voy añadiendo
     for ( let etiqueta of gasto.etiquetas ) {
         arrayEtiquetas += `
             <span class="gasto-etiquetas-etiqueta">${etiqueta}</span>
-        `
+        `;
     }
 
+    // Voy añadiendo las propiedades del gasto
     mostrarGasto.innerHTML += `
         <div class="gasto">
             <div class="gasto-descripcion">${gasto.descripcion}</div>
@@ -28,8 +32,33 @@ function mostrarGastoWeb(idElemento, gasto){
 
 }
 
+function mostrarGastosAgrupadosWeb( idElemento, agrup, periodo ){
+// Crea dentro del elemento HTML con id idElemento indicado una estructura HTML para el objeto agrup que se pase como parámetro
+
+    let mostrarAgrupacion = document.getElementById(idElemento);
+    let arrayAgrupacion = "";
+
+    // Añado el array de los gastos agrupados por un periodo
+    for( let [nombre, valor] of Object.entries( agrup ) ){
+        arrayAgrupacion += `
+            <div class="agrupacion-dato">
+                <span class="agrupacion-dato-clave">${nombre}</span>
+                <span class="agrupacion-dato-valor">${valor}</span>
+            </div>
+        `;
+    }
+
+    // Voy añadiendo la agrupacones de gastos
+    mostrarAgrupacion.innerHTML = `
+        <div class="agrupacion">
+            <h1>Gastos agrupados por ${periodo}</h1>
+            ${arrayAgrupacion}
+        </div>
+    `;
+}
 
 export   { 
     mostrarDatoEnId,
-    mostrarGastoWeb
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
 } 
