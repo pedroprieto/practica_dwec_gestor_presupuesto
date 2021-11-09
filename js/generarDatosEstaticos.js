c5e31eb6ac363467485ca22f711a5fbd381779a0
Jose Angel Alfonso <joseangelal@gmail.com>
2021-11-09 19:58:04 +0100

    Realizo la función mostrarGastoWeb

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -4,3 +4,12 @@ import * as gesPresWeb from "./gestionPresupuestoWeb.js";
 gesPres.actualizarPresupuesto(1500);
 
 gesPresWeb.mostrarDatoEnId("presupuesto", gesPres.mostrarPresupuesto());
+
+
+
+let g1 = new gesPres.CrearGasto("Compra Carne", 23.44, "2021-05-06", "casa", "comida");
+
+gesPres.anyadirGasto(g1);
+
+
+console.log(g1.etiquetas)
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,16 +1,54 @@
-import { mostrarPresupuesto } from "./gestionPresupuesto";
+
 
 function mostrarDatoEnId(idElemento, valor){
 
     let mostrar = document.getElementById(idElemento);
 
-    mostrar.textContent = valor;
-
+    mostrar.innerHTML = valor;
     
+       
 };
 
-function mostrarGastoWeb(){
 
+function mostrarGastoWeb(idElemento, gasto){
+
+    let divPrincipal = document.getElementById(idElemento);
+    
+    
+
+    let divGasto = document.createElement('div');
+    divGasto.className = "gasto";
+    
+    
+    let divGastoDescripcion = document.createElement('div');
+    divGastoDescripcion.className = "gasto-descripcion";
+    divGastoDescripcion.append(gasto.descripcion);
+    
+    let divGastoFecha = document.createElement('div');
+    divGastoFecha.className = "gasto-fecha";
+    divGastoFecha.append(gasto.fecha);
+
+    let divGastoValor = document.createElement('div');
+    divGastoValor.className = "gasto-valor";
+    divGastoValor.append(gasto.valor);
+
+    let divGastoEtiquetas = document.createElement('div');
+    divGastoEtiquetas.className = "gasto-etiquetas"
+
+    for (let etiqueta of gasto.etiqueta) {
+        
+        let span = document.createElement('span');
+        span.className = "gasto-etiquetas-etiqueta";
+        span.append(etiqueta);
+        divGastoEtiquetas.append(span);        
+    }
+
+    divGasto.append(divGastoDescripcion);
+    divGasto.append(divGastoFecha);
+    divGasto.append(divGastoValor);
+    divGasto.append(divGastoEtiquetas);
+    
+    divPrincipal.append(divGasto);
 
 };
 
