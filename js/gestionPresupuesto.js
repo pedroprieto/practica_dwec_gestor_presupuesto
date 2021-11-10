6a1811b5cc2e8a42187e7de05d47ec23b2b2286e
JSM2DAWZ <sanchez15189@iesmarenostrum.com>
2021-11-10 10:21:53 +0100

    añadido objeto gasto y funciones del objeto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,14 +1,41 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
+
 
 let presupuesto;
 presupuesto = 0;
 
-function actualizarPresupuesto() {
-    if (cantidad >= 0)
-    {
-        presupuesto = cantidad;
+let gasto = {
+    descripcion : "",
+    valor : 0,
+
+
+    mostrarGasto() {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    },
+
+    actualizarDescripcion(descripcion) {
+    
+        this.descripcion = descripcion;
+    },
+
+    actualizarValor(valor) {
+    
+        if (valor >= 0) 
+        {
+            this.valor = valor;
+        }
+    }
+}
+
+
+
+
+function actualizarPresupuesto(valor) {
+    
+    if (valor >= 0) {
+    
+        presupuesto = valor;
         return presupuesto;
     } 
     else {
@@ -18,11 +45,19 @@ function actualizarPresupuesto() {
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    gasto.descripcion = descripcion;
+    if(valor >= 0) {
+        gasto.valor = valor;
+
+    } else {
+        gasto.valor = 0;
+    }
+    return gasto;
+
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
