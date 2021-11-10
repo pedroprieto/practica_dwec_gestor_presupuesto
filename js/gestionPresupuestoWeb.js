83297f2353b906d6517682423089da8f02be8c7a
Rafael Gandolfo Garcia <rafagandolfogarcia@gmail.com>
2021-11-10 14:45:33 +0100

    mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -4,11 +4,36 @@ function mostrarDatoEnId(idElemento,valor){
     document.getElementById(idElemento).append(valor);
 }
 
-
+function auxCrearElemento(tagHTML,clase,contenido){
+    let etiqueta=document.createElement(tagHTML);
+    etiqueta.className=clase;
+    etiqueta.innerHTML=contenido;
+    return etiqueta;
+}
 
 function mostrarGastoWeb(idElemento,gasto){
 
-    
+    let tag = document.createElement('div');
+    tag.className="gasto";
+
+    tag.prepend(auxCrearElemento('div',"gasto-descripcion", gasto.descripcion));
+    tag.prepend(auxCrearElemento('div',"gasto-fecha", gasto.fecha));
+    tag.prepend(auxCrearElemento('div',"gasto-valor", gasto.valor));
+
+    let etiquetas=document.createElement('div');
+    etiquetas.className="gasto-etiquetas";
+
+    if ( gasto.etiquetas ){
+
+        for(let etiqueta of gasto["etiquetas"]){
+            let span=document.createElement('span');
+            span.className="gasto-etiquetas-etiqueta"
+            span.innerHTML=etiqueta;
+            etiquetas.prepend(span);
+        }
+    }
+    tag.prepend(etiquetas);
+    document.getElementById(idElemento).append(tag);
 }
 
 function mostrarGastosAgrupadosWeb(idElemento,agrupacionGasto,periodo){}
