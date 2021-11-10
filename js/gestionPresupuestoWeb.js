b01b59eda615a3f2a59549498bb98b00356c9ffd
Rafael Gandolfo Garcia <rafagandolfogarcia@gmail.com>
2021-11-10 14:42:12 +0100

    mostrardatoenid

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,20 @@
+
+
+function mostrarDatoEnId(idElemento,valor){
+    document.getElementById(idElemento).append(valor);
+}
+
+
+
+function mostrarGastoWeb(idElemento,gasto){
+
+    
+}
+
+function mostrarGastosAgrupadosWeb(idElemento,agrupacionGasto,periodo){}
+
+export  {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+};
