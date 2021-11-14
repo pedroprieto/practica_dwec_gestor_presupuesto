b84cfcc7d442ab598f64142b1d3cd660494ceb56
Susana Fernández <fernandez20131@iesmarenostrum.com>
2021-11-14 14:28:29 +0100

    Interacción html - fichero gestionPresupuestoWeb.js

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,58 @@
+
+function mostrarDatoEnId(idElemento, valor) {
+    let element = document.getElementById(idElemento);
+    element.textContent = valor;
+}
+
+function mostrarGastoWeb(idElemento, gasto) {
+    let html = `<div class="gasto">
+    <div class="gasto-descripcion">${gasto.descripcion}</div>
+    <div class="gasto-fecha">${gasto.fecha}</div> 
+    <div class="gasto-valor">${gasto.valor}}</div> 
+    <div class="gasto-etiquetas">`;
+
+    for (let etiqueta of gasto.etiquetas) {
+        html += `<span class="gasto-etiquetas-etiqueta">
+        ${etiqueta}
+      </span>`;
+    }
+
+      
+    html += `</div> 
+    </div>
+    `;
+
+    let element = document.getElementById(idElemento);
+    element.innerHTML = html;
+}
+
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+    let element = document.getElementById(idElemento);
+    let periodoText;
+    if(periodo == 'dia') {
+        periodoText = 'día';
+    } else if(periodo == 'anyo') {
+        periodoText = 'año';
+    } else {
+        periodoText = periodo;
+    }
+
+    let html = `<div class="agrupacion">
+    <h1>Gastos agrupados por ${periodoText}</h1>`;
+
+    for (let agrupacion of Object.keys(agrup)) {
+        html += `<div class="agrupacion-dato">
+        <span class="agrupacion-dato-clave">${agrupacion}</span>
+        <span class="agrupacion-dato-valor">${agrup[agrupacion]}</span>
+      </div>`;
+    }
+
+    html += '</div>';
+    element.innerHTML = html;
+}
+
+export {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
