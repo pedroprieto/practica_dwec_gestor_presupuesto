e46e96f0eefe6472baad074ceac9c057d5fc7bcb
Susana Fernández <fernandez20131@iesmarenostrum.com>
2021-11-14 16:34:15 +0100

    Interacción html - práctica finalizada

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -3,12 +3,12 @@ import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js';
 
 gestionPresupuesto.actualizarPresupuesto(1500);
 gestionPresupuestoWeb.mostrarDatoEnId('presupuesto', gestionPresupuesto.mostrarPresupuesto());
-let gasto1 = gestionPresupuesto.CrearGasto('Compra carne', 23.44, '2021-10-06', 'casa', 'comida');
-let gasto2 = gestionPresupuesto.CrearGasto('Compra fruta y verdura', 14.25, '2021-09-06', 'supermercado', 'comida');
-let gasto3 = gestionPresupuesto.CrearGasto('Bonobús', 18.60, '2020-05-26', 'transporte');
-let gasto4 = gestionPresupuesto.CrearGasto('Gasolina', 60.42, '2021-10-08', 'transporte', 'gasolina');
-let gasto5 = gestionPresupuesto.CrearGasto('Seguro hogar', 206.45, '2021-09-26', 'casa', 'seguros');
-let gasto6 = gestionPresupuesto.CrearGasto('Seguro coche', 195.78, '2021-10-06', 'transporte', 'seguros');
+let gasto1 = new gestionPresupuesto.CrearGasto('Compra carne', 23.44, '2021-10-06', 'casa', 'comida');
+let gasto2 = new gestionPresupuesto.CrearGasto('Compra fruta y verdura', 14.25, '2021-09-06', 'supermercado', 'comida');
+let gasto3 = new gestionPresupuesto.CrearGasto('Bonobús', 18.60, '2020-05-26', 'transporte');
+let gasto4 = new gestionPresupuesto.CrearGasto('Gasolina', 60.42, '2021-10-08', 'transporte', 'gasolina');
+let gasto5 = new gestionPresupuesto.CrearGasto('Seguro hogar', 206.45, '2021-09-26', 'casa', 'seguros');
+let gasto6 = new gestionPresupuesto.CrearGasto('Seguro coche', 195.78, '2021-10-06', 'transporte', 'seguros');
 
 gestionPresupuesto.anyadirGasto(gasto1);
 gestionPresupuesto.anyadirGasto(gasto2);
