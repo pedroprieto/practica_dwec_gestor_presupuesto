deaaac742cb1733aefc09a84e51a08e3693ce91c
Pau Garcia Gomez <garcia20139@iesmarenostrum.com>
2021-11-19 19:16:18 +0100

    Realizo las correcciones indicadas por el profesor

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -28,147 +28,47 @@ function mostrarPresupuesto() {
 
 function CrearGasto(descrip,cantid, fec, etiq) {
     // TODO
-    gasto.descripcion= descrip;
-    gasto.fecha = fec;
+    this.descripcion= descrip;
+    this.valor = 0;
+    this.fecha = fec;
+    this.etiquetas = [];
 
     if(cantid >= 0)
     {
-        gasto.valor = cantid;
+        this.valor = cantid;
     }else
-        gasto.valor = 0;
+        this.valor = 0;
 
     if (etiq == null){
-        gasto.etiquetas= [];
+        this.etiquetas= [];
     }else{
-        gasto.etiquetas= etiq;
+        this.etiquetas= etiq;
     }
 
     if(fec == null){
-        gasto.fecha = new Date();
+        this.fecha = new Date();
     }else{
-        gasto.fecha = new Date(Date.parse(fec));
-    }
-    return (gasto);
-}
-
-
-
-//Objeto gasto y sus métodos
-let gasto = {
-    descripcion: "",
-    valor: 0,
-    fecha = new Date(timestamp),
-    etiquetas = []
-};
-
-function mostrarGasto(){
-    return("Gasto correspondiente a " + gasto.descripcion + " con valor " + gasto.valor + " €");
-}
-
-function actualizarDescripcion (descr) {
-    gasto.descripcion = descr;
-}
-
-function actualizarValor (val) {
-    if(val >= 0)
-        gasto.valor = val;
-    else
-        return;
-}
-
-//métodos práctica2
-function mostrarGastoCompleto(){
-    console.log("Gasto correspondiente a " + gasto.descripcion + "con valor" + gasto.valor + " €");
-    console.log("Fecha: " + gasto.fecha);
-    console.log("Etiquetas:");
-    for(i = 0;i < gasto.etiquetas.length; i++){
-        console.log("- " + gasto.etiquetas[i]);
-    }
-}
-
-function actualizarFecha(paramFecha){
-    gasto.fecha = new Date(Date.parse(paramFecha));
-}
-
-function anyadirEtiquetas(...argumentos){
-    for(i = 0; i < gasto.etiquetas.length; i++){
-        if(gasto.etiquetas.includes(argumentos[i], 0)){
-            gastos.push(argumentos[i]);
-        }  
-        if(idGasto > 0) {
-            idGasto++;
-        }
-    }
-}
-
-function borrarEtiquetas(...argumen){
-    let entrada;
-
-    for(i = 0; i < gasto.etiquetas.length; i++){
-        if(gasto.etiquetas.includes(argumentos[i], 0)){
-            entrada = gasto.etiquetas.indexOf(argumen[i],0);
-            gasto.etiquetas.splice(entrada, 1); 
-        }  
-        if(idGasto > 0) {
-            idGasto++;
-        }
-    }
-}
-
-
-gasto.mostrarGasto = mostrarGasto;
-gasto.actualizarDescripcion = actualizarDescripcion;
-gasto.actualizarValor = actualizarValor;
-
-
-gasto.mostrarGastoCompleto = mostrarGastoCompleto;
-gasto.actualizarFecha = actualizarFecha;
-gasto.anyadirEtiquetas = anyadirEtiquetas;
-gasto.borrarEtiquetas = borrarEtiquetas;
-
-
-//Funciones práctica 2
-
-function listarGastos(){
-    for(i = '0'; i < gastos.length;i++){
-        console.log(gastos[i]);
-    }
-    return(gastos);
-}
-
-
-function anyadirGasto(paramGasto){
-    paramGasto.id= idGasto;
-    idGasto++; 
-    gastos.push(paramGasto);
-}
-
-function borrarGasto(id){
-    gastos.etiquetasa.splice(id, 1);
-    if(idGasto > 0){
-        idGasto = idGasto -1;
-    }
-}
-
-function calcularTotalGastos(){
-    let totalGastos = 0;
-    for(i=0; i <= gasto.etiquetas.length;i++){
-        totalGastos += gasto.etiquetas[i];
+        this.fecha = new Date(Date.parse(fec));
     }
 
-    return(totalGastos);
+
+
+    this.mostrarGasto = function(){
+        return("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + " €");
+    }
+
+    this.actualizarDescripcion = function(descr){
+        this.descripcion = descr;
+    }
+
+    this.actualizarValor = function(val){
+        if(val >= 0)
+            this.valor = val;
+        else
+            return;
+    }
+
 }
-
-function calcularBalance(){
-    let tG;
-    let balance;
-
-    tG = gastos.calcularTotalGastos();
-    balance = presupuesto - tG;
-
-    return(balance);
-}
-
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -176,11 +76,6 @@ function calcularBalance(){
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto,
-    listarGastos,
-    anyadirGasto,
-    borrarGasto,
-    calcularTotalGastos,
-    calcularBalance
+    CrearGasto
 }
 
