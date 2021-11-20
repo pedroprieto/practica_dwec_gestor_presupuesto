270aa04140168a084c56e6e1f3184e3dfc4d67c9
Leidygemade <91139481+Leidygemade@users.noreply.github.com>
2021-11-20 20:36:22 +0100

    Añadir estructura HTML a funciónGastosAgrupadoWeb (pendiente de ver el test)

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -27,22 +27,42 @@ function mostrarGastoWeb(idElemento, gasto) {
 
     let div4 = document.createElement('div');
     div1.classList.add('gasto-etiquetas');
-    
-
     for (let etiqueta of gasto.etiquetas) {
         let span = document.createElement('span');
         span.classList.add('gasto-etiquetas-etiqueta');
         span.createTextNode(etiqueta);
         div4.append(span);
     }
-
     divPadre.append(div4);
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
     let id = document.getElementById(idElemento);
 
+    let divPadre = document.createElement('div');
+    divPadre.classList.add('agrupación');
+    id.append(divPadre);
 
+    let titulo = document.createElement('h1');
+    titulo.createTextNode('Gastos agrupados por '+periodo);
+    divPadre.append(titulo);
+
+    for(let key in agrup) {
+        let divAgrup = document.createElement('div');
+        divAgrup.classList.add('agrupacion-dato');
+        
+        let spanClave = document.createElement('span');
+        spanClave.classList.add('agrupacion-dato-clave');
+        spanClave.createTextNode(key);
+        divAgrup.append(spanClave);
+
+        let spanValor = document.createElement('span');
+        spanValor.classList.add('agrupacion-dato-valor');
+        spanValor.createTextNode(agrup[key]);
+        divAgrup.append(spanValor);
+    }
+
+    divPadre.append(divAgrup);
 }
 
 export {
