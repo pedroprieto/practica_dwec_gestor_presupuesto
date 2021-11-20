8225eda00a3ce5f4f555ab7cf1330febf94ff12f
Leidygemade <91139481+Leidygemade@users.noreply.github.com>
2021-11-20 20:08:39 +0100

    Añadir estructura HTML a función mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,14 +1,47 @@
 function mostrarDatoEnId(idElemento, valor) {
     let id = document.getElementById(idElemento);
-    id.document.write(valor);
+    id.innerHTML(valor);
 }
 
 function mostrarGastoWeb(idElemento, gasto) {
     let id = document.getElementById(idElemento);
-    id.document.write(gasto);
+
+    let divPadre = document.createElement('div');
+    divPadre.classList.add('gasto')
+    id.append(divPadre);
+
+    let div1 = document.createElement('div');
+    div1.classList.add('gasto-descripcion');
+    div1.createTextNode(gasto.descripcion);
+    divPadre.append(div1);
+
+    let div2 = document.createElement('div');
+    div1.classList.add('gasto-fecha');
+    div1.createTextNode(gasto.fecha);
+    divPadre.append(div2);
+
+    let div3 = document.createElement('div');
+    div1.classList.add('gasto-valor');
+    div1.createTextNode(gasto.valor);
+    divPadre.append(div3);
+
+    let div4 = document.createElement('div');
+    div1.classList.add('gasto-etiquetas');
+    
+
+    for (let etiqueta of gasto.etiquetas) {
+        let span = document.createElement('span');
+        span.classList.add('gasto-etiquetas-etiqueta');
+        span.createTextNode(etiqueta);
+        div4.append(span);
+    }
+
+    divPadre.append(div4);
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+    let id = document.getElementById(idElemento);
+
 
 }
 
