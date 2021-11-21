2169a5283ce5faba419939da3ddd511b517886e5
Patricia <patricia.romarquez@gmail.com>
2021-11-21 23:06:57 +0100

    Fixed CrearGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,48 +4,48 @@
 var presupuesto = 0;
 
 function actualizarPresupuesto(valor) {
-    if (valor >= 0) {
-        presupuesto = valor;
-        return valor;
-    }
-    console.log("Error valor negativo: " + valor);
-    return -1;
+  if (valor >= 0) {
+    presupuesto = valor;
+    return valor;
+  }
+  console.log("Error valor negativo: " + valor);
+  return -1;
 }
 
 function mostrarPresupuesto() {
-    return `Tu presupuesto actual es de ${presupuesto} €`;
+  return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
 function CrearGasto(descripcion, valor) {
-    if (typeof valor !== "number" || valor < 0) {
-        valor = 0;
-    }
+  if (isNaN(valor) || valor < 0) {
+    valor = 0;
+  }
 
-    return {
-        descripcion: descripcion,
-        valor: valor,
-        mostrarGasto: function () {
-            console.log(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
-            return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
-        },
-        actualizarDescripcion: function (descripcion) {
-            this.descripcion = descripcion;
-        },
-        actualizarValor: function (valor) {
-            if (valor >= 0) {
-                this.valor = valor;
-            }
-        }
-    };
+  this.descripcion = descripcion;
+  this.valor = valor;
+
+  this.mostrarGasto = function () {
+    return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+  };
+
+  this.actualizarDescripcion = function (descripcion) {
+    this.descripcion = descripcion;
+  };
+
+  this.actualizarValor = function (valor) {
+    if (valor >= 0) {
+      this.valor = valor;
+    }
+  };
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto   
+export {
+  mostrarPresupuesto,
+  actualizarPresupuesto,
+  CrearGasto
 }
 
 
