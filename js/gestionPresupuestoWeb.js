a6bbcc2dcd6af61fa002089e47c96f355b74372b
Maria C. Mediavilla <mediavilla20800@iesmarenostrum.com>
2021-11-23 18:41:56 +0100

    eventos

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,3 +1,5 @@
+import * as gesPres from "./gestionPresupuesto";
+
 
 function mostrarDatoEnId(idElemento,valor){
 
@@ -6,7 +8,7 @@ function mostrarDatoEnId(idElemento,valor){
 }
 function mostrarGastoWeb (idElemento, gasto){
 
-
+    
     let divGen = document.createElement('div');
     divGen.className = "gasto"; 
 
@@ -28,6 +30,8 @@ function mostrarGastoWeb (idElemento, gasto){
 
     divGen.append(divDes, divFech, divVal, divEti);
   
+
+
     for (let etiqueta of gasto.etiquetas)
     {
          
@@ -37,6 +41,14 @@ function mostrarGastoWeb (idElemento, gasto){
         spanEti.append(`${etiqueta},`);
         
         divEti.append(spanEti);
+
+
+        
+    let elimitaretiquetassobre = new BorrarEtiquetasHandle();
+    elimitaretiquetassobre.gasto = gasto;
+    elimitaretiquetassobre.etiqueta = etiqueta;
+    span.addEventListener("click", elimitaretiquetassobre);
+
     }
 
       
@@ -44,7 +56,30 @@ function mostrarGastoWeb (idElemento, gasto){
 
      
     contenido.append(divGen);    
+
+    let botoneditar = document.createElement("button");
+    botoneditar.type ="button";
+    botoneditar.className = "gasto-editar";
+    botoneditar.textContent = "Editar";
+    let btnedit = new EditarHandle();
+    btnedit.gasto = gasto;
+    botoneditar.addEventListener("click", btnedit);
+
+let botonborrar = document.createElement("button");
+    botonborrar.type ="button";
+    botonborrar.className = "gasto-editar";
+    botonborrar.textContent = "Editar";
+    let btnborrar = new BorrarHandle();
+    btnborrar.gasto = gasto;
+    botonborrar.addEventListener("click", btnborrar);
+
+
+
 }
+    DivGen.append(botoneditar);
+    DivGen.append(botonborrar);
+
+
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
 
     let div = document.createElement('div');
@@ -66,8 +101,8 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
 
         
 
-        span.append("key: " + key);
-        span1.append(" Value: " + value);
+        span.append(" " + key);
+        span1.append("  " + value);
         
         div1.append(span);
         div1.append(span1);
@@ -78,10 +113,104 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
 
     contenido.append(div);
 }
+
+document.getElementById("actualizarpresupuesto").addEventListener("click", botonactualizarpresupuesto);
+document.getElementById("anyadirgasto").addEventListener("click", nuevoGastoWeb);
+
+function botonactualizarpresupuesto(){
+    let promt = prompt("Introduzca nuevo presupuesto");
+    let nuevopresupuesto = parsefloat(promt);
+
+    gesPres.actualizarPresupuesto(nuevopresupuesto);
+    
+    repintar();
+}
+
+function nuevoGastoWeb(){
+
+    let descripcion = prompt("Introduzca descripcion");
+    let valor = prompt("Introduzca valor");
+    let valorbien = parsefloat(valor);
+
+    let fecha = prompt("Introduzca fecha");
+    let fechabien = new Date(fecha);
+    fechabien.toISOString;
+    let etiquetas = prompt("Introduzca etiquetas");
+    let arrEtiquetas = etiquetas.split(', ');
+
+    let gastonuevo = new CrearGasto(descripcion,valorbien,fechabien,...arrEtiquetas);
+    
+    gesPres.anyadirGasto(gastonuevo);
+    
+    repintar();
+}
+
+function repintar(){
+
+    mostrarDatoEnId("presupuesto", gesPres.mostrarPresupuesto());
+    mostrarDatoEnId("gastos-totales", gesPres.calcularTotalGastos());
+    mostrarDatoEnId("balance-total", gesPres.calcularBalance());
+
+    document.getElementById("listado-gastos-completo").innerHTML = "";
+
+    let listaGastos = gesPres.listarGastos();
+    for (let g of listaGastos)
+    {
+        mostrarGastoWeb("listado-gastos-completo", g);
+    }
+}
+
+function EditarHandle(){
+
+    this.handleEvent = function(e){
+
+    let descripcion = prompt("Introduzca descripcion");
+    let valor = prompt("Introduzca valor");
+    let valorbien = parsefloat(valor);
+
+    let fecha = prompt("Introduzca fecha");
+    let fechabien = new Date(fecha);
+    fechabien.toISOString;
+    let etiquetas = prompt("Introduzca etiquetas");
+    let arrEtiquetas = etiquetas.split(', ');
+
+    this.gasto.actualizarDescripcion(descripcion);
+    this.gasto.actualizarValor(valorbien);
+    this.gasto.actualizarfecha(fechabien);
+    this.gasto.anyadirEtiquetas(...arrEtiquetas);
+
+        
+    repintar();
+    
+       } 
+      
+
+}
+function BorrarHandle(){
+    this.handleEvent = function(e){
+
+    gesPres.borrarGasto(this.gasto.id);   
+            
+        repintar();
+        
+           } 
+
+}
+function BorrarEtiquetasHandle(){
+
+    this.handleEvent = function(e){
+
+this.gasto.borrarEtiquetas(this.etiqueta);
+
+
+         repintar();
+    }
+}
+
 export  {
     mostrarDatoEnId,
     mostrarGastoWeb,
     mostrarGastosAgrupadosWeb
-};
+}
 
 
