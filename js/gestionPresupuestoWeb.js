c628d41647d9d4d89d29b71dff1e723a5c8899af
Leidygemade <91139481+Leidygemade@users.noreply.github.com>
2021-11-23 14:42:57 +0100

    Corrijo lo de las etiquetas como una lista de parámetros

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -133,7 +133,7 @@ function nuevoGastoWeb() {
     let etiquetasArray = etiquetas.split(',');
 
     //crear el gasto
-    let gastoNuevo = new gesPres.CrearGasto(descripcion,convertirValor,fecha,etiquetasArray);
+    let gastoNuevo = new gesPres.CrearGasto(descripcion, convertirValor, fecha, ...etiquetasArray);
 
     //añadir el gasto
     gesPres.anyadirGasto(gastoNuevo);
