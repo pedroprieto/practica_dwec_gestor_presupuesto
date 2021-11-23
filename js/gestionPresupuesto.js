e6bcc9132eded49258d7b2e7289f22a973fbd117
Sonia Soriano <soriano20887@iesmarenostrum.com>
2021-11-23 11:52:36 +0100

    Creacion transformarListadoEtiquetas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -297,6 +297,14 @@ function agruparGastos(periodo, etiquetas, fechaDesde, fechaHasta)
     {});
 }
 
+function transformarListadoEtiquetas(etiquetasTiene) 
+{
+    let palabras = /[a-z]/gi; //Todas las coincidencias de letras sin distinguir mayus y minusculas
+    let resul = etiquetasTiene.match(palabras);
+
+    return resul;
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -312,4 +320,5 @@ export
     calcularBalance,
     filtrarGastos,
     agruparGastos,
+    transformarListadoEtiquetas
 }
