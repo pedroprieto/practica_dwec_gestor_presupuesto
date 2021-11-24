a096dc0ac57d614063bef7a68a6413b13ff4e8f2
Pedro Delgado <pdelgado@intrasoft-intl.com>
2021-11-24 16:33:04 +0100

    Añadidos nuevos métodos a gestionPresupuesto.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -91,9 +91,10 @@ function calcularBalance() {
     return presupuesto - calcularTotalGastos();
 }
 
-// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
-// Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+function filtrarGastos() {}
+
+function agruparGastos() {}
+
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
@@ -102,5 +103,7 @@ export   {
     anyadirGasto,
     borrarGasto,
     calcularTotalGastos,
-    calcularBalance
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos
 }
