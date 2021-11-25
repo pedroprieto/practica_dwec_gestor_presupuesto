8a73500345942dc45a3aed97b7fcd570d65b1edc
Pedro Delgado <pdelgado@intrasoft-intl.com>
2021-11-25 17:58:19 +0100

    Implementación método mostrarGastoWeb.

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -3,7 +3,45 @@ function mostrarDatoEnId(idElemento, valor) {
 }
 
 function mostrarGastoWeb(idElemento, gasto) {
-    let gastoHTLM = document.createElement('div', {class: 'gasto'});
+
+    // Elemento raiz del gasto
+    const gastoHTLM = document.createElement('div');
+    gastoHTLM.className = 'gasto';
+
+    // Descripción
+    const descripcionHTML = document.createElement('div');
+    descripcionHTML.className = 'gasto-descripcion';
+    const descripcionText = document.createTextNode(gasto.descripcion);
+    descripcionHTML.appendChild(descripcionText);
+    gastoHTLM.appendChild(descripcionHTML);
+
+    // Fecha
+    const fechaHTML = document.createElement('div');
+    fechaHTML.className = 'gasto-fecha';
+    const fechaText = document.createTextNode(new Date(gasto.fecha).toLocaleString());
+    fechaHTML.appendChild(fechaText);
+    gastoHTLM.appendChild(fechaHTML);
+
+    // Valor
+    const valorHTML = document.createElement('div');
+    valorHTML.className = 'gasto-valor';
+    const valorText = document.createTextNode(gasto.valor);
+    valorHTML.appendChild(valorText);
+    gastoHTLM.appendChild(valorHTML);
+
+    // Etitquetas
+    const etiquetasHTML = document.createElement('div');
+    etiquetasHTML.className = 'gasto-etiquetas';
+    const etiquetas = gasto.etiquetas;
+    etiquetas.forEach(etiqueta => {
+        const etiquetaHTML = document.createElement('span');
+        etiquetaHTML.className = 'gasto-etiquetas-etiqueta';
+        const etiquetaText = document.createTextNode(etiqueta);
+        etiquetaHTML.appendChild(etiquetaText);
+        etiquetasHTML.appendChild(etiquetaHTML);
+    });
+    gastoHTLM.appendChild(etiquetasHTML);
+
     document.getElementById(idElemento).append(gastoHTLM);
 }
 
