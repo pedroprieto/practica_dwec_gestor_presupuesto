739a70bdc557822d71187c8e8b5a00342e950c4b
Maria Jose Gomez Pastor <gomez20740@iesmarenostrum.com>
2021-11-25 13:55:31 +0100

    modificación de export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -134,6 +134,14 @@ function calcularBalance() {
     return balance;
 }
 
+function filtrarGastos() {
+    
+}
+
+function agruparGastos() {
+    
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -145,5 +153,7 @@ export   {
     anyadirGasto,
     borrarGasto,
     calcularTotalGastos,
-    calcularBalance
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos,
 }
