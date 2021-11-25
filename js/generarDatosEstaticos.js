bad24da52aac6935b2e98eeba72bb2eb87f5b88e
Alejandro Martinez <martinez20793@iesmarenostrum.com>
2021-11-25 17:59:49 +0100

    Subida actualizacion HTML

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,6 @@
+import * as gesPres from ("./gestionPresupuesto.js");
+import * as gesPresWeb from ("./gestionPresupuestoWeb.js");
+
+
+gesPres.actualizarPresupuesto(1500);
+gesPresWeb.mostrarDatoEnId('presupuesto', gesPres.mostrarPresupuesto());
---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,31 @@
+
+function mostrarDatoEnId(idElemento, valor){
+    document.getElementById(idElemento).innerHTML = valor;
+}
+
+function mostrarGastoWeb(idElemento, gasto){
+
+}
+
+function mostrarGastosAgrupadosWeb(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export   {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb,
+}
