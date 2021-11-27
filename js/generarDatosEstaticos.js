b3863d6ae9e61962ccde4b05960e2f4f44def8fc
Alejandro Martinez <martinez20793@iesmarenostrum.com>
2021-11-27 05:05:46 +0100

    incluida función mostrarGastoWeb

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,6 +1,56 @@
-import * as gesPres from ("./gestionPresupuesto.js");
-import * as gesPresWeb from ("./gestionPresupuestoWeb.js");
+import * as gesPres from "./gestionPresupuesto.js";
+import * as gesPresWeb from "./gestionPresupuestoWeb.js";
 
 
 gesPres.actualizarPresupuesto(1500);
 gesPresWeb.mostrarDatoEnId('presupuesto', gesPres.mostrarPresupuesto());
+
+  let g1 = new gesPres.CrearGasto("Compra carne", 23.44, "2021-10-06", "supermercado", "comida");
+  let g2 = new gesPres.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+  let g3 = new gesPres.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+  let g4 = new gesPres.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+  let g5 = new gesPres.CrearGasto("Seguro Hogar", 206.45, "2021-09-26", "casa", "seguros");
+  let g6 = new gesPres.CrearGasto("Seguro Coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+  gesPres.anyadirGasto(g1);
+  gesPres.anyadirGasto(g2);
+  gesPres.anyadirGasto(g3);
+  gesPres.anyadirGasto(g4);
+  gesPres.anyadirGasto(g5);
+  gesPres.anyadirGasto(g6);
+
+  gesPresWeb.mostrarDatoEnId("gastos-totales", gesPres.calcularTotalGastos());
+  gesPresWeb.mostrarDatoEnId("balance-total", gesPres.calcularBalance());
+
+  let listaGastos = gesPres.listarGastos();
+  for(let gasto of listaGastos){
+      gesPresWeb.mostrarGastoWeb("listado-gastos-completo", gasto);
+  }
+
+  let filtroGastos1 = gesPres.filtrarGastos({fechaDesde: "2021-09-01", fechaHasta: "2021-09-30"});
+  
+  for (let gasto of filtroGastos1)
+  {
+    gesPresWeb.mostrarGastoWeb("listado-gastos-filtrado-1", gasto);
+  }
+
+  let filtroGastos2 = gesPres.filtrarGastos({valorMinimo: 50});
+  
+  for (let gasto of filtroGastos2)
+  {
+    gesPresWeb.mostrarGastoWeb("listado-gastos-filtrado-2", gasto);
+  }
+
+  let filtroGastos3 = gesPres.filtrarGastos({valorMinimo: 200, etiquetas: "seguros"});
+  
+  for (let gasto of filtroGastos3)
+  {
+    gesPresWeb.mostrarGastoWeb("listado-gastos-filtrado-3", gasto);
+  }
+
+  let filtroGastos4 = gesPres.filtrarGastos({valorMaximo: 50, etiquetas: "comida , transporte"});
+  
+  for (let gasto of filtroGastos4)
+  {
+    gesPresWeb.mostrarGastoWeb("listado-gastos-filtrado-4", gasto);
+  }
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -5,6 +5,44 @@ function mostrarDatoEnId(idElemento, valor){
 
 function mostrarGastoWeb(idElemento, gasto){
 
+    let divGasto = document.createElement('div');
+    let divGasDescripcion = document.createElement('div');
+    let divGasFecha = document.createElement('div');
+    let divGasValor =  document.createElement('div');
+    let divGasEtiqueta =  document.createElement('div');
+
+    divGasto.className = "gasto";
+
+    divGasDescripcion.className ="gasto-descripcion";
+    divGasDescripcion.append(gasto.descripcion);
+    
+    divGasFecha.className ="gasto-fecha";
+    let fechaRecorte = new Date(gasto.fecha).toLocaleString();
+    fechaRecorte = fechaRecorte.substring(0,9);
+    divGasFecha.append(fechaRecorte);
+    
+    divGasValor.className ="gasto-valor";
+    divGasValor.append(gasto.valor);
+
+    divGasto.append(divGasDescripcion);
+    divGasto.append(divGasFecha);
+    divGasto.append(divGasValor);
+
+    divGasEtiqueta.className = "gasto-etiquetas";
+
+    for (let etiqueta of gasto.etiquetas)
+    {        
+        let spanEtiqueta = document.createElement('span');
+        spanEtiqueta.className="gasto-etiquetas-etiqueta";
+
+        spanEtiqueta.innerHTML = etiqueta + "<br>";
+        divGasEtiqueta.append(spanEtiqueta);
+    }
+    
+    divGasto.append(divGasEtiqueta);
+
+    document.getElementById(idElemento).append(divGasto);
+
 }
 
 function mostrarGastosAgrupadosWeb(){
