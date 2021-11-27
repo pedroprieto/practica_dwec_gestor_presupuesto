7a31ba7a371252768ea4ab07c08cab694d24ae34
PatriRomero <romero16622@iesmarenostrum.com>
2021-11-27 20:13:10 +0100

    Implementado mostrar presupuesto

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -2,3 +2,4 @@ import * as gestion from "./gestionPresupuesto.js";
 import * as web from "./gestionPresupuestoWeb.js";
 
 gestion.actualizarPresupuesto(1500)
+web.mostrarDatoEnId("presupuesto", gestion.mostrarPresupuesto());
