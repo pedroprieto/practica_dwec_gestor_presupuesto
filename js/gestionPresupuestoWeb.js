e11deaa826106bfbdcb52549a34aca538f5c530a
PatriRomero <romero16622@iesmarenostrum.com>
2021-11-27 20:10:32 +0100

    Implementado mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,3 +1,25 @@
 function mostrarDatoEnId(idElemento, valor) {
   document.getElementById(idElemento).innerHTML += valor;
 }
+
+function mostrarGastoWeb(idElemento, gasto) {
+  var etiquetasText = ``;
+
+  for (let etiqueta of gasto.etiquetas) {
+    etiquetasText += `
+      <span class="gasto-etiquetas-etiqueta">
+        ${etiqueta}
+      </span>
+    `;
+  }
+
+  mostrarDatoEnId(idElemento, `
+  <div class="gasto">
+  <div class="gasto-descripcion">${gasto.descripcion}</div>
+  <div class="gasto-fecha">${gasto.obtenerPeriodoAgrupacion("dia")}</div> 
+  <div class="gasto-valor">${gasto.valor}</div> 
+  <div class="gasto-etiquetas">
+    ${etiquetasText}
+  </div> 
+  </div>`);
+}
