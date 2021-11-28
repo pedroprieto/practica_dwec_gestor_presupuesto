ed23ec76176679f168bfaeca6b46165e6a82a533
Maria Jose Gomez Pastor <gomez20740@iesmarenostrum.com>
2021-11-28 14:48:46 +0100

    mostrargastoweb filtrado tests 6/9

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -20,6 +20,40 @@ gestionPresupuesto.anyadirGasto(gasto6);
 gestionPresupuesto.actualizarPresupuesto(1500);
 
 /* Mostrar datos */
-gestionPresupuestoWeb.mostrarDatoEnId('presupuesto', gestionPresupuesto.mostrarPresupuesto());
-gestionPresupuestoWeb.mostrarDatoEnId('gastos-totales', gestionPresupuesto.calcularTotalGastos());
-gestionPresupuestoWeb.mostrarDatoEnId('balance-total', gestionPresupuesto.calcularBalance());
+gestionPresupuestoWeb.mostrarDatoEnId("presupuesto", gestionPresupuesto.mostrarPresupuesto());
+gestionPresupuestoWeb.mostrarDatoEnId("gastos-totales", gestionPresupuesto.calcularTotalGastos());
+gestionPresupuestoWeb.mostrarDatoEnId("balance-total", gestionPresupuesto.calcularBalance());
+
+gestionPresupuesto.listarGastos().forEach(function(gasto) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-completo", gasto);
+});
+
+/* Datos filtrados */
+let filtro1 = { fechaDesde: "2021-08-31", fechaHasta: "2021-10-01", };
+let filtro2 = { valorMinimo: 50 };
+let filtro3 = { valorMinimo: 200, etiquetasTiene: ["seguros"], };
+let filtro4 = { valorMaximo: 50, etiquetasTiene: ["comida", "transporte"], };
+
+let gastosFiltro1 = gestionPresupuesto.filtrarGastos(filtro1);
+let gastosFiltro2 = gestionPresupuesto.filtrarGastos(filtro2);
+let gastosFiltro3 = gestionPresupuesto.filtrarGastos(filtro3);
+let gastosFiltro4 = gestionPresupuesto.filtrarGastos(filtro4);
+
+gastosFiltro1.forEach(function(gasto) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-1", gasto);
+});
+gastosFiltro2.forEach(function(gasto) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-2", gasto);
+})
+;gastosFiltro3.forEach(function(gasto) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-3", gasto);
+});
+gastosFiltro4.forEach(function(gasto) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-4", gasto);
+});
+
+/* Datos agrupados */
+
+gestionPresupuestoWeb.mostrarDatoEnId("agrupacion-dia", gestionPresupuesto.agruparGastos("dia"));
+gestionPresupuestoWeb.mostrarDatoEnId("agrupacion-mes", gestionPresupuesto.agruparGastos());
+gestionPresupuestoWeb.mostrarDatoEnId("agrupacion-anyo", gestionPresupuesto.agruparGastos("anyo"));
