1d587149eaba95744d193aa054731441345eb526
Maria Jose Gomez Pastor <gomez20740@iesmarenostrum.com>
2021-12-01 11:33:37 +0100

    test1 regexp

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -237,6 +237,11 @@ function agruparGastos(periodo = "mes", etiquetas = [], fechaDesde = "", fechaHa
     }, {});
 }
 
+function transformarListadoEtiquetas(listaEtiquetas) {
+    let expRegular = /[\;\:\.\,\s]+/g;
+    return listaEtiquetas.split(expRegular);
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -251,4 +256,5 @@ export   {
     calcularBalance,
     filtrarGastos,
     agruparGastos,
+    transformarListadoEtiquetas,
 }
