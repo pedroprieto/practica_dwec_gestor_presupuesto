61fb63488e1061cd92d4330dc53aa3bfaf763a60
Maria Jose Gomez Pastor <gomez20740@iesmarenostrum.com>
2021-12-01 13:08:08 +0100

    Creada función cargarGastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -242,6 +242,10 @@ function transformarListadoEtiquetas(listaEtiquetas) {
     return listaEtiquetas.split(expRegular);
 }
 
+function cargarGastos(gastos) {
+    gastos = gastos;
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -257,4 +261,5 @@ export   {
     filtrarGastos,
     agruparGastos,
     transformarListadoEtiquetas,
+    cargarGastos,
 }
