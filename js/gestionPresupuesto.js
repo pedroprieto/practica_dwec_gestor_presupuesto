e264b762fc6d55eec874ba28b0464f02722bd788
Maria Jose Gomez Pastor <gomez20740@iesmarenostrum.com>
2022-02-06 16:23:51 +0100

    Corrijo bien la rehidratación de los gastos ya que tenía comportamiento anómalo al interacturar con la api.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -242,11 +242,29 @@ function transformarListadoEtiquetas(listaEtiquetas) {
     return listaEtiquetas.split(expRegular);
 }
 
-function cargarGastos(nuevosGastos) {
-    for (const gasto of nuevosGastos) {
-        let nuevoGasto = new CrearGasto(gasto.descripcion, gasto.valor, gasto.fecha, gasto.etiquetas);
-
-        gastos.push(nuevoGasto);
+function cargarGastos(gastosAlmacenamiento) {
+    // gastosAlmacenamiento es un array de objetos "planos"
+    // No tienen acceso a los métodos creados con "CrearGasto":
+    // "anyadirEtiquetas", "actualizarValor",...
+    // Solo tienen guardadas sus propiedades: descripcion, valor, fecha y etiquetas
+  
+    // Reseteamos la variable global "gastos"
+    gastos = [];
+    // Procesamos cada gasto del listado pasado a la función
+    for (let g of gastosAlmacenamiento) {
+        // Creamos un nuevo objeto mediante el constructor
+        // Este objeto tiene acceso a los métodos "anyadirEtiquetas", "actualizarValor",...
+        // Pero sus propiedades (descripcion, valor, fecha y etiquetas) están sin asignar
+        let gastoRehidratado = new CrearGasto();
+        // Copiamos los datos del objeto guardado en el almacenamiento
+        // al gasto rehidratado
+        // https://es.javascript.info/object-copy#cloning-and-merging-object-assign
+        Object.assign(gastoRehidratado, g);
+        // Ahora "gastoRehidratado" tiene las propiedades del gasto
+        // almacenado y además tiene acceso a los métodos de "CrearGasto"
+          
+        // Añadimos el gasto rehidratado a "gastos"
+        gastos.push(gastoRehidratado)
     }
 }
 
