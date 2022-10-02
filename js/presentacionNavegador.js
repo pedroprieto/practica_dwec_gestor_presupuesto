4f8d9e63837e7b9fcf9865d28425cc179ab0e780
Luis Gilabert <luigilbla@alu.edu.gva.es>
2022-10-02 16:02:14 +0200

    cambios preparación

--- js/presentacionNavegador.js
+++
@@ -1,3 +0,0 @@
-let nombre="Luis";
-let apellidos="Gilabert Blanes"
-alert(`${nombre} ${apellidos}`);
--- js/presentacionNode.js
+++
@@ -1,3 +0,0 @@
-let nombre="Luis";
-let apellidos="Gilabert Blanes"
-console.log(`${nombre} ${apellidos}`);
