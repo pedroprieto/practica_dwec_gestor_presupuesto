71ff7c3117ed532ef28ce12fc0977f10c91425ef
Luis Rodes <luisjrodes@gmail.com>
2022-10-03 21:25:20 +0200

    Primeras Lineas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,50 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
+"use strict";
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(actual) {
     // TODO
+    if (actual >= 0) {
+        presupuesto = actual;
+        return presupuesto;
+    }
+    else {
+        return "El valor no es correcto -1";
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    let mensaje;
+    mensaje = `Tú presupuesto actual es de ${presupuesto} €`
+    return mensaje;
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion, valor) {
     // TODO
+    // Propiedades del objeto
+    this.descrpcion = descripcion;
+    this.valor = valor;
+    if ( valor >= 0) {
+        valor = valor;
+    }
+    else {
+        valor = 0;
+    }
+    this.mostrarGasto = function() {
+        let mensaje;
+        mensaje = `Gasto correspondiente a ${this.descrpcion} con valor ${this.valor} €`;
+        return mensaje;
+    }
+    this.actualizarDescripcion = function(actualizaDescripcion) {
+        this.descrpcion = actualizaDescripcion;
+    }
+    this.actualizarValor = function(nuevoValor) {
+        if( nuevoValor >= 0 && nuevoValor == Number) {
+            this.valor = nuevoValor;
+        }
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
