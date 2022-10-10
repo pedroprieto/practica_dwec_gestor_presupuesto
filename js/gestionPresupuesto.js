a9572abb20a9e95ccd047f7d30bcd302a186f647
Claudia Dielma <noedielma@gmail.com>
2022-10-10 17:02:52 +0200

    Cambios en las 4 primeras funciones

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,63 @@
+   
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    // TODO 
+    if(valor < 0 || isNaN (valor)) {
+        return -1;
+    }else{
+      return presupuesto = valor;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return "Tu presupuesto actual es de " +""+ presupuesto + " " + "€";
 }
+    
+  function CrearGasto(descripcion, valor) {
+    //TODO
+    this.descripcion = descripcion;
+    this.valor = valor;
+    this.valor = valor < 0 || isNaN(this.valor) ? 0: this.valor;
+    
+  }
+  
+   var gasto1 = new CrearGasto( "Gasto", 70);
+   
+    
+    function mostrarGasto(){
+      return `Gasto correspondiente a ${gasto1.descripcion} con valor ${gasto1.valor} €`;
+    }
+    console.log(mostrarGasto( ));
+    
+    
+     
+    
+ 
+    
+    function actualizarDescripcion(d){
+      
+      
+  }
+ 
 
-function CrearGasto() {
-    // TODO
-}
+    
+  function actualizarValor(v){
+    
+
+  }
+  
+ 
+   
+  
+
+
+ 
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
