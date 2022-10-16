839119e5ef39016fdbe7454188a7e8db17fc7865
Cintia Castillo <cincascar@alu.edu.gva.es>
2022-10-16 08:19:49 +0200

    Función calcular el total de los gastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -103,6 +103,16 @@ function CrearGasto( descripcion, valor, fecha, ...etiquetas ) {
         }
     }
 
+    //Función calcularTotalGastos
+    function calcularTotalGastos (){
+        let total = 0;
+
+        for (let g of gastos){
+            total += g.valor;
+        }
+        return total;
+    }
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
