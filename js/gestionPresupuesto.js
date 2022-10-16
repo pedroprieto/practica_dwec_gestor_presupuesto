65d43d22d9a65a3631e541fef8fced492d509d57
Cintia Castillo <cincascar@alu.edu.gva.es>
2022-10-16 08:03:46 +0200

    Función borrar gasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -94,6 +94,15 @@ function CrearGasto( descripcion, valor, fecha, ...etiquetas ) {
         gastos.push(gasto);
     }
 
+    //Función borrarGasto
+    function borrarGasto(id){
+        let pos = gastos.findIndex(gasto => gasto.id === id);
+
+        if(pos !=-1){
+            gastos.splice(pos,1);
+        }
+    }
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
