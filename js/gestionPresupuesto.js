8562a33948783b886227f5d16467d5b821e9c738
paugargom2 <paugargom2@alu.edu.gva.es>
2022-10-17 11:42:23 +0200

    Creamos la función anyadirGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -27,8 +27,10 @@ function listarGastos() {
     return (gastos);
 }
 
-function anyadirGasto() {
-
+function anyadirGasto(gasto) {
+    gasto.id = idGasto;
+    idGasto ++;
+    gastos.push(gasto);
 }
 
 function borrarGasto() {
