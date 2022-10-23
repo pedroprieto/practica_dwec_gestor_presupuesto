b535cf23c8d9acee7535c52a086505a194923c40
Jose Francisco Pérez Castillo <jospercas2@alu.edu.gva.es>
2022-10-23 23:43:08 +0200

    En los commit anteriores se me pasó realizar las pruebas. Me he dado cuenta al subirlo. Corrijo los fallos que me daba en el envío anterior

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,7 +4,7 @@
 
 //Variable global presupuesto
 let presupuesto = 0;
-let gasto = {}; 
+let gasto = new CrearGasto; 
 
 function actualizarPresupuesto(nuevoPresupuesto) {
     if(nuevoPresupuesto >= 0){
@@ -21,23 +21,27 @@ function mostrarPresupuesto() {
     return 'Tu presupuesto actual es de ' + presupuesto + " €";
 }
 
-function CrearGasto(descripcion, valor) {
-    //Objeto gasto
-    gasto = {
-        descripcion: descripcion,
-        valor: valor,
-        mostrarGasto: function(){
-            console.log ("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor +" €");
-        },
-        actualizarDescripcion: function(nuevaDescripcion){
-            gasto.descripcion = nuevaDescripcion;
-        },
-        actualizarValor: function(nuevoValor){
-            if(nuevoValor>=0){
-                gasto.valor = nuevoValor;
-            }
-        },        
+//Función constructora
+function CrearGasto(descripcionGasto, valorGasto){
+
+    this.descripcion = descripcionGasto;
+    this.valorUsar = isNaN(valorGasto)?0:valorGasto;
+    this.valor = this.valorUsar>=0?this.valorUsar:0;
+    
+    
+    this.mostrarGasto = function (){
+        return ("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor +" €");
     }
+
+    this.actualizarDescripcion = function (descripcionGasto){
+        this.descripcion = descripcionGasto;
+    }
+    
+    this.actualizarValor = function (valorGasto){
+        if(valorGasto >= 0){
+            this.valor = valorGasto;
+        }    
+    } 
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
