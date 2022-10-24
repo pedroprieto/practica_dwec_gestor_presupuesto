ae9d7b873de296d208520f05801a57f4e5f41a2a
Desiree <desmoyper@alu.edu.gvs.es>
2022-10-24 14:10:09 +0200

    Practica terminada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -22,7 +22,7 @@ function mostrarPresupuesto() {
 function CrearGasto(descripcion, valor) {
     this.descripcion = descripcion;
     if (valor < 0){
-        valor = 0;
+        this.valor = 0;
     }
     else{
         this.valor = valor;
@@ -35,11 +35,16 @@ function CrearGasto(descripcion, valor) {
         return this.descripcion;
     }
     this.actualizarValor = function(valor){
-        if (valor => 0){
-            this.valor = valor;
-       return this.valor;
-    } 
+        if (valor < 0){
+            this.valor = 0;
+        }
+        else{
+            return this.valor;
+        } 
+    }
 }
+
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
