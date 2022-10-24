90ede4da2a339b4b5a6c2f2983484d447e19ee0a
Desiree <desmoyper@alu.edu.gvs.es>
2022-10-24 13:36:35 +0200

    Funcion CrearGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -16,13 +16,30 @@ function actualizarPresupuesto(cantidad) {
 }
 
 function mostrarPresupuesto() {
-   return `Tu presupuesto actual es  de ${presupuesto} €´ ;
+   return `Tu presupuesto actual es  de ${presupuesto} €` ;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion;
+    if (valor < 0){
+        valor = 0;
+    }
+    else{
+        this.valor = valor;
+    }
+    this.mostrarGasto = function(){
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    }
+    this.actualizarDescripcion = function(descripcion){
+        this.descripcion = descripcion;
+        return this.descripcion;
+    }
+    this.actualizarValor = function(valor){
+        if (valor => 0){
+            this.valor = valor;
+       return this.valor;
+    } 
 }
-
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
