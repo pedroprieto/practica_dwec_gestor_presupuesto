2f914ece3ced08a682eb6a74eddc27e68e7414b3
Luis Gilabert <luigilbla@alu.edu.gva.es>
2022-10-25 17:16:57 +0200

    Interaccion html v1 master

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,14 @@
+
+function mostrarDatoEnId(idElemento, valor){
+
+}
+
+function mostrarGastoWeb(idElemento, objgasto){
+  
+}
+
+function mostrarGastosAgrupadosWeb(idElemento, objagrup, periodo){
+  
+}
+
+export {mostrarDatoEnId, mostrarGastoWeb, mostrarGastosAgrupadosWeb};
