80724ac3a13f0a7504bc9a79730ed2ae788514b5
Daniela Amoasii Marin <danamo2@alu.edu.gva.es>
2022-10-25 18:23:07 +0200

    Añadir fecha y etiquetas al objeto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,6 +1,9 @@
+// Variables Globales
 let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
-// FUNCIONES
+// FUNCIONES PRACTICA JAVASCRIPT i
 function actualizarPresupuesto(cantidad) {
     if (cantidad >= 0){
         return presupuesto = cantidad;
@@ -14,16 +17,36 @@ function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €`;    
 }
 
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
+    //descripcion
     this.descripcion = descripcion;
+    //valor
     if (valor < 0 || isNaN(valor)){
         this.valor = 0;
     } else{
         this.valor = valor;
     }
 
+    // fecha
+    fecha = Date.parse(fecha);
+
+    if (fecha == null || isNaN(fecha)){
+        this.fecha = +new Date(); // se pasa a timestamp con el + delante
+    } else{
+        this.fecha = fecha; // getTime() sirve para pasar la fecha a formato timestamp
+    }
+
+    //etiqueta
+    etiquetas = anyadirEtiquetas(...etiquetas);
+    if(etiquetas == null){
+        this.etiquetas = [];
+    }else{
+        this.etiquetas = anyadirEtiquetas(...etiquetas);
+    }
+    
+
     this.mostrarGasto = function(){
-        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} € a fecha de ${this.fecha} con las etiquetas:${this.etiquetas}` ;
     }
 
     this.actualizarDescripcion = function (descripcion){
@@ -38,8 +61,60 @@ function CrearGasto(descripcion, valor) {
         return this.valor;
     }
 }
-var gasto = new CrearGasto("Alquiler", "hi");
-console.log(gasto.mostrarGasto());
+
+
+// Crear una lista con etiquetas sin repeticiones 
+function anyadirEtiquetas(...theArgs){
+    var lista = [];
+    for (const arg of theArgs){
+        // comprobar si existe en la lista, si no existe, añadirlo
+        if (lista.includes(arg) == false){
+            lista.push(arg);
+        }
+    }
+    return lista;
+}
+
+// FUNCIONES JAVASCRIPT II
+function listarGastos(){
+
+    return gastos;
+}
+
+function anyadirGasto(gasto){
+    var id = idGasto;
+    idGasto++;
+
+    var lista = [id, gasto];
+    gastos.push(lista);
+    
+}
+// comprobar si funciona
+//var gasto = new CrearGasto("Alquiler", 300, "2021-10-06T13:10", "casa", "libro", "casa");
+//anyadirGasto(gasto);
+//console.log(gasto.mostrarGasto());
+//console.log(listarGastos());
+
+//var gasto1 = new CrearGasto("Cine", 15, "2022-10-06T13:10", "SpiderMan");
+//anyadirGasto(gasto1);
+//console.log(gasto.mostrarGasto());
+//console.log(listarGastos());
+
+
+
+
+function borrarGasto(){
+
+}
+
+function calcularTotalGastos(){
+
+}
+
+function calcularBalance(){
+    
+}
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -47,5 +122,10 @@ console.log(gasto.mostrarGasto());
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos, 
+    calcularBalance
 }
