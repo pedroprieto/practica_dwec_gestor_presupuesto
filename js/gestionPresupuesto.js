01cb50078fbc154bcb706510ca38718adee45caf
Guillermina Pinarreta <guillermina888@gmail.com>
2022-10-26 20:17:59 +0200

    Crear variables gastos y idGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,6 +1,8 @@
 "use strict"
 
 let presupuesto = 0; 
+let gastos = [];
+idGasto = 0;
 
 function actualizarPresupuesto(par1) {
     if (par1 >= 0) {
@@ -38,6 +40,7 @@ function CrearGasto(descrip, val) {
     }
 }
 
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -45,4 +48,9 @@ export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
+    listarGastos
+    anyadirGasto
+    borrarGasto
+    calcularTotalGastos
+    calcularBalance
 }
