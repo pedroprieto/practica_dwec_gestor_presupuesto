6a0f9ea42c19add66f55c580a4382192ad2ad972
Yolanda Aparicio Gimenez <yolicolegio2022@outlook.es>
2022-10-26 10:40:27 +0200

    Todos los test me pasan todas las funciones, Tarea completada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -8,79 +8,117 @@ let gastos = [];
 let idGasto = 0;
 
 function actualizarPresupuesto(nuevoValor) {
-    if( nuevoValor >= 1){
+    if (nuevoValor >= 1) {
 
         presupuesto = nuevoValor
         return presupuesto
 
-    }else{
+    } else {
 
         console.log("El valor es negativo")
         return -1
     }
-    
+
 }
 
 function mostrarPresupuesto() {
-  // TODO
-  let text = `Tu presupuesto actual es de ${presupuesto} €`
-  return text
+    // TODO
+    let text = `Tu presupuesto actual es de ${presupuesto} €`
+    return text
 
 }
-function CrearGasto(descripcion, valor, fecha, etiquetas) {
+function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
 
     this.descripcion = descripcion;
 
-    if(valor >= 0){
+    if (valor >= 0) {
         this.valor = valor;
-    }else{
+    } else {
         this.valor = 0
     }
 
     let dataFecha = Date.parse(fecha)
-    
-    if(!dataFecha){
+
+    if (!dataFecha) {
 
         this.fecha = Date.parse(new Date())
-        
-    }else{
-    
+
+    } else {
+
         this.fecha = dataFecha;
     }
 
-    if(!etiquetas){
+    if (!etiquetas) {
         this.etiquetas = [];
-    }else{
+    } else {
         this.etiquetas = etiquetas;
     }
 
 
-    this.mostrarGasto = function() {  
-        return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
+    this.mostrarGasto = function () {
+        return (`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
     }
 
-    this.actualizarDescripcion = function(descripcion) {
+    this.mostrarGastoCompleto = function () {
+     
+        let fgasto=`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.\n` +`Fecha: ${new Date(this.fecha).toLocaleString()}\n`+"Etiquetas:\n"
+        
+        for (let i=0; i<this.etiquetas.length; i++){
+          fgasto +="- " + this.etiquetas[i] +  "\n";
+        }
+        return fgasto;
+    }
+
+    this.actualizarDescripcion = function (descripcion) {
         this.descripcion = descripcion;
     }
 
-    this.actualizarValor = function(valor) {
-       if(valor >= 0){
+    this.actualizarValor = function (valor) {
+        if (valor >= 0) {
             this.valor = valor;
-       }
+        }
+    }
+    this.actualizarFecha = function (fecha) {
+
+        let dataFecha = Date.parse(fecha)
+
+        if (dataFecha) {
+            this.fecha = dataFecha;
+        }
+    }
+    this.anyadirEtiquetas = function (...etiquetas) {
+        for (let i = 0; i < etiquetas.length; i++){
+
+            let etiqueta = etiquetas[i];
+
+            if(this.etiquetas.indexOf(etiqueta) === -1){
+                
+                this.etiquetas.push(etiqueta)
+            }
+        }
+    }
+    this.borrarEtiquetas = function (...etiquetas) {
+        for (let i = 0; i < etiquetas.length; i++){
+
+            let etiqueta = etiquetas[i];
+            let posicion = this.etiquetas.indexOf(etiqueta);
+
+            if (posicion >= 0) {
+                this.etiquetas.splice(posicion, 1);
+            }
+        }
     }
 
- 
- 
+
 }
 
 
 
-function listarGastos()
-{
+function listarGastos() {
     return gastos
 }
 
-function anyadirGasto(gasto){
+function anyadirGasto(gasto) {
     gasto.id = idGasto;
     idGasto += 1;
     gastos.push(gasto);
@@ -88,34 +126,35 @@ function anyadirGasto(gasto){
 
 
 
-function borrarGasto(id){
+function borrarGasto(id) {
 
-    for(let i = 0; i < gastos.length; i++){
-        
-        if(gastos[i].id == id){
+    for (let i = 0; i < gastos.length; i++) {
+
+        if (gastos[i].id == id) {
             gastos.splice(i, 1)
         }
     }
 }
-function calcularTotalGastos(){
+
+function calcularTotalGastos() {
     let sumaGastos = 0;
 
-    for(let i = 0; i < gastos.length; i++){
-           sumaGastos = sumaGastos + gastos[i].valor
+    for (let i = 0; i < gastos.length; i++) {
+        sumaGastos = sumaGastos + gastos[i].valor
     }
     return sumaGastos
 }
-function calcularBalance(){
-    let gastostotales  = calcularTotalGastos()
+
+function calcularBalance() {
+    let gastostotales = calcularTotalGastos()
     let balance = presupuesto - gastostotales;
-    console.log(balance)
     return balance
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto,
@@ -129,22 +168,22 @@ export   {
 
 
 
-// Ejecuciones
-let gasto1 = new CrearGasto("Gasto 1");
-let gasto2 = new CrearGasto("Gasto 2", 23.55);
-let gasto3 = new CrearGasto("Gasto 3", 23.55, "2021-10-06T13:10" );
-let gasto4 = new CrearGasto("Gasto 4", 23.55, "2021-10-06T13:10", "casa" );
-let gasto5 = new CrearGasto("Gasto 5", 23.55, "2021-10-06T13:10", "casa", "supermercado" );
-let gasto6 = new CrearGasto("Gasto 6", 23.55, "2021-10-06T13:10", "casa", "supermercado", "comida" );
+// // Ejecuciones
+// let gasto1 = new CrearGasto("Gasto 1");
+// let gasto2 = new CrearGasto("Gasto 2", 23.55);
+// let gasto3 = new CrearGasto("Gasto 3", 23.55, "2021-10-06T13:10" );
+// let gasto4 = new CrearGasto("Gasto 4", 23.55, "2021-10-06T13:10", "casa" );
+// let gasto5 = new CrearGasto("Gasto 5", 23.55, "2021-10-06T13:10", "casa", "supermercado" );
+// let gasto6 = new CrearGasto("Gasto 6", 23.55, "2021-10-06T13:10", "casa", "supermercado", "comida" );
 
-anyadirGasto(gasto1)
-anyadirGasto(gasto2)
-anyadirGasto(gasto3)
-anyadirGasto(gasto4)
-anyadirGasto(gasto5)
-anyadirGasto(gasto6)
+// anyadirGasto(gasto1)
+// anyadirGasto(gasto2)
+// anyadirGasto(gasto3)
+// anyadirGasto(gasto4)
+// anyadirGasto(gasto5)
+// anyadirGasto(gasto6)
 
 
-calcularBalance()
+// calcularBalance()
 
-console.log("holka")
+// console.log()
