f864712722f292acbb442d442a3d03c931be0b67
Agustin Perez <agus.perez.quintana@gmail.com>
2022-10-26 21:54:50 +0200

    Actualizacion gestionPresupuesto.js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,25 +1,50 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
+let presupuesto = 0;
 // TODO: Variable global
+ 
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(dinero) {
+    if (dinero >= 0){
+        presupuesto += dinero;
+        alert(presupuesto);
+    }
+    else {
+        alert(-1);
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    alert(`Tu presupuesto actual es ${presupuesto}€`)
 }
 
 function CrearGasto() {
-    // TODO
+    let gasto = {
+        descripcion : "",
+        valor : 0
+    }
+
+    function mostrarGasto(){
+        alert(`Gasto: ${descripcion}\n
+        Coste: ${valor}`)
+    }
+
+    function actualizarDescripccion(nuevaDesc){
+        nuevaDesc = prompt("Introduzca la nueva descripccion")
+        descripcion = nuevaDesc;
+    }
+
+    function actualizarValor(nuevoVal){
+        nuevoVal = prompt("Introduza el nuevo valor")
+        valor = parseInt(nuevoVal);
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+/*export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
 }
+*/
