d937ae2f881d4b1a01cfba04d8a85a6ab8ef5628
Desiree <desmoyper@alu.edu.gva.es>
2022-10-30 13:32:23 +0100

    He corregido unas cosas despues de ver la tutoria

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,11 +1,11 @@
 let presupuesto = 0;
 
-function actualizarPresupuesto(cantidad){
-    if (cantidad >= 0){
-        return presupuesto = cantidad;
-    } else{
+function actualizarPresupuesto(valor){
+    if (valor < 0 || isNaN(valor)){
         return -1;
-        console.log("Presupuesto inferior a 0");
+       
+    } else{
+        return presupuesto = valor;
     }
 }
 
@@ -15,12 +15,8 @@ function mostrarPresupuesto() {
 
 function CrearGasto(descripcion, valor) {
     this.descripcion = descripcion;
-    if (valor < 0 || isNaN(valor)){
-        this.valor = 0;
-    }
-    else{
-        this.valor = valor;
-    }
+    this.valor = valor;
+    this.valor = valor < 0 || isNaN(this.valor) ? 0: this.valor; 
     this.mostrarGasto = function(){
         return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
     }
@@ -35,8 +31,6 @@ function CrearGasto(descripcion, valor) {
         return this.valor;
     }
 }
-
-
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
