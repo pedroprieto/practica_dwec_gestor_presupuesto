8869fa8b0cf7a6155bed324ca967d6e9208ce4e2
Luis Rodes <luirodroc@alu.edu.gva.es>
2022-11-02 13:05:36 +0100

    funciones vacias

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,24 @@
+
+
+//Funciones
+function mostrarDatoEnId (IdElemento, valor) {
+
+}
+
+function mostrarGastoWeb (IdElemento, gasto) {
+
+}
+
+function mostrarGastosAgrupadosWeb ( IdElemento, agrup, periodo) {
+    
+}
+
+
+
+
+// Exportar las funciones creadas
+export {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
