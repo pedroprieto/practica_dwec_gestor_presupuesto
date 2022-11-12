ec213f40d685f58210110325362bea3023d482bc
Cintia Castillo <cincascar@alu.edu.gva.es>
2022-11-12 13:26:03 +0100

    Función mostrar gastos web empezada

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -7,8 +7,42 @@ function mostrarDatoEnId(idElemento, valor){
     mostrarGasto = `${valor}`;
 }
 
-function mostrarGastoWeb(){
+//función mostrarGastoWeb
+function mostrarGastoWeb(idElemento, gasto){
 
+    let body = document.getElementById(idElemento);
+
+//<div class="gasto">
+    let divGasto = document.createElement ('div');
+    divGasto.className = "gasto";
+
+  //<div class="gasto-descripcion">DESCRIPCIÓN DEL GASTO</div>
+    let divDescripcion = document.createElement('div');
+    divDescripcion.className = "gasto-descripcion";
+    divDescripcion.innerHTML = `${gasto.descripcion}`;
+
+  //<div class="gasto-fecha">FECHA DEL GASTO</div> 
+    let divFecha = document.createElement('div');
+    divFecha.className = "gasto-fecha";
+    divFecha.innerHTML = `${gasto.fecha}`;
+
+  //<div class="gasto-valor">VALOR DEL GASTO</div> 
+    let divValor = document.createElement('div');
+    divValor.className = "gasto-valor";
+    divValor.innerHTML = `${gasto.valor}`;
+
+  //<div class="gasto-etiquetas">
+    let divEtiquetas = document.createElement('div');
+    divEtiquetas.className = "gasto-etiquetas";
+    //<span class="gasto-etiquetas-etiqueta">
+      //ETIQUETA 1
+    //</span>
+    //<span class="gasto-etiquetas-etiqueta">
+      //ETIQUETA 2
+    //</span>
+    //<!-- Etcétera -->
+  //</div> 
+//</div>
 }
 
 function mostrarGastosAgrupadosWeb(){
