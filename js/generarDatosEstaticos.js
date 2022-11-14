c4d82c56206c8350facdf9751ad56e813d76cd9c
Cintia Castillo <cincascar@alu.edu.gva.es>
2022-11-14 21:00:35 +0100

    actualizar y mostrar presupuesto

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,3 +1,9 @@
 //Importar librerías
 import * as gestionPresupuestoWeb from './gestionPresupuestoWeb';
 import * as gestionPresupuesto from './gestionPresupuesto';
+
+// Actualizar el presupuesto a 1500€
+gestionPresupuesto.actualizarPresupuesto( 1500 );
+
+// Mostrar el presupuesto en el div#presupuesto
+gestionPresupuestoWeb.mostrarDatoEnId( "presupuesto", gestionPresupuesto.mostrarPresupuesto() );
