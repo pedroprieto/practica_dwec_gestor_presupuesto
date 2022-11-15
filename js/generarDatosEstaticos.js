afa18473b43e4110c4789c0115f75b4a3c2a52e9
Cintia Castillo <cincascar@alu.edu.gva.es>
2022-11-15 20:27:57 +0100

    generar datos estáticos

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,70 +1,70 @@
 //Importar librerías
-import * as gestionPresupuestoWeb from './gestionPresupuestoWeb';
-import * as gestionPresupuesto from './gestionPresupuesto';
+import * as gestionPresWeb from './gestionPresupuestoWeb';
+import * as gestionPre from './gestionPresupuesto';
 
 // Actualizar el presupuesto a 1500€
-gestionPresupuesto.actualizarPresupuesto( 1500 );
+gestionPre.actualizarPresupuesto( 1500 );
 
 // Mostrar el presupuesto en el div#presupuesto
-gestionPresupuestoWeb.mostrarDatoEnId( "presupuesto", gestionPresupuesto.mostrarPresupuesto() );
+gestionPresWeb.mostrarDatoEnId( "presupuesto", gestionPre.mostrarPresupuesto() );
 
 // Crear los siguientes gastos
-let gasto1 = new gestionPresupuesto.CrearGasto( "Compra carne", 23.44, "2021-10-06", "casa", "comida" );
-let gasto2 = new gestionPresupuesto.CrearGasto( "Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida" );
-let gasto3 = new gestionPresupuesto.CrearGasto( "Bonobús", 18.60, "2020-05-26", "transporte" );
-let gasto4 = new gestionPresupuesto.CrearGasto( "Gasolina", 60.42, "2021-10-08", "transporte", "gasolina" );
-let gasto5 = new gestionPresupuesto.CrearGasto( "Seguro hogar", 206.45, "2021-09-26", "casa", "seguros" );
-let gasto6 = new gestionPresupuesto.CrearGasto( "Seguro coche", 195.78, "2021-10-06", "transporte", "seguros" );
+let gasto1 = new gestionPre.CrearGasto( "Compra carne", 23.44, "2021-10-06", "casa", "comida" );
+let gasto2 = new gestionPre.CrearGasto( "Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida" );
+let gasto3 = new gestionPre.CrearGasto( "Bonobús", 18.60, "2020-05-26", "transporte" );
+let gasto4 = new gestionPre.CrearGasto( "Gasolina", 60.42, "2021-10-08", "transporte", "gasolina" );
+let gasto5 = new gestionPre.CrearGasto( "Seguro hogar", 206.45, "2021-09-26", "casa", "seguros" );
+let gasto6 = new gestionPre.CrearGasto( "Seguro coche", 195.78, "2021-10-06", "transporte", "seguros" );
 
 // Añadir los gastos creados
-gestionPresupuesto.anyadirGasto( gasto1 );
-gestionPresupuesto.anyadirGasto( gasto2 );
-gestionPresupuesto.anyadirGasto( gasto3 );
-gestionPresupuesto.anyadirGasto( gasto4 );
-gestionPresupuesto.anyadirGasto( gasto5 );
-gestionPresupuesto.anyadirGasto( gasto6 );
+gestionPre.anyadirGasto( gasto1 );
+gestionPre.anyadirGasto( gasto2 );
+gestionPre.anyadirGasto( gasto3 );
+gestionPre.anyadirGasto( gasto4 );
+gestionPre.anyadirGasto( gasto5 );
+gestionPre.anyadirGasto( gasto6 );
 
 // Mostrar los gastos totales en div#gastos-totales
-gestionPresupuestoWeb.mostrarDatoEnId( "gastos-totales", gestionPresupuesto.calcularTotalGastos() );
+gestionPresWeb.mostrarDatoEnId( "gastos-totales", gestionPre.calcularTotalGastos() );
 
 // Mostrar el balance total en div#balance-total
-gestionPresupuestoWeb.mostrarDatoEnId( "balance-total", gestionPresupuesto.calcularBalance() );
+gestionPresWeb.mostrarDatoEnId( "balance-total", gestionPre.calcularBalance() );
 
 // Mostrar el listado completo de gastos en div#listado-gastos-completo
-let gastos = gestionPresupuesto.listarGastos();
+let gastos = gestionPre.listarGastos();
 for ( let gasto of gastos ){
-    gestionPresupuestoWeb.mostrarGastoWeb( "listado-gastos-completo", gasto );
+    gestionPresWeb.mostrarGastoWeb( "listado-gastos-completo", gasto );
 }
 
 // Mostrar el listado de gastos realizados en septiembre de 2021 en div#listado-gastos-filtrado-1
-let gastosFiltrados = gestionPresupuesto.filtrarGastos( {fechaDesde:"2021-09-01", fechaHasta:"2021-09-30"} );
+let gastosFiltrados = gestionPre.filtrarGastos( {fechaDesde:"2021-09-01", fechaHasta:"2021-09-30"} );
 
 // Mostrar el listado de gastos de más de 50€ en div#listado-gastos-filtrado-2
-gastosFiltrados = gestionPresupuesto.filtrarGastos( {valorMinimo:50} );
+gastosFiltrados = gestionPre.filtrarGastos( {valorMinimo:50} );
 for ( let gasto of gastosFiltrados ){
-    gestionPresupuestoWeb.mostrarGastoWeb( "listado-gastos-filtrado-2", gasto );
+    gestionPresWeb.mostrarGastoWeb( "listado-gastos-filtrado-2", gasto );
 }
 
 //Mostrar el listado de gastos de más de 200€ con etiqueta seguros en div#listado-gastos-filtrado-3
-gastosFiltrados = gestionPresupuesto.filtrarGastos( {valorMinimo:200, etiquetas:"seguros"} );
+gastosFiltrados = gestionPre.filtrarGastos( {valorMinimo:200, etiquetas:"seguros"} );
 for ( let gasto of gastosFiltrados ){
-    gestionPresupuestoWeb.mostrarGastoWeb( "listado-gastos-filtrado-3", gasto);
+    gestionPresWeb.mostrarGastoWeb( "listado-gastos-filtrado-3", gasto);
 }
 
 //Mostrar el listado de gastos que tengan las etiquetas comida o transporte de menos de 50€ en div#listado-gastos-filtrado-4
-gastosFiltrados = gestionPresupuesto.filtrarGastos( {etiquetas:"comida, transporte", valorMaximo:50} );
+gastosFiltrados = gestionPre.filtrarGastos( {etiquetas:"comida, transporte", valorMaximo:50} );
 for ( let gasto of gastosFiltrados ){
-    gestionPresupuesto.mostrarGastoWeb( "listado-gastos-filtrado-4", gasto );
+    gestionPre.mostrarGastoWeb( "listado-gastos-filtrado-4", gasto );
 }
 
 //Mostrar el total de gastos agrupados por día en div#agrupacion-dia
 gastosFiltrados.agruparGastos( "dia");
-gestionPresupuestoWeb.mostrarGastosAgrupadosWeb( "agrupacion-dia", gastosFiltrados, "día" );
+gestionPresWeb.mostrarGastosAgrupadosWeb( "agrupacion-dia", gastosFiltrados, "día" );
 
 //Mostrar el total de gastos agrupados por mes en div#agrupacion-mes
 gastosFiltrados.agruparGastos( "mes");
-gestionPresupuestoWeb.mostrarGastosAgrupadosWeb( "agrupacion-mes", gastosFiltrados, "mes" );
+gestionPresWeb.mostrarGastosAgrupadosWeb( "agrupacion-mes", gastosFiltrados, "mes" );
 
 //Mostrar el total de gastos agrupados por año en div#agrupacion-anyo 
 gastosFiltrados.agruparGastos( "anyo" );
-gestionPresupuestoWeb.mostrarGastosAgrupadosWeb( "agrupacion-anyo", gastosFiltrados, "año");
+gestionPresWeb.mostrarGastosAgrupadosWeb( "agrupacion-anyo", gastosFiltrados, "año");
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,15 +1,14 @@
-import * as gestionPresupuesto from './gestionPresupuesto';
 
 // función mostrarDatoEnId
 function mostrarDatoEnId(idElemento, valor){
     let mostrarGasto = document.getElementById(idElemento);
-    mostrarGasto.innerHTML = `${valor}`;
+    mostrarGasto.innerText = valor;
 }
 
 //función mostrarGastoWeb
 function mostrarGastoWeb(idElemento, gasto){
 
-    let body = document.getElementById(idElemento);
+    let contenedor = document.getElementById(idElemento);
 
   //<div class="gasto">
     let divGasto = document.createElement('div');
@@ -18,17 +17,17 @@ function mostrarGastoWeb(idElemento, gasto){
     //<div class="gasto-descripcion">DESCRIPCIÓN DEL GASTO</div>
       let divDescripcion = document.createElement('div');
       divDescripcion.className = "gasto-descripcion";
-      divDescripcion.innerHTML = `${gasto.descripcion}`;
+      divDescripcion.innerText = gasto.descripcion;
 
     //<div class="gasto-fecha">FECHA DEL GASTO</div> 
       let divFecha = document.createElement('div');
       divFecha.className = "gasto-fecha";
-      divFecha.innerHTML = `${gasto.fecha}`;
+      divFecha.innerText = gasto.fecha;
 
     //<div class="gasto-valor">VALOR DEL GASTO</div> 
       let divValor = document.createElement('div');
       divValor.className = "gasto-valor";
-      divValor.innerHTML = `${gasto.valor}`;
+      divValor.innerText = gasto.valor;
 
     //<div class="gasto-etiquetas">
       let divEtiquetas = document.createElement('div');
@@ -45,58 +44,40 @@ function mostrarGastoWeb(idElemento, gasto){
         //<span class="gasto-etiquetas-etiqueta">ETIQUETA 2</span>
     
         for (let e of gasto.etiquetas) {
-          let spanEtiqueta = document.createElement("span");
+          let spanEtiqueta = document.createElement('span');
           spanEtiqueta.className = "gasto-etiquetas-etiqueta";
           spanEtiqueta.innerHTML = `${e}`;
 
           divEtiquetas.append(spanEtiqueta);
         }
     //Añado todo al documento
-    body.append(divGasto);
+    contenedor.append(divGasto);
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
-  let mostrarAgrupacion = document.getElementById(idElemento);
 
-  /*<div class="agrupacion">
-  <!-- PERIODO será "mes", "día" o "año" en función de si el parámetro
-       de la función es "mes", "dia" o "anyo" respectivamente -->
-  <h1>Gastos agrupados por PERIODO</h1>*/
-  let divAgrup = document.createElement('div');
-  divAgrup.className = "agrupación";
-  divAgrup.innerHTML = "";
+  
+    var divAgrup = document.getElementById(idElemento);
+    divAgrup.innerHTML = "";
+        
+        let arrayAgrupacion = "";
 
-  let arrayAgrupacion = "";
+        for( let [nombre, valor] of Object.entries( agrup ) ){
+            arrayAgrupacion += `
+                <div class="agrupacion-dato">
+                    <span class="agrupacion-dato-clave">${nombre}</span>
+                    <span class="agrupacion-dato-valor">${valor}</span>
+                </div>
+            `;
+        }
 
-  divAgrup.innerHTML = `
+        divAgrup.innerHTML = `
             <div class="agrupacion">
                 <h1>Gastos agrupados por ${periodo}</h1>
                 ${arrayAgrupacion}
             </div>
         `;
-  
 
-  /*Se deberá crear un div.agrupacion-dato para cada propiedad del objeto agrup:
-       https://es.javascript.info/keys-values-entries#object-keys-values-entries -->
-       
-  <div class="agrupacion-dato">
-    <span class="agrupacion-dato-clave">NOMBRE_PROPIEDAD_OBJETO_AGRUP</span>
-    <span class="agrupacion-dato-valor">VALOR_PROPIEDAD_OBJETO_AGRUP</span>
-  </div>
-
-  <div class="agrupacion-dato">
-    <span class="agrupacion-dato-clave">NOMBRE_PROPIEDAD_OBJETO_AGRUP</span>
-    <span class="agrupacion-dato-valor">VALOR_PROPIEDAD_OBJETO_AGRUP</span>
-  </div>
-</div>*/
-  for( let [nombre, valor] of Object.entries( agrup ) ){
-    arrayAgrupacion += `
-      <div class="agrupacion-dato">
-          <span class="agrupacion-dato-clave">${nombre}</span>
-          <span class="agrupacion-dato-valor">${valor}</span>
-      </div>
-    `;
-  }
 }
 
 export   { 
