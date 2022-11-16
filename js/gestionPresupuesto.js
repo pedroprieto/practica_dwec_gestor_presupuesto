3e35c5fc4cfc0e387df975caefc7fb8e2b3f8e3a
Agustin Perez <agus.perez.quintana@gmail.com>
2022-11-16 19:55:15 +0100

    Cambios finales

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -6,47 +6,48 @@ let presupuesto = 0;
 
 function actualizarPresupuesto(dinero) {
     if (dinero >= 0){
-        presupuesto += dinero;
-        alert(presupuesto);
+        presupuesto = dinero;
+        return presupuesto;
     }
-    else {
-        alert(-1);
-    }
+    else 
+        return -1;
+    
 }
 
 function mostrarPresupuesto() {
-    alert(`Tu presupuesto actual es ${presupuesto}€`);
+    return(`Tu presupuesto actual es ${presupuesto}€`);
 }
 
-function CrearGasto() {
-    let gasto = {
-        descricion: "",
-        valor: 0,
-        mostrarGasto(){
-            `Gasto correspondiente a ${this.descricion} con valor ${this.valor}`
-        },
-        actualizarDescripccion(descripcion){
-            this.descricion = descripcion;        
-        },
-        actualizarValor(valor){
-            if(valor >= 0){
-                this.valor = valor;
-            }
-        }
+function CrearGasto(valor, descricion) {
+    if (valor >= 0)
+        this.valor = valor;
+    else
+        this.valor = 0;
+
+    this.descricion = descricion;
+}
+
+let gasto = {
+    valor: 0,
+    descricion: "",
+    mostrarGasto: function() {
+        return `Gasto correspondiente a ${this.descricion} con valor ${this.valor}`;
+    },
+    actualizarDescripcion: function(descripcion) {
+        this.descricion = descripcion;
+    },
+    actualizarValor(valor) {
+        if (valor >= 0)
+            this.valor = valor;            
     }
+    
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-/*export   {
+export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
 }
-*/
-
-CrearGasto();
-actualizarDescripccion();
-actualizarValor(6);
-mostrarGasto();
