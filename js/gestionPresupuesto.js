69d5632d6fc16c0441ceaffd289a914c62cb0127
Antonio Pico <antpicpin@alu.edu.gva.es>
2022-11-28 00:33:36 +0100

    Constructor del gasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -16,10 +16,24 @@ function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
-}
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion;
+    this.valor = (valor >= 0) ? valor : 0;
+    
+    this.mostrarGasto = function() {
+        return `Gasto correspondiente a ${descripcion} con valor ${valor} €`;
+    };
 
+    this.actualizarDescripcion = function(nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    };
+
+    this.actualizarValor = function(nuevoValor) {
+        if (nuevoValor >= 0) {
+            this.valor = nuevoValor;
+        }
+    };
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
