f8ff2bfdfa3861f5a4ec148ffef2f711dbfceecf
Cintia Castillo <cincascar@alu.edu.gva.es>
2022-12-09 21:33:24 +0100

    Función cargarGastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -256,6 +256,33 @@ function CrearGasto( descripcion, valor, fecha, ...etiquetas ) {
             return etiquetasfiltrado;
         }
 
+        //Función cargarGastos
+        function cargarGastos(gastosAlmacenamiento) {
+            // gastosAlmacenamiento es un array de objetos "planos"
+            // No tienen acceso a los métodos creados con "CrearGasto":
+            // "anyadirEtiquetas", "actualizarValor",...
+            // Solo tienen guardadas sus propiedades: descripcion, valor, fecha y etiquetas
+          
+            // Reseteamos la variable global "gastos"
+            gastos = [];
+            // Procesamos cada gasto del listado pasado a la función
+            for (let g of gastosAlmacenamiento) {
+                // Creamos un nuevo objeto mediante el constructor
+                // Este objeto tiene acceso a los métodos "anyadirEtiquetas", "actualizarValor",...
+                // Pero sus propiedades (descripcion, valor, fecha y etiquetas) están sin asignar
+                let gastoRehidratado = new CrearGasto();
+                // Copiamos los datos del objeto guardado en el almacenamiento
+                // al gasto rehidratado
+                // https://es.javascript.info/object-copy#cloning-and-merging-object-assign
+                Object.assign(gastoRehidratado, g);
+                // Ahora "gastoRehidratado" tiene las propiedades del gasto
+                // almacenado y además tiene acceso a los métodos de "CrearGasto"
+                  
+                // Añadimos el gasto rehidratado a "gastos"
+                gastos.push(gastoRehidratado)
+            }
+        }
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -270,5 +297,6 @@ export   {
     calcularBalance,
     filtrarGastos,
     agruparGastos,
-    transformarListadoEtiquetas
+    transformarListadoEtiquetas,
+    cargarGastos
 }
