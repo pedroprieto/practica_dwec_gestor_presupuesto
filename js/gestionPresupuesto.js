652a25304be666d9c5495de7b31250d02eee68a4
Agustin Perez <agustin.pq@icloud.com>
2023-01-19 10:06:19 +0100

    Comienzo JS III, defino funciones vacias

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -93,6 +93,9 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
     }
 }
 
+
+//JS II
+
 function listarGastos() {
     return gastos;
 }
@@ -128,6 +131,16 @@ function calcularBalance() {
     return balance;
 }
 
+// JS III
+
+function filtrarGastos(){
+
+}
+
+function agruparGastos(){
+    
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -139,5 +152,7 @@ export   {
     anyadirGasto,
     borrarGasto,
     calcularTotalGastos,
-    calcularBalance
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos
 }
