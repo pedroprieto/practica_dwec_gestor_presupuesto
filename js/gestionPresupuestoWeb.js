21ad272d41d368a4fc142f3bb122d002df5e4fe2
Desiree Moya Perez <desmoyper@edu.alu.gva.es>
2023-01-22 18:25:56 +0100

    Funcion mostrarGastosAgrupadosWeb modificada y salen las gráficas

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -91,6 +91,12 @@ function mostrarGastoWeb(idElemento, gasto) {
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+    // Obtener la capa donde se muestran los datos agrupados por el período indicado.
+// Seguramente este código lo tengas ya hecho pero el nombre de la variable sea otro.
+// Puedes reutilizarlo, por supuesto. Si lo haces, recuerda cambiar también el nombre de la variable en el siguiente bloque de código
+var divP = document.getElementById(idElemento);
+// Borrar el contenido de la capa para que no se duplique el contenido al repintar
+divP.innerHTML = "";
     let gastosAgrup = document.getElementById(idElemento);
     let divAgrup = document.createElement("div");
     divAgrup.className = "agrupacion";
@@ -111,6 +117,65 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
         divAgrup.append(divAgrudato);
     }
     gastosAgrup.append(divAgrup);
+    // Estilos
+divP.style.width = "33%";
+divP.style.display = "inline-block";
+// Crear elemento <canvas> necesario para crear la gráfica
+// https://www.chartjs.org/docs/latest/getting-started/
+let chart = document.createElement("canvas");
+// Variable para indicar a la gráfica el período temporal del eje X
+// En función de la variable "periodo" se creará la variable "unit" (anyo -> year; mes -> month; dia -> day)
+let unit = "";
+switch (periodo) {
+case "anyo":
+    unit = "year";
+    break;
+case "mes":
+    unit = "month";
+    break;
+case "dia":
+default:
+    unit = "day";
+    break;
+}
+
+// Creación de la gráfica
+// La función "Chart" está disponible porque hemos incluido las etiquetas <script> correspondientes en el fichero HTML
+const myChart = new Chart(chart.getContext("2d"), {
+    // Tipo de gráfica: barras. Puedes cambiar el tipo si quieres hacer pruebas: https://www.chartjs.org/docs/latest/charts/line.html
+    type: 'bar',
+    data: {
+        datasets: [
+            {
+                // Título de la gráfica
+                label: `Gastos por ${periodo}`,
+                // Color de fondo
+                backgroundColor: "#555555",
+                // Datos de la gráfica
+                // "agrup" contiene los datos a representar. Es uno de los parámetros de la función "mostrarGastosAgrupadosWeb".
+                data: agrup
+            }
+        ],
+    },
+    options: {
+        scales: {
+            x: {
+                // El eje X es de tipo temporal
+                type: 'time',
+                time: {
+                    // Indicamos la unidad correspondiente en función de si utilizamos días, meses o años
+                    unit: unit
+                }
+            },
+            y: {
+                // Para que el eje Y empieza en 0
+                beginAtZero: true
+            }
+        }
+    }
+});
+// Añadimos la gráfica a la capa
+divP.append(chart);
 }
 
 function repintar() {
