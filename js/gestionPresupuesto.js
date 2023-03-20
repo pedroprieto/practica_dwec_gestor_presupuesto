42600bb78d675fbfc6c5ccf1f84ffcddde21117d
Victor Bellver Bartolome <vbbprogramacion@gmail.com>
2023-03-20 11:20:13 +0100

    Primera practica terminada --- Tests pasados

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,51 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+var presupuesto = 0;    
 
 // TODO: Variable global
 
 
-function actualizarPresupuesto() {
+function actualizarPresupuesto(value) {
     // TODO
+    let retValue = 0;
+
+    if(value < 0 || isNaN(value)) {
+        retValue = -1;
+
+    }else{
+        presupuesto = value;
+        retValue = value;
+    }
+
+    return retValue;
 }
 
 function mostrarPresupuesto() {
     // TODO
+     return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
+function CrearGasto(descripcionIn, valorIn = 0) {
     // TODO
+    //PROPIEDADES
+    if (valorIn < 0 || isNaN(valorIn))
+        valorIn = 0;
+
+    this.descripcion = descripcionIn,
+    this.valor = parseFloat(valorIn),
+
+    //METODOS
+    this.mostrarGasto = function() {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    },
+
+    this.actualizarDescripcion = function(newDescripcion) {
+        this.descripcion = newDescripcion;
+    },
+    
+    this.actualizarValor = function(newValor) {
+        if(typeof newValor == 'number' &&  newValor >= 0)
+            this.valor = newValor;
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
