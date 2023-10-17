85902cbada1a4b1ff09b6fbe510c71ac01cc152a
Pablo Esteve <pabestsan@alu.edu.gva.es>
2023-10-17 19:29:24 +0200

    Creación del fichero presentacionNavegador.js

---
+++ js/presentacionNavegador.js
@@ -0,0 +1,6 @@
+/*El script deberá mostrar una alerta por pantalla con Nombre y Apellidos*/
+
+let nombre = "Pablo";
+let apellido1 = "Esteve";
+let apellido2 = "Sánchez";
+alert ("Mi nombre es: " nombre + " " + apellido1 + " " + apellido2);
