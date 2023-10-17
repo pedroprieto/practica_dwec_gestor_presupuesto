3cfc97a370c6c9a4998ce97dae7be3f074b16409
Pablo Esteve <pabestsan@alu.edu.gva.es>
2023-10-17 19:49:46 +0200

    Creación fichero presentacionNode.js

---
+++ js/presentacionNode.js
@@ -0,0 +1,6 @@
+/*El script deberá mostrar console.log() por pantalla con tu nombre y apellidos.*/
+
+let nombre = "Pablo";
+let apellido1 = "Esteve";
+let apellido2 = "Sánchez";
+console.log("Mi nombre es: " + nombre + " " + apellido1 + " " + apellido2);
