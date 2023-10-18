d801d1020776638b2a29610dfa38fcfb1f39f50d
Saul Santos <galrentot@outlook.es>
2023-10-18 22:10:38 +0200

    Test1 superado, pero varios problemas con los metodos y las funciones.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,24 +1,53 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-let presupuesto = 0;
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(actualizarPres) {
     // TODO
+    if (actualizarPres >= 0) {
+        presupuesto = actualizarPres;
+        return actualizarPres;
+    } else {
+        var mensErr = "Error el valor del Presupuesto es ";
+        return mensErr, -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion, valor) {
     // TODO
+    this.mostrarGasto = function () {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    }
+    this.actualizarDescripcion = function (actualizarDescrip) {
+        this.descripcion = actualizarDescrip;
+    }
+    this.actualizarValor = function (actualizarVal) {
+        if (actualizarVal >= 0) {
+            this.valor = actualizarVal;
+        } else {
+            valor = this.valor;
+        }
+
+    }
+    this.descripcion = descripcion;
+    if (valor >= 0) {
+        this.valor = valor;
+        return this.valor;
+    } else {
+        this.valor = 0;
+        return this.valor;
+    }
+
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
