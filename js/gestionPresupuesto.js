f724a7940889039bb3a6b33ada8db4595b403b1f
miguerutb <mtrascasas@gmail.com>
2023-10-18 16:03:16 +0200

    agruparperiodo

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -79,6 +79,21 @@ Etiquetas:\n`
       this.etiquetas = Object.assign(this.etiquetas.filter(e => e !== etiqueta))
     }
   }
+
+  this.obtenerPeriodoAgrupacion = (periodo) => {
+    const f = new Date(this.fecha)
+    const year = f.getFullYear()
+    const month = String(f.getMonth() + 1).padStart(2, '0')
+    const day = String(f.getDate()).padStart(2, '0')
+
+    if (periodo === 'dia') {
+      return `${year}-${month}-${day}`
+    } else if (periodo === 'mes') {
+      return `${year}-${month}`
+    } else if (periodo === 'anyo') {
+      return `${year}`
+    }
+  }
 }
 
 function listarGastos () {
@@ -104,6 +119,14 @@ function calcularBalance () {
   return presupuesto - calcularTotalGastos()
 }
 
+function agruparGastos () {
+
+}
+
+function filtrarGastos () {
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -115,5 +138,8 @@ export {
   anyadirGasto,
   borrarGasto,
   calcularTotalGastos,
-  calcularBalance
+  calcularBalance,
+  agruparGastos,
+  filtrarGastos
+
 }
