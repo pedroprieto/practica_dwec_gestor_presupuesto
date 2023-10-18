492a11561fd144bf734bce3a86a5b5ed18c68789
Francisco Vicedo <pacovvbb@gmail.com>
2023-10-18 13:14:24 +0200

    Modifico funcion borrarGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -56,8 +56,15 @@ function anyadirGasto(gasto){
 
 }
 
-function borrarGasto(){
+function borrarGasto(id){
 
+    for(let i=0; i<gastos.length;i++){
+        if(gastos[i].id==id){
+            gastos.splice(i,1);
+            break;
+        }
+    }
+    
 }
 
 function  calcularTotalGastos(){
