5e7c0ae856aa0ffbab22a577bb646c9c8fcc3fc8
Adrian Romero Alonso <adrromalo2@alu.edu.gva.es>
2023-10-20 12:02:53 +0200

    Politica de codigo limpio y comentarios Añadidos - Fundamentos II Terminada Actions Activadas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -121,7 +121,7 @@ function CrearGasto(descripcion,valor,fecha, ...etiquetas) {                    
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
+// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo.
 export   {
     listarGastos,
     anyadirGasto,
