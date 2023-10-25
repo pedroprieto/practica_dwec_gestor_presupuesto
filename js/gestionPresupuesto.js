f82101ee8b20d107b61790a12180b39b17fd997e
Laura Sellers <lauselfer@alu.edu.gva.es>
2023-10-25 18:55:47 +0200

    Fundamentos JS I completado

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,53 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0; 
 
 
-function actualizarPresupuesto() {
+
+function actualizarPresupuesto(cantidad) {
     // TODO
+    if(cantidad > 0){
+       return  presupuesto = cantidad; 
+    }else{
+        return -1; 
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+
+    return "Tu presupuesto actual es de " + presupuesto + " €"; 
 }
 
-function CrearGasto() {
-    // TODO
-}
+function CrearGasto(descripcion, valor) {
+    // Comprobación y ajuste del valor
+    if (typeof valor !== 'number' || valor < 0) {
+      valor = 0;
+    }
+  
+    // Propiedades
+    this.descripcion = descripcion;
+    this.valor = valor;
+  
+    // Métodos
+    this.mostrarGasto = function() {
+      return ("Gasto correspondiente a " +  this.descripcion +  " con valor " + this.valor + " €");
+    }
+  
+    this.actualizarDescripcion = function(nuevaDescripcion) {
+      this.descripcion = nuevaDescripcion;
+    }
+  
+    this.actualizarValor = function(nuevoValor) {
+      if (typeof nuevoValor === 'number' && nuevoValor >= 0) {
+        this.valor = nuevoValor;
+      }
+    }
+  }
+  
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -21,5 +55,6 @@ function CrearGasto() {
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto, 
+    
 }
