5c643ac11d40757d401255286462a7a3f3a7d4ee
Guillermo <guillermo.albert@outlook.com>
2023-10-29 08:28:03 +0100

    Creadas nuevas funciones vacías y se añaden al objeto export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -44,11 +44,23 @@ function CrearGasto(descripcion, valor) {
     }
   }
 
+  // Funciones vacías para que el código se ejecute sin errores
+function listarGastos() {}
+function anyadirGasto() {}
+function borrarGasto() {}
+function calcularTotalGastos() {}
+function calcularBalance() {}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance,
 }
