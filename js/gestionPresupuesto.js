e8139abab17783e94d2f9989dcee7ace524a6661
Guillermo <guillermo.albert@outlook.com>
2023-10-29 16:36:32 +0100

    Inicio de JSIII. Creadas funciones vacías 'filtrarGastos' y 'agruparGastos'

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -142,6 +142,10 @@ function calcularBalance() {
   return totalBalance;
 }
 
+filtrarGastos = function(){}
+
+agruparGastos = function(){}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -154,4 +158,6 @@ export {
   borrarGasto,
   calcularTotalGastos,
   calcularBalance,
+  filtrarGastos,
+  agruparGastos,
 };
