94f7a8aa25c46caf83efcba3e48aab0323a12b93
Guillermo <guillermo.albert@outlook.com>
2023-10-29 13:02:51 +0100

    Función 'calcularBalance' completada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -114,7 +114,10 @@ function calcularTotalGastos() {
 }
 
 
-function calcularBalance() {}
+function calcularBalance() {
+    const totalBalance = presupuesto - calcularTotalGastos();
+    return totalBalance;
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
