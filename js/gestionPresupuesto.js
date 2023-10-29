754b8cf5336298fa07f871c54564e6f7b488e42c
Ezequiel Maggio <ezematitrabajo@gmail.com>
2023-10-29 10:56:45 +0100

    Funcion Construtora con sus 3 metedos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,10 +4,10 @@ let presupuesto = 0;
 
 
 function actualizarPresupuesto(p) {
-    if(p >= 0){
-         p = presupuesto;
-         return p;
-    }else {
+    if (p >= 0) {
+        p = presupuesto;
+        return p;
+    } else {
         return p = -1;
     }
 }
@@ -18,15 +18,31 @@ function mostrarPresupuesto() {
 
 console.log(mostrarPresupuesto());
 
-function CrearGasto() {
-    
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion;
+    this.valor = (valor >= 0) ? valor : 0;
+
+    this.actualizarDescripcion = function (des) {
+        this.descripcion = des;
+    }
+
+    this.actualizarValor = function (valor) {
+        if (valor >= 0) {
+            return this.valor;
+        } else {
+            return this.valor;
+        }
+    }
+    this.mostrarGasto = function () {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor}`;
+    }
 }
 
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
