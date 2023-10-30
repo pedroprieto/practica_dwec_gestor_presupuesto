2e0d05304a19fa047dce2069aeee2343a5547f28
Francisco Vicedo <pacovvbb@gmail.com>
2023-10-30 12:42:10 +0100

    mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -3,8 +3,39 @@ function mostrarDatoEnID(idElemento,valor){
     element.textContent=valor;
 }
 
-function mostrarGastoWeb(){
-
+function mostrarGastoWeb(idElemento,gasto){
+    let element =document.getElementById(idElemento);
+    
+    let divGasto = document.createElement("div");
+    divGasto.classList.add("gasto");
+    element.appendChild(divGasto);
+    
+    let divDescripcion=document.createElement("div");
+    divDescripcion.classList.add("gasto-descripcion");
+    divDescripcion.textContent=gasto.descripcion;
+    divGasto.appendChild(divDescripcion);
+    
+    
+    let divGastoFecha= document.createElement("div");
+    divGastoFecha.classList.add("gasto-fecha");
+    divGastoFecha.textContent=gasto.fecha;
+    divGasto.appendChild(divGastoFecha);
+    
+    let divValor= document.createElement("div");
+    divValor.classList.add("gasto-valor");
+    divValor.textContent=gasto.valor;
+    divGasto.appendChild(divValor);
+    
+    let divEtiquetas = document.createElement("div");
+    divEtiquetas.classList.add("gasto-etiquetas");
+    divGasto.appendChild(divEtiquetas);
+    
+    gasto.etiquetas.forEach(etiqueta=>{
+    let etiquetaSpan= document.createElement("span");
+    etiquetaSpan.classList.add("gasto-etiquetas-etiqueta");
+    etiquetaSpan.textContent=etiqueta;
+    divEtiquetas.appendChild(etiquetaSpan);
+    })
 }
 
 function mostrarGastosAgrupadosWeb(){
