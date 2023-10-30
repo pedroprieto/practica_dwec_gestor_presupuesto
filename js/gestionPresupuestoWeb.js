c68e7e5572d12259278fdcef9cd39b4081a6bacc
Adrian Molina <adrmolbui@alu.edu.gva.es>
2023-10-30 13:33:49 +0100

    Realizo función mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,6 +1,41 @@
-function mostrarDatoEnId(idElemento, valor){
+function mostrarDatoEnId(idElemento, valor) {
     let elemento = document.getElementById(idElemento)
-    if(elemento){
+    if (elemento) {
         elemento.textContent = valor;
     }
+}
+
+function mostrarGastoWeb(idElemento, gasto) {
+    let elemento = document.getElementById(idElemento)
+
+    let divGasto = document.createElement("div");
+    divGasto.classList.add("gasto");
+    elemento.appendChild(divGasto);
+
+
+    let divDescripcion = document.createElement("div");
+    divDescripcion.classList.add("gasto-descripcion");
+    divDescripcion.textContent = gasto.descripcion;
+    elemento.appendChild(divDescripcion);
+
+    let divGastoFecha = document.createElement("div");
+    divGastoFecha.classList.add("gasto-fecha");
+    divGastoFecha.textContent = gasto.fecha;
+    divGasto.appendChild(divGastoFecha);
+
+    let divValor = document.createElement("div");
+    divValor.classList.add("gasto-valor");
+    divValor.textContent = gasto.valor;
+    divGasto.appendChild(divValor);
+
+    let divEtiquetas = document.createElement("div");
+    divEtiquetas.classList.add("gasto-etiquetas");
+    divGasto.appendChild(divEtiquetas);
+
+    gasto.etiquetas.forEach(etiquetas => {
+        let etiquetaSpan = document.createElement("span");
+        etiquetaSpan.classList.add("gasto-etiquetas-etiqueta");
+        etiquetaSpan.textContent = etiquetas;
+        divEtiquetas.appendChild(etiquetaSpan);
+    })
 }
