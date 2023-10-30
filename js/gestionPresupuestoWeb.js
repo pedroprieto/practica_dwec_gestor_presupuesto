c5845d4b8edc0dc2e0e160557a53c370ffc591bb
Francisco Vicedo <pacovvbb@gmail.com>
2023-10-30 12:40:25 +0100

    mostrarDatoEnID

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,12 @@
+function mostrarDatoEnID(idElemento,valor){
+    let element= document.getElementById(idElemento);
+    element.textContent=valor;
+}
+
+function mostrarGastoWeb(){
+
+}
+
+function mostrarGastosAgrupadosWeb(){
+
+}
