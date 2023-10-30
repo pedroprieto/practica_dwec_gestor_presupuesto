1b586427e417b72769d8feb1168229a77d85df89
Guille <guillermina888@gmail.com>
2023-10-30 22:45:15 +0100

    Código limpio - Comparación con libro

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,55 +2,55 @@
 
 let presupuesto = 0;
 
-function actualizarPresupuesto(ingreso) {//Funciona
+//* FUNCIONES: 
+
+function actualizarPresupuesto(ingreso) {                       //OK 
     if (ingreso >= 0) {
         return presupuesto = ingreso;
     }
     else {
-        
         console.log("Error");
         return -1;
     }
 }
 
-function mostrarPresupuesto() {//Funciona
-     return `Tu presupuesto actual es de ${presupuesto} €`;
+function mostrarPresupuesto() {                                 //OK
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
+//* OBJETO
+
 function CrearGasto(descrip, valorIntroducido) {
-    
+
     this.descripcion = descrip;
 
-    if (valorIntroducido > 0) {     
+    // prodiedad valor= opción condicional ? del libro: "this.valor = (valor >=0) ? valor : 0;"
+    if (valorIntroducido > 0) { 
         this.valor = valorIntroducido;
-    }
+    }                                  
     else {
         this.valor = 0;
     }
 
-    //* Métodos de objeto:
-    this.mostrarGasto = function () {
-       return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    //* Métodos del objeto:
+
+    this.mostrarGasto = function () {                           //OK
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
     };
-    this.actualizarDescripcion = function (nuevoTexto) {
-        return this.descripcion = nuevoTexto;
+    this.actualizarDescripcion = function (nuevoTexto) {        //OK 
+        this.descripcion = nuevoTexto;
     };
-    this.actualizarValor = function (nuevoValor) {
+    this.actualizarValor = function (nuevoValor) {              //OK Opción libro, con condicional ?sería: "this.valor = (valor >=0) ? valor : this.valor;"
         if (nuevoValor >= 0) {
             this.valor = nuevoValor;
         }
-
     };
-   
-
-       
- 
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
