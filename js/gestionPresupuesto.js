60af8233463d1a9d4528f810b6732b805cb2702a
Pablo Esteve <pabestsan@alu.edu.gva.es>
2023-11-01 11:52:02 +0100

    Primera prueba práctica js2

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -3,7 +3,8 @@
 // TODO: Variable global
 
 let presupuesto = 0;
-
+let Gastos = [];
+let idGasto = 0;
 
 function actualizarPresupuesto(result)
  {
@@ -26,7 +27,7 @@ function mostrarPresupuesto()
     return resultado;
 }
 
-function CrearGasto(descripcion, valor)
+function CrearGasto(descripcion, valor, fecha, ...etiquetas)
 {
     // TODO
     this.mostrarGasto = function()
@@ -45,10 +46,118 @@ function CrearGasto(descripcion, valor)
         this.valor = (valor >=0) ? valor : this.valor;
     }
 
+    this.mostrarGastoCompleto = function()
+    {
+        let comenta = "Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + " €." +
+        "Fecha: " + new Date(this.fecha).toLocaleString() +
+        "Etiquetas:\n";
+        
+        for(let etiq of this.etiquetas)
+        {
+            comenta = comenta + " - " + etiq + "\n";
+        }
+        return comenta;
+    }
+
+    this.actualizarFecha = function(fecha)
+    {
+        let fech = Date.parse(fecha);
+        if(fech)
+        {
+            this.fecha = fech;
+        }
+    }
+
+    this.anyadirEtiquetas = function(...etiquet)
+    {
+        for(let etiq of etiquet)
+        {
+            if(this.etiquetas.indexof(etiq) == -1)
+            {
+                this.etiquetas.push(etiq);
+            }
+        }
+    }
+
+    this.borrarEtiquetas = function(...etiquet)
+    {
+        let nuevo = [];
+        for(let etiq of this.etiquetas)
+        {
+            if(etiquet.indexof(etiq) == -1)
+            {
+                nuevo.push(etiq);
+            }
+        }
+        this.etiquetas = nuevo;
+    }
+
     this.descripcion = descripcion;
     this.valor = (valor >=0) ? valor : 0;
+
+    let fech = Date.parse(fecha);
+    if (fech)
+    {
+        this.fecha = fecha;
+    }
+    else
+    {
+        this.fecha = Date.parse(new Date());
+    }
+
+    this.etiquetas = [];
+    this.anyadirEtiquetas(...etiquetas);
     
 }
+
+function listarGastos()
+{
+    return Gastos;
+}
+
+function anyadirGastos(gastos)
+{
+    gastos.id = idGasto++;
+    Gastos.push(gastos);
+}
+
+function borrarGastos(idGasto)
+{
+    let gastos = null;
+
+    for(let gast of Gastos)
+    {
+        if(gast.id == idGasto)
+        {
+            gastos = gast;
+        }
+    }
+    
+    if(gastos)
+    {
+        let despuesgastos = Gastos.indexof(gastos);
+        Gastos.splice(despuesgastos, 1);
+    }
+}
+
+function calcularTotalGastos()
+{
+    let totalGastos = 0;
+
+    for (let gast of Gastos)
+    {
+        totalGastos = totalGastos + gast.valor;
+    }
+
+    return totalGastos;
+}
+
+function calcularBalance()
+{
+    return presupuesto - calcularTotalGastos();
+}
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -56,5 +165,10 @@ function CrearGasto(descripcion, valor)
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
+    listarGastos,
+    anyadirGastos,
+    borrarGastos,
+    calcularTotalGastos,
+    calcularBalance,
     CrearGasto
 }
