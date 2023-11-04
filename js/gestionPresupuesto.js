699f8db08386f3a8e8504348ef62306808f55ba7
Brenda Milena Senilliani <bresen@alu.edu.gva.es>
2023-11-04 20:43:54 +0100

    Implemento la función actualizarPresupuesto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,10 +1,16 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
-let presupuesto = 0; 
+let presupuesto = 0;
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if (valor >= 0) {
+        presupuesto = valor;
+        return presupuesto;
+    }
+    else {
+        console.log("El valor introducido no es válido");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
@@ -18,7 +24,7 @@ function CrearGasto() {
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
