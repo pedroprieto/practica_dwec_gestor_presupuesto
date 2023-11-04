c0dbde7890d6f6231700d5abc7e80fe4752614be
Brenda Milena Senilliani <bresen@alu.edu.gva.es>
2023-11-04 23:44:56 +0100

    Implemento la función anyadirGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -42,8 +42,10 @@ function listarGastos() {
     return gastos;
 }
 
-function anyadirGasto() {
-    
+function anyadirGasto(gasto) {
+    gasto.id = idGasto;
+    idGasto++;
+    gastos.push(gasto);
 }
 
 function borrarGasto() {
