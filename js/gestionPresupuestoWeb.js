86c13f77af72920b4fa4011e77dc9d536ca19a7a
Brenda Milena Senilliani <bresen@alu.edu.gva.es>
2023-11-05 18:44:50 +0100

    Añado funciones vacías y las exporto

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,17 @@
+function mostrarDatoEnId() {
+
+}
+
+function mostrarGastoWeb() {
+
+}
+
+function mostrarGastosAgrupadosWeb() {
+
+}
+
+export {    
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
