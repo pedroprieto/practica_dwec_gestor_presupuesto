9f627eeeaafacde225999f42e622c9283b32097a
Rodrigo Esteban Gimenez <rodgim@alu.edu.gva.es>
2023-11-05 09:36:36 +0100

    implemento funcion calcularBalance

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -54,7 +54,9 @@ function calcularTotalGastos() {
     return gastos.reduce((suma, gasto) => suma + gasto.valor, 0);
 }
 
-function calcularBalance() {}
+function calcularBalance() {
+    return presupuesto - calcularTotalGastos();
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
