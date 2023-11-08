68f8104aea87b06e5f945516820830cf4bee9b9f
Brenda Milena Senilliani <bresen@alu.edu.gva.es>
2023-11-08 14:32:26 +0100

    Creo los gastos

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -5,3 +5,12 @@ gp.actualizarPresupuesto(1500);
 
 let valor = gp.mostrarPresupuesto();
 gpw.mostrarDatoEnId("presupuesto", valor);
+
+let gastos = [
+    new gp.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"),
+    new gp.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"),
+    new gp.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte"),
+    new gp.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"),
+    new gp.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"),
+    new gp.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros")
+];
