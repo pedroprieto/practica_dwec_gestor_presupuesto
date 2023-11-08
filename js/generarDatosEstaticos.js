0b92b8d72819995375e44f3d02172119a43c06f7
Brenda Milena Senilliani <bresen@alu.edu.gva.es>
2023-11-08 13:02:25 +0100

    Actualizo y muestro el presupuesto

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,3 +1,7 @@
 import * as gp from './gestionPresupuesto.js';
 import * as gpw from './gestionPresupuestoWeb.js';
 
+gp.actualizarPresupuesto(1500);
+
+let valor = gp.mostrarPresupuesto();
+gpw.mostrarDatoEnId("presupuesto", valor);
