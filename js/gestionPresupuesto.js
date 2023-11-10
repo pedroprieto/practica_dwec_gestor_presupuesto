0ddc57654a567ed791cdb3fbdbf2a0c19be4001b
Jose Francisco Perez Castillo <jospercas2@alu.edu.gva.es>
2023-11-10 13:53:10 +0100

    Añado funciones filtrarGastos y agruparGastos a export y genero las funciones vacías y sin parámetros

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -155,6 +155,14 @@ function calcularBalance() {
     return presupuesto - calcularTotalGastos();
 }
 
+function filtrarGastos() {
+
+}
+
+function agruparGastos() {
+    
+}
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -167,5 +175,7 @@ export   {
     anyadirGasto,
     borrarGasto,
     calcularTotalGastos,
-    calcularBalance
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos
 }
