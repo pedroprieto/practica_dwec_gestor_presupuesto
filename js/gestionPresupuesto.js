eddc0336fa6a20e1e5feaec3b1a04eff80210554
Jorge Marco <jorge.yo456@gmail.com>
2023-11-10 19:30:36 +0100

    anyadir gasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -81,11 +81,15 @@ Etiquetas:\n`
 function listarGastos(){
     return gastos;
 }
-function anyadirGasto(){
+function anyadirGasto(gasto){
+    gasto.id=idGasto;
+    idGasto++;
+    gastos.push(gasto)
 }
 function borrarGasto(){
 }
-function calcularTotalGastos(){}
+function calcularTotalGastos(){
+}
 function calcularBalance(){}
 
 
