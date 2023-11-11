c9414ad4685b633934904e5d6462997e47809e47
Sergio Berenguer <serberper@alu.edu.gva.es>
2023-11-11 13:21:16 +0100

    Apartado Funciones

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -7,16 +7,30 @@ let presupuesto = 0;
 
 function actualizarPresupuesto(valor) {
     // TODO
-
+    if (typeof valor === 'number' && valor >=0) {
+        presupuesto =valor;
+        return presupuesto;
+    }
+    else {
+        console.log("El valor introducido no es válido.");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    console.log("Tu presupuesto actual es de " + presupuesto.toString() +"€.");
+}
+//función CrearGasto porqeu es un constructor y va en en mayúsculas.
+function CrearGasto(valor) {
+    // TODO
+    this.valor = (typeof valor === 'number' && valor >=0) ? valor: 0; 
+
 }
 
-function CrearGasto() {
-    // TODO
-}
+// Lo que haríamos es un nuevo objeto llamado gasto, podríamos haberlo creaado en la función también.
+let gasto = new CrearGasto();
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -24,5 +38,5 @@ function CrearGasto() {
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    crearGasto
 }
