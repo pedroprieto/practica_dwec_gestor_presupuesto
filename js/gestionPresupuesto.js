fc74d0cce93e4a2334fa6a407a017042d3490067
Rodrigo Esteban Gimenez <rodgim@alu.edu.gva.es>
2023-11-11 09:11:35 +0100

    agrego funciones vacias

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -84,6 +84,14 @@ function calcularBalance() {
     return presupuesto - calcularTotalGastos();
 }
 
+function filtrarGastos() {
+
+}
+
+function agruparGastos() {
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -96,4 +104,6 @@ export   {
     borrarGasto, 
     calcularTotalGastos, 
     calcularBalance,
+    filtrarGastos,
+    agruparGastos,
 }
