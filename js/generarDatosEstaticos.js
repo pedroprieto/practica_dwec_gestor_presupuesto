a69ab4e87e2344e62e1a039d4ff33cb8ff9765f1
Guillermo <guillermo.albert@outlook.com>
2023-11-11 12:27:58 +0100

    Se muestran los gastos filtrados

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,41 +1,3 @@
-/* import * as gesPres from "./gestionPresupuesto.js"
-import * as gesPresWeb from "./gestionPresupuestoWeb.js"
-
-// Actualizar el presupuesto a 1500€
-gesPres.actualizarPresupuesto(1500)
-
-// Mostrar el presupuesto en el div#presupuesto
-let presupuesto = gesPres.mostrarPresupuesto()
-gesPresWeb.mostrarDatoEnId("presupuesto", presupuesto)
-
-// Crear los gastos con los datos proporcionados
-let gasto1 = new gesPres.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida")
-let gasto2 = new gesPres.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida")
-let gasto3 = new gesPres.CrearGasto("Bonobús", 18.6, "2020-05-26", "transporte")
-let gasto4 = new gesPres.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina")
-let gasto5 = new gesPres.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros")
-let gasto6 = new gesPres.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros")
-
-// Añadir los gastos al array de gastos
-gesPres.anyadirGasto(gasto1)
-gesPres.anyadirGasto(gasto2)
-gesPres.anyadirGasto(gasto3)
-gesPres.anyadirGasto(gasto4)
-gesPres.anyadirGasto(gasto5)
-gesPres.anyadirGasto(gasto6)
-
-// Mostrar los gastos totales en div#gastos-totales (funciones calcularTotalGastos y mostrarDatoEnId)
-let totalGastos = gesPres.calcularTotalGastos()
-gesPresWeb.mostrarDatoEnId("gastos-totales", totalGastos)
-
-// Mostrar el balance total en div#balance-total (funciones calcularBalance y mostrarDatoEnId)
-let balance = gesPres.calcularBalance()
-gesPresWeb.mostrarDatoEnId("balance-total", balance)
-
-// Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos y mostrarGastoWeb)
-let gastosCompleto = gesPres.listarGastos()
-gesPresWeb.mostrarGastoWeb("listado-gastos-completo", gastosCompleto) */
-
 import * as gestionPresupuesto from "./gestionPresupuesto.js"
 import * as gestionPresupuestoWeb from "./gestionPresupuestoWeb.js"
 
@@ -75,4 +37,41 @@ let listaGastos = gestionPresupuesto.listarGastos()
 // Recorremos el array de gastos y mostramos cada gasto en el div#listado-gastos-completo
 for (let gasto of listaGastos) {
 	gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-completo", gasto)
+}
+
+// Mostramos el listado de gastos agrupados por día, mes y año en los divs#agrupacion-dia, #agrupacion-mes y #agrupacion-anyo
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-dia", "día", gestionPresupuesto.agruparGastos("dia"))
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-mes", "mes", gestionPresupuesto.agruparGastos("mes"))
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-anyo", "año", gestionPresupuesto.agruparGastos("anyo"))
+
+// Mostramos el listado de gastos realizados en septiembre de 2021 en listado-gastos-filtrado-1
+let filtro1 = { fechaDesde: "2021-09-01", fechaHasta: "2021-09-30" }
+let gastos1 = gestionPresupuesto.filtrarGastos(filtro1)
+// Recorremos el array de gastos y mostramos cada gasto en el div#listado-gastos-filtrado-1
+for (let gasto of gastos1) {
+	gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-1", gasto)
+}
+
+// Mostramos el listado de gastos de más de 50€ en div#listado-gastos-filtrado-2
+let filtro2 = { valorMinimo: 50 }
+let gastos2 = gestionPresupuesto.filtrarGastos(filtro2)
+// Recorremos el array de gastos y mostramos cada gasto en el div#listado-gastos-filtrado-2
+for (let gasto of gastos2) {
+	gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-2", gasto)
+}
+
+// Mostramos el listado de gastos de más de 200€ con etiqueta seguros en div#listado-gastos-filtrado-3
+let filtro3 = { valorMinimo: 200, etiquetasTiene: ["seguros"] }
+let gastos3 = gestionPresupuesto.filtrarGastos(filtro3)
+// Recorremos el array de gastos y mostramos cada gasto en el div#listado-gastos-filtrado-3
+for (let gasto of gastos3) {
+	gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-3", gasto)
+}
+
+// Mostramos el listado de gastos que tengan las etiquetas comida o transporte de menos de 50€ en div#listado-gastos-filtrado-4
+let filtro4 = { valorMaximo: 50, etiquetasTiene: ["comida", "transporte"] }
+let gastos4 = gestionPresupuesto.filtrarGastos(filtro4)
+// Recorremos el array de gastos y mostramos cada gasto en el div#listado-gastos-filtrado-4
+for (let gasto of gastos4) {
+	gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-4", gasto)
 }
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,66 +1,4 @@
-// Función de dos parámetros que se encargará de escribir el valor (texto) en el elemento HTML con id idElemento indicado:
-// idElemento - Hará referencia al id del elemento HTML donde se insertará el resultado en formato texto.
-// valor - El valor a mostrar.
-
-/*
-function mostrarDatoEnId(idElemento, valor) {
-	let elemento = document.getElementById(idElemento)
-	elemento.innerText = valor
-}
-
-//
-function mostrarGastoWeb(idElemento, gasto) {
-	// Creamos una variable que hará referencia al elemento HTML donde se insertará el resultado
-	let contenedor = document.getElementById(idElemento)
-
-	// Creamos un div con la clase gasto y lo añadimos al contenedor
-	let divGasto = document.createElement("div")
-	divGasto.classList.add("gasto")
-	contenedor.appendChild(divGasto)
-
-	// Creamos un div con la clase gasto-descripcion y lo añadimos al div gasto
-	let divDescripcion = document.createElement("div")
-	divDescripcion.classList.add("gasto-descripcion")
-	divDescripcion.textContent = "DESCRIPCIÓN DEL GASTO :" + gasto.descripcion
-	divGasto.appendChild(divDescripcion)
-
-	// Creamos un div con la clase gasto-fecha y lo añadimos al div gasto
-	let divFecha = document.createElement("div")
-	divFecha.classList.add("gasto-fecha")
-	let fecha = new Date(gasto.fecha).toLocaleDateString() // parse de timestamp a fecha corta
-	divFecha.textContent = "FECHA DEL GASTO: " + fecha
-	divGasto.appendChild(divFecha)
-
-	// Creamos un div con la clase gasto-valor y lo añadimos al div gasto
-	let divValor = document.createElement("div")
-	divValor.classList.add("gasto-valor")
-	divValor.textContent = "VALOR DEL GASTO :" + gasto.valor
-	divGasto.appendChild(divValor)
-
-	// Creamos un div con la clase gasto-etiquetas y lo añadimos al div gasto
-	let divEtiquetas = document.createElement("div")
-	divEtiquetas.classList.add("gasto-etiquetas")
-	divGasto.appendChild(divEtiquetas)
-
-	// Creamos un span con la clase gasto-etiquetas-etiqueta por cada etiqueta del gasto y lo añadimos al div gasto-etiquetas
-	// Usamos for of
-    // La consola marca gestionPresupuestoWeb.js:47 Uncaught TypeError: gasto.etiquetas is not iterable
-    // Hipótesis: gasto.etiquetas no es un array
-    // Comprobamos en gestionPresupuesto.js y vemos que sí es un array
-    // Hipótesis: el problema está en la función mostrarGastoWeb
-    // Comprobamos en gestionPresupuestoWeb.js y vemos que el problema está en la línea 47
-	for (const etiqueta of gasto.etiquetas) {
-		let spanEtiqueta = document.createElement("span")
-		spanEtiqueta.classList.add("gasto-etiquetas-etiqueta")
-		spanEtiqueta.textContent = etiqueta
-		divEtiquetas.appendChild(spanEtiqueta)
-	}
-
-	// Agregamos al contenedor el div gasto creado con todos sus elementos
-	contenedor.appendChild(divGasto)
-}
-
- */
+import * as gestionPresupuesto from "./gestionPresupuesto.js"
 
 // Función para mostrar un valor en un elemento HTML por su ID
 function mostrarDatoEnId(idElemento, valor) {
@@ -112,4 +50,46 @@ function mostrarGastoWeb(idElemento, gasto) {
 	contenedor.appendChild(divGasto)
 }
 
-export { mostrarDatoEnId, mostrarGastoWeb }
+// Función de tres parámetros que se encargará de crear dentro del elemento HTML con id idElemento indicado una estructura HTML para el objeto agrup que se pase como parámetro
+function mostrarGastosAgrupadosWeb(idElemento, periodo, agrup) {
+	// Creamos una variable que hará referencia al elemento HTML donde se insertará el resultado
+	let contenedor = document.createElement("div")
+	contenedor.classList.add("agrupacion")
+
+	// Creamos un h1 con el texto "Gastos agrupados por Periodo" y lo añadimos al contenedor
+	let h1 = document.createElement("h1")
+	h1.textContent = "Gastos agrupados por " + periodo
+	contenedor.appendChild(h1)
+
+	// Para cada dato del objeto agrup se crea un div
+	for (let a of Object.keys(agrup)) {
+		// Creamos un div con la clase agrupacion-dato y lo añadimos al contenedor
+		let divAgrupacion = document.createElement("div")
+		divAgrupacion.classList.add("agrupacion-dato")
+
+		// Creamos un span con la clase agrupacion-dato-clave
+		let spanPropiedad = document.createElement("span")
+		spanPropiedad.classList.add("agrupacion-dato-clave")
+		// El texto del span será la propiedad del objeto agrup
+		spanPropiedad.textContent = a
+		// Agregamos al div agrupacion el span agrupacion-dato-clave
+		divAgrupacion.appendChild(spanPropiedad)
+
+		// Creamos un span con la clase agrupacion-dato-valor
+		let spanValor = document.createElement("span")
+		spanValor.classList.add("agrupacion-dato-valor")
+		// El texto del span será el valor de la propiedad del objeto agrup que se accede con agrup[a]
+		spanValor.textContent = agrup[a]
+		// Agregamos al div agrupacion el span agrupacion-dato-valor
+		divAgrupacion.appendChild(spanValor)
+
+		// Agregamos al contenedor el div agrupacion creado con todos sus elementos
+		contenedor.appendChild(divAgrupacion)
+	}
+
+	// Agregamos al elemento deseado el contenedor creado con todos sus elementos
+	let elemento = document.getElementById(idElemento)
+	elemento.appendChild(contenedor)
+}
+
+export { mostrarDatoEnId, mostrarGastoWeb, mostrarGastosAgrupadosWeb }
