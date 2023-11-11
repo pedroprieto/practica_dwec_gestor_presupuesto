34d4cac51a12e3bc6ad361d77add62fadfe492ed
Rodrigo Esteban Gimenez <rodgim@alu.edu.gva.es>
2023-11-11 09:58:32 +0100

    añado gastos y muestro gastos totales

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -3,3 +3,15 @@ import * as gestionWeb from "./gestionPresupuestoWeb.js";
 
 gestion.actualizarPresupuesto(1500);
 gestionWeb.mostrarDatoEnId("presupuesto", gestion.mostrarPresupuesto());
+
+let gastosEntrada = [
+    ["Compra carne", 23.44, "2021-10-06", "casa", "comida"],
+    ["Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"],
+    ["Bonobús", 18.60, "2020-05-26", "transporte"],
+    ["Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"],
+    ["Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"],
+    ["Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"],
+]
+gastosEntrada.forEach(gasto => gestion.anyadirGasto(new gestion.CrearGasto(...gasto)));
+
+gestionWeb.mostrarDatoEnId("gastos-totales", gestion.calcularTotalGastos());
