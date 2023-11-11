d79e2721a281c3c6b09fc20cbfb1d8fce8ca9d2b
Rodrigo Esteban Gimenez <rodgim@alu.edu.gva.es>
2023-11-11 09:44:12 +0100

    actualizo y muestro presupuesto

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,3 +1,5 @@
 import * as gestion from "./gestionPresupuesto.js";
 import * as gestionWeb from "./gestionPresupuestoWeb.js";
 
+gestion.actualizarPresupuesto(1500);
+gestionWeb.mostrarDatoEnId("presupuesto", gestion.mostrarPresupuesto());
