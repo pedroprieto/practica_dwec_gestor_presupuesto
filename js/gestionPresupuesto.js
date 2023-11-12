2d84c345c63279249e407ae7249c6119d03514ea
Amaya Zárate <amazarher@alu.edu.gva.es>
2023-11-12 17:43:11 +0100

    "Terminado Fundamentos de JavaScript II"

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,13 +1,9 @@
-// He consultado el archivo con las soluciones porque habían cosas que no entendía el porqué
-// de los errores, algunas eran simples espacios que faltaban en el texto, o las comillas que había 
-// puesto mal. Y otras que no he conseguido entender.
-
 // Variables globales
 let presupuesto = 0;
 let gastos = [];
 let idGasto = 0;
 
-// Funciones
+// MIO
 function actualizarPresupuesto(num) {
     if(num >= 0){
         presupuesto = num;
@@ -19,16 +15,91 @@ function actualizarPresupuesto(num) {
     }
 }
 
+
+// MIO
 function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
 function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
-    
+
+    // Métodos
+    // MIO
+    this.mostrarGasto = function(){
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    }
+
+    // MIO
+    this.actualizarDescripcion = function(descripcion){
+        this.descripcion = descripcion;
+        return descripcion;
+    }
+
+    // MIO
+    this.actualizarValor = function(valor){
+        if(valor >= 0){
+            this.valor = valor;
+            return valor;
+        }
+        else{
+            valor = this.valor;
+            return valor;
+        }
+    }
+
+    // MIO
+    this.mostrarGastoCompleto = function(){
+        let texto = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.
+Fecha: ${new Date(this.fecha).toLocaleString()}
+Etiquetas:\n`
+            for(let e of this.etiquetas){
+                texto += `- ${e}\n`
+            }
+            return texto;
+    }
+
+    this.actualizarFecha = function(fecha) {
+        let f = Date.parse(fecha);
+        if (f) {
+            this.fecha = f;
+        }
+    }
+
+    // MIO
+    this.anyadirEtiquetas = function(...etiqueta){
+        for(let e of etiqueta){
+            if(this.etiquetas.indexOf(e) == -1){
+                this.etiquetas.push(e);
+            }
+        }
+    }
+    // SOLUCION
+    this.borrarEtiquetas = function(...etqs) {
+        // No entiendo porqué se pone push si se supone que hay que borrar la etiqueta que
+        // coincida con la que nos dan.
+        let newetiquetas = [];
+
+        for (let e of this.etiquetas) {
+	    if (etqs.indexOf(e) == -1) {
+                newetiquetas.push(e);
+	    }
+        }
+
+        this.etiquetas = newetiquetas;
+
+        // No entiendo porqué no funciona el código de abajo.
+        /*for(let e of this.etiquetas){
+            for(let f of etqs){
+                if(e == f){
+                    this.etiquetas.slice(f);
+                }
+            }
+        }*/
+    }
+
+    // PROPIEDADES --- MIO
     this.descripcion = descripcion;
-    this.valor = (valor >=0) ? valor : 0;  
-    // No entiendo porqué no puedo poner simplemente this.valor = valor; 
-    // si ya compruebo en el método actualizar valor si es un número negativo.
+    this.valor = (valor >=0) ? valor : 0;
     let f = Date.parse(fecha);
     if(f){
         this.fecha = f;
@@ -36,102 +107,24 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
     else{
         this.fecha = Date.parse(new Date());
     }
-    // Mío
-    /*if (isNaN(f)){
-        this.fecha = Date.now;
-    }
-    else{
-        this.fecha = f;
-    }*/
     this.etiquetas = [];
     this.anyadirEtiquetas(...etiquetas);
-
-    this.mostrarGasto = function(){
-        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
-    }
-
-    this.mostrarGastoCompleto = function(){
-        let texto = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.
-        Fecha: ${new Date(this.fecha).toLocaleString()}
-        Etiquetas:\n`
-            for(let e of this.etiquetas){
-                texto += `- ${e}\n`
-            }
-            return texto;
-    }
-    this.actualizarDescripcion = function(descripcion){
-        this.descripcion = descripcion;
-        /*return descripcion;*/
-    }
-    this.actualizarValor = function(valor){
-        this.valor = (valor >=0) ? valor : this.valor;
-        /*if(valor >= 0){
-            this.valor = valor;
-            return valor;
-        }
-        else{
-            valor = this.valor;
-            return valor;
-        }*/
-    }
-    this.actualizarFecha = function(fecha){
-        let f = Date.parse(fecha);
-        if(f){
-            this.fecha = f;
-        }
-        /*if (isNaN(f)){
-            this.fecha = Date.now;
-        }
-        else{
-            this.fecha = f;
-        }*/ 
-    }
-    this.anyadirEtiquetas = function(...etiqueta){
-        for(let e of etiqueta){
-            if(this.etiquetas.indexOf(e) == -1){
-                this.etiquetas.push(e);
-            }
-            // Porqué no funciona lo siguiente...
-            /*if(!this.etiqueta.includes(e)){
-                this.etiquetas.push(e);
-            }*/
-        }
-    }
-
-    this.borrarEtiquetas = function(...etiqueta){
-        let newetiquetas = [];
-
-        for (let e of this.etiquetas) {
-	    if (etiqueta.indexOf(e) == -1) {
-                newetiquetas.push(e);
-	    }
-        }
-
-        this.etiquetas = newetiquetas;
-
-        // Porqué no funciona ésto
-        /*for(let etqs of this.etiquetas){
-            for(let etq of etiqueta){
-                if(etq == etqs){
-                    this.etiquetas.splice(etqs);
-                }
-            }
-        }
-    }*/
-    }
 }
 
+// MIO
 function listarGastos(){
     return gastos;
 }
 
+// MIO
 function anyadirGasto(gasto){
     gasto.id = idGasto;
     idGasto++;
     gastos.push(gasto);
 }
 
-function borrarGasto(idGasto){
+/* SOLUCION ---*/
+function borrarGasto(idGasto) {
     let gasto = null;
     for (let g of gastos) {
 	if (g.id == idGasto) {
@@ -142,22 +135,26 @@ function borrarGasto(idGasto){
         let posGasto = gastos.indexOf(gasto);
         gastos.splice(posGasto, 1);
     }
-    /*for(let g of gastos){
-        if(g == idGasto){
+}
+// MIO
+/*function borrarGasto(idGasto){
+    for(let g of gastos){
+        if(g.id == idGasto){
             gastos.splice(g);
         }
-    }*/
-}
+    }
+}*/
 
+// MIO
 function calcularTotalGastos(){
     let sumaGastos = 0;
     for (let g of gastos){
         sumaGastos += g.valor;
-        /*sumaGastos += g;*/
     }
     return sumaGastos;
 }
 
+// MIO
 function calcularBalance(){
     let balance = presupuesto - calcularTotalGastos();
     return balance;
