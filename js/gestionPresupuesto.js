1435478fc8cebaa94aa7df1c0cdcc290b27f96a2
Amaya Zárate <amazarher@alu.edu.gva.es>
2023-11-12 16:17:34 +0100

    "Fundamentos de JS II"

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -30,54 +30,94 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
     // No entiendo porqué no puedo poner simplemente this.valor = valor; 
     // si ya compruebo en el método actualizar valor si es un número negativo.
     let f = Date.parse(fecha);
-    if (isNaN(f)){
+    if(f){
+        this.fecha = f;
+    }
+    else{
+        this.fecha = Date.parse(new Date());
+    }
+    // Mío
+    /*if (isNaN(f)){
         this.fecha = Date.now;
     }
     else{
         this.fecha = f;
-    }    
-    this.etiquetas = etiquetas[""];
+    }*/
+    this.etiquetas = [];
+    this.anyadirEtiquetas(...etiquetas);
+
+    this.mostrarGasto = function(){
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    }
 
     this.mostrarGastoCompleto = function(){
-        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} € \n
-        Fecha: ${this.fecha.toLocaleString()} \nEtiquetas: ${this.etiquetas}`;
-    };
+        let texto = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.
+        Fecha: ${new Date(this.fecha).toLocaleString()}
+        Etiquetas:\n`
+            for(let e of this.etiquetas){
+                texto += `- ${e}\n`
+            }
+            return texto;
+    }
     this.actualizarDescripcion = function(descripcion){
         this.descripcion = descripcion;
-        return descripcion;
-    };
+        /*return descripcion;*/
+    }
     this.actualizarValor = function(valor){
-        if(valor >= 0){
+        this.valor = (valor >=0) ? valor : this.valor;
+        /*if(valor >= 0){
             this.valor = valor;
             return valor;
         }
         else{
             valor = this.valor;
             return valor;
-        }
-    };
+        }*/
+    }
     this.actualizarFecha = function(fecha){
         let f = Date.parse(fecha);
-        if (isNaN(f)){
+        if(f){
+            this.fecha = f;
+        }
+        /*if (isNaN(f)){
             this.fecha = Date.now;
         }
         else{
             this.fecha = f;
-        }   
+        }*/ 
     }
     this.anyadirEtiquetas = function(...etiqueta){
-        for(let e of etiquetas){
-            if(etiqueta != e ){
-                this.etiquetas.push(etiqueta);
+        for(let e of etiqueta){
+            if(this.etiquetas.indexOf(e) == -1){
+                this.etiquetas.push(e);
             }
+            // Porqué no funciona lo siguiente...
+            /*if(!this.etiqueta.includes(e)){
+                this.etiquetas.push(e);
+            }*/
         }
     }
+
     this.borrarEtiquetas = function(...etiqueta){
-        for(let e of etiquetas){
-            if(etiqueta == e ){
-                this.etiquetas.splice(etiqueta);
+        let newetiquetas = [];
+
+        for (let e of this.etiquetas) {
+	    if (etiqueta.indexOf(e) == -1) {
+                newetiquetas.push(e);
+	    }
+        }
+
+        this.etiquetas = newetiquetas;
+
+        // Porqué no funciona ésto
+        /*for(let etqs of this.etiquetas){
+            for(let etq of etiqueta){
+                if(etq == etqs){
+                    this.etiquetas.splice(etqs);
+                }
             }
         }
+    }*/
     }
 }
 
@@ -85,24 +125,35 @@ function listarGastos(){
     return gastos;
 }
 
-function anyadirGasto(CrearGasto){
-    CrearGasto.id = idGasto;
+function anyadirGasto(gasto){
+    gasto.id = idGasto;
     idGasto++;
-    gastos.push(CrearGasto.id);
+    gastos.push(gasto);
 }
 
 function borrarGasto(idGasto){
-    for(let g of gastos){
+    let gasto = null;
+    for (let g of gastos) {
+	if (g.id == idGasto) {
+	    gasto = g;
+	}
+    }
+    if (gasto) {
+        let posGasto = gastos.indexOf(gasto);
+        gastos.splice(posGasto, 1);
+    }
+    /*for(let g of gastos){
         if(g == idGasto){
             gastos.splice(g);
         }
-    }
+    }*/
 }
 
 function calcularTotalGastos(){
     let sumaGastos = 0;
     for (let g of gastos){
-        sumaGastos += g;
+        sumaGastos += g.valor;
+        /*sumaGastos += g;*/
     }
     return sumaGastos;
 }
