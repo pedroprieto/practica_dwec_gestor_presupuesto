a9e95ea919c3bccd4475825a53f9776ba72fb95f
Pablo Esteve <pabestsan@alu.edu.gva.es>
2023-11-13 17:53:22 +0100

    Creación de los export y de las funciones filtrarGastos y agruparGastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -96,6 +96,7 @@ Etiquetas:\n`
 
         this.etiquetas = nuevo;
     }
+    
 
     this.descripcion = descripcion;
     this.valor = (valor >= 0) ? valor : 0;
@@ -165,6 +166,18 @@ function calcularBalance()
     return presupuesto - calcularTotalGastos();
 }
 
+function filtrarGastos(datos)
+{
+
+}
+
+function agruparGastos(periodo, etiquetas, fechaDesde, fechaHasta)
+{
+    
+}
+
+
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -176,5 +189,8 @@ export   {
     borrarGasto,
     calcularTotalGastos,
     calcularBalance,
-    CrearGasto
+    CrearGasto,
+    filtrarGastos,
+    agruparGastos
+
 }
