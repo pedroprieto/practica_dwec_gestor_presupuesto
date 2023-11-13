4361b587f0d2683e06c132e14935e7bc0cbee1a9
Brenda Milena Senilliani <bresen@alu.edu.gva.es>
2023-11-13 20:11:28 +0100

    Importo la librería gestionPresupuesto

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,3 +1,5 @@
+import * as gp from "./gestionPresupuesto.js";
+
 function mostrarDatoEnId(idElemento, valor) {
     let elemento = document.querySelector(`#${idElemento}`);
     elemento.textContent = valor;
