fc47eee7f05cab81bdb020278636c3c747c0bf6a
Diego <dievalsie@alu.edu.gva.es>
2023-11-15 22:06:33 +0100

    Parece que tengo listo fundamentos de js I

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -38,6 +38,12 @@ function CrearGasto(descripcion, valor) {
         this.descripcion = descripcion;
     }
 
+    this.actualizarValor = function(valor){
+        if (valor >= 0){
+            this.valor = valor;
+        }
+    }
+
 }
 
 
