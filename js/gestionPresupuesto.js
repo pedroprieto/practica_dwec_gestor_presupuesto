c9c6951a7048061a4b98d495ca3b9f5a6b1862c2
atms1dawz <angmorsen@alu.edu.gva.es>
2023-11-16 20:50:15 +0100

    hay cuatro fallos en el test

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,28 +2,31 @@
 
 // TODO: Variable global
 let presupuesto = 0;
+console.log("Presupuesto igual a " + presupuesto);
 
-// llamadas a las funciones
-presupuesto = actualizarPresupuesto(500);
 
-if (presupuesto >= 0){
-    console.log(mostrarPresupuesto());
-}
+// EJECUCIÓN DEL PROGRAMA: LLAMADA A LAS FUNCIONES
+let numero = actualizarPresupuesto(2000);
+
+console.log(mostrarPresupuesto(numero));
+
 
 // Primera llamada a la función crearGasto: crea el objeto.
 let gasto1 = new CrearGasto("Gastos ordinarios", 350);
 gasto1.mostrarGasto();
-
-// Segunda llamada a la función crearGasto: actualiza el objeto.
 gasto1.actualizarDescripcion("Gastos imprevistos");
-gasto1.actualizarValor(500);
-gasto1.mostrarGasto();
 
-// funciones
+// FUNCIONES
+
+// 1 función actualizarPresupuesto
 function actualizarPresupuesto(numero) {
     if ((numero >= 0) && (!isNaN(numero))){
+        presupuesto = numero
         return numero;
-    } else {
+    } else if (numero == presupuesto){
+        return 0;
+    }
+    else {
         console.log("Error: número menor que cero o carácter no numérico");
         return -1;
     }
@@ -31,30 +34,57 @@ function actualizarPresupuesto(numero) {
 
 
 function mostrarPresupuesto() {
-    return "Tu presupuesto actual es de " + presupuesto + " €";
+    if ((numero >= 0) && (!isNaN(numero))){
+        return "Tu presupuesto actual es de " + presupuesto + " €";
+    }
 }
 
 function CrearGasto(descripcion, valor) {
-    let gasto = {
-        descripcion: descripcion,
-        valor: valor,
-        
-        mostrarGasto(){
-            console.log("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor);
-        },
+        let gasto = {
+            descripcion: descripcion,
+            valor: valor,
 
-        actualizarDescripcion(nuevaDescripcion){
-            this.descripcion = nuevaDescripcion;
-        },
+            mostrarGasto: function(){
+                console.log("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor);
+            },
 
-        actualizarValor(nuevoValor){
-            this.valor = nuevoValor;
+            actualizarDescripcion: function (nuevaDescripcion){
+                this.descripcion = nuevaDescripcion;
+                this.mostrarGasto();
+            },
+
+            actualizarValor: function (nuevoValor){
+                if ((nuevoValor >= 0) && (!isNaN(nuevoValor))){
+                    this.valor = nuevoValor;
+                }  
+                this.mostrarGasto();
+            }      
+
+
+        };
+
+        if (gasto.valor < 0){
+            gasto.valor = 0;
+            return gasto;
         }
 
-    };
+        return gasto;
+    }
 
-    return gasto;
-}
+/*
+
+        
+
+
+        gasto.
+
+ 
+        }*/
+
+
+
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
