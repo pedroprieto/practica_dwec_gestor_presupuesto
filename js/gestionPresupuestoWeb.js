d70206e909150db044720c47ca9b2ef146da141c
Jorge Marco <jorge.yo456@gmail.com>
2023-11-16 20:48:31 +0100

    interaccionHTML

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,12 @@
+function mostrarDatoEnId(){
+
+}
+
+function mostrarGastoWeb(){}
+function mostrarGastosAgrupadosWeb(){}
+
+export{
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
