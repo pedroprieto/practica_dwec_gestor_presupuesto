0b3affbad231b2c031b21935832aba528f0b4928
atms1dawz <angmorsen@alu.edu.gva.es>
2023-11-16 19:21:47 +0100

    funcion crearGasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,11 +4,20 @@
 let presupuesto = 0;
 
 // llamadas a las funciones
-presupuesto = actualizarPresupuesto(-1);
+presupuesto = actualizarPresupuesto(500);
 
 if (presupuesto >= 0){
     console.log(mostrarPresupuesto());
 }
+
+// Primera llamada a la función crearGasto: crea el objeto.
+let gasto1 = new CrearGasto("Gastos ordinarios", 350);
+gasto1.mostrarGasto();
+
+// Segunda llamada a la función crearGasto: actualiza el objeto.
+gasto1.actualizarDescripcion("Gastos imprevistos");
+gasto1.actualizarValor(500);
+gasto1.mostrarGasto();
 
 // funciones
 function actualizarPresupuesto(numero) {
@@ -25,8 +34,26 @@ function mostrarPresupuesto() {
     return "Tu presupuesto actual es de " + presupuesto + " €";
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    let gasto = {
+        descripcion: descripcion,
+        valor: valor,
+        
+        mostrarGasto(){
+            console.log("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor);
+        },
+
+        actualizarDescripcion(nuevaDescripcion){
+            this.descripcion = nuevaDescripcion;
+        },
+
+        actualizarValor(nuevoValor){
+            this.valor = nuevoValor;
+        }
+
+    };
+
+    return gasto;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
