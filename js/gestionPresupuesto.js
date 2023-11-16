c74a3ebf1e97fccf1cbcd7baa2add81df27064a7
AICDAW2223 <adribacan@alu.edu.gva.es>
2023-11-16 03:25:30 +0100

    Pasan 6/9 tests

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,13 +4,7 @@
 
 let presupuesto = 0;
 
-let gasto={
-    descripcion:"",
-    valor:0   
-}
-gasto.setDatos= function(valor){
- this.valor=valor;
-}
+
 
 
 function actualizarPresupuesto( valor ) {
@@ -37,10 +31,30 @@ function mostrarPresupuesto() {
     
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto( descripcion, valor) {
+    this.descripcion=descripcion;
+    this.valor=valor;
+    if(valor<0 || isNaN(valor)){
+        this.valor=0
+        return (`gasto= new gasto  ${this.descripcion}  ${this.valor} €`)
+    }
+    else{
+       
+        return (`gasto= new gasto  ${this.descripcion}  ${this.valor}`)
+    }
 }
-
+function mostrarGasto(){
+    return(`Gasto correspondiente a ${descripcion} con valor ${valor} €`)
+}
+function actualizarDescripcion(descripcion){
+return (`${descripcion}`)
+}
+function actualizarValor(valor){
+if (valor>0)
+return (`${valor}`)
+else
+return
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
