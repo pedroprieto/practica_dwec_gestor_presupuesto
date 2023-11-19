11cd46315b26b9174f0e4d05648dc6fa99c98b6f
Luis-Rodes <luisjrodes@gmail.com>
2023-11-19 13:33:35 +0100

    Cambio parametros fecha por fallo test 3

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -236,11 +236,11 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
     this.obtenerPeriodoAgrupacion = function (periodo) {
         let fecha = new Date(this.fecha);
         if ( periodo =="dia") {
-            return fecha.toISOString().substring(0 , 11);
+            return fecha.toISOString().substring(0 , 10);
         } else if (periodo == "mes") {
-            return fecha.toISOString().substring(0 , 8);
+            return fecha.toISOString().substring(0 , 7);
         } else if (periodo == "anyo") {
-            return fecha.toISOString().substring(0, 5);
+            return fecha.toISOString().substring(0, 4);
         }
 
     }
