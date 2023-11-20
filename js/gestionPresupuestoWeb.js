8201bb9edc5b8b29a76a5c3ed07c2aa9b63ef179
Patricia Romero Marquez <patricia.romarquez@gmail.com>
2023-11-21 00:37:21 +0100

    creo funcion mostrarDatoEnID

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,6 +1,15 @@
 
+function mostrarDatoEnId(idElemento, valor) {
 
+    //obtengo el elemento HTML por su id
+    let elemento = document.getElementById(idElemento);
+    //propiedad texto visible dentro del elemento. Reemplazo el contexto de texto actual del elemento con el valor.
+    elemento.innerText = valor;
+}
 
+function mostrarGastoWeb(idElemento, gasto) {
+    
+}
 
 
 
