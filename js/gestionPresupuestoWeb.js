bd5426658c1c225236226afd1c1f615581b487a2
Diego <dievalsie@alu.edu.gva.es>
2023-11-23 00:36:25 +0100

    Se hacen microtest en archivos a parte, para simular un pequeño entorno de pruebas, por lo que al volcar el contenido ya hemos probado dos funciones por nuestra cuenta antes de este commit

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,23 +1,73 @@
 
-
-
 function mostrarDatoEnId(idElemento, valor){
-  let elemento = document.getElementById(idElemento);
-  elemento.innerText(valor);
+  const elemento = document.getElementById(idElemento);
+  elemento.textContent = valor;
 }
+
 
 function mostrarGastoWeb(idElemento, gasto){
-  let elementoTarget = document.getElementById(idElemento);
-  let elementoGasto = elementoTarget.createElement(div.gasto);
-  elementoTarget.append(elementoGasto);
+  let targetElement = document.getElementById(idElemento);
+  let gastoTag = document.createElement("div");
+  gastoTag.classList.add("gasto");
 
-  let gastoDescr = elementoGasto.createElement(div.gastoDescripcion);
+  
+  appender();
 
+  targetElement = targetElement.querySelector(".gasto");
+  gastoTag = document.createElement("div");
+  gastoTag.classList.add("gasto-escripcion");
+  gastoTag.textContent = gasto.descripcion;
+  appender();
+  gastoTag = document.createElement("div");
+  gastoTag.classList.add("gasto-fecha");
+  gastoTag.textContent = gasto.fecha;
+  appender();
+  gastoTag = document.createElement("div");
+  gastoTag.classList.add("gasto-valor");
+  gastoTag.textContent = gasto.valor;
+  appender();
+  gastoTag = document.createElement("div");
+  gastoTag.classList.add("gasto-etiquetas");
+  appender();
+
+  targetElement = targetElement.querySelector(".gasto-etiquetas");
+  for (let eti in gasto.etiquetas){
+    gastoTag = document.createElement("span");
+    gastoTag.classList.add("gasto-etiquetas-etiqueta");
+    gastoTag.textContent = gasto.etiquetas[eti];
+    appender();
+  }
+}
+
+
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
+
+  function appender() {targetElement.append(gastoTag);}
+
+  let targetElement = document.getElementById(idElemento);
+  let gastoTag = document.createElement("div");
+  gastoTag.classList.add("agrupacion");
+  
+  function appender() {targetElement.append(gastoTag);}
+
+  appender();
+  gastoTag = document.createElement("h1");
+  gastoTag.textContent = `Gastos agrupados por ${periodo}`
+
+
+  for (let grupo in agrup){
+    
+  }
 
 }
 
 
-
+let gasto1 = {
+  descripcion: "Gasto de cositas",
+  fecha: "2022-10-10",
+  valor: 200,
+  etiquetas: ["Hogar", "Perro"]
+}
 
 
 
