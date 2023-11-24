1e7b8b993dcb9185dabfcda39e53cf125eaecf77
Jorge Marco <jorge.yo456@gmail.com>
2023-11-24 11:25:05 +0100

    funcion mostrarGastoWeb

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -7,11 +7,18 @@ gpWeb.mostrarDatoEnId("presupuesto",gp.mostrarPresupuesto());
 //Crear y añadir gastos
 var gasto1 = new gp.CrearGasto("Compra carne",23.44,"2021-10-06","casa","comida");
 gp.anyadirGasto(gasto1);
-gp.anyadirGasto(gp.CrearGasto("Compra fruta y verdura",14.25,"2021-09-06","supermercado","comida"));
-gp.anyadirGasto(gp.CrearGasto("Bonobús",18.60,"2021-05-26","transporte"));
-gp.anyadirGasto(gp.CrearGasto("Gasolina",60.42,"2021-10-08","transporte","gasolina"));
-gp.anyadirGasto(gp.CrearGasto("Seguro Hogar",209.45,"2021-09-26","casa","seguros"));
-gp.anyadirGasto(gp.CrearGasto("Seguro Coche",195.78,"2021-10-06","transporte","seguros"));
+gp.anyadirGasto(new gp.CrearGasto("Compra fruta y verdura",14.25,"2021-09-06","supermercado","comida"));
+gp.anyadirGasto(new gp.CrearGasto("Bonobús",18.60,"2021-05-26","transporte"));
+gp.anyadirGasto(new gp.CrearGasto("Gasolina",60.42,"2021-10-08","transporte","gasolina"));
+gp.anyadirGasto(new gp.CrearGasto("Seguro Hogar",209.45,"2021-09-26","casa","seguros"));
+gp.anyadirGasto(new gp.CrearGasto("Seguro Coche",195.78,"2021-10-06","transporte","seguros"));
 
 gpWeb.mostrarDatoEnId("gastos-totales",gp.calcularTotalGastos());
 gpWeb.mostrarDatoEnId("listado-gastos-completo",gp.calcularBalance());
+
+var gastos= gp.listarGastos();
+gastos.forEach(element => {
+    gpWeb.mostrarGastoWeb("listado-gastos-completo",element);
+});
+
+gpWeb
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -7,8 +7,40 @@ function mostrarDatoEnId(idElemento, valor){
     document.getElementById(idElemento).appendChild(p);
 }
 
-function mostrarGastoWeb(){}
-function mostrarGastosAgrupadosWeb(){}
+function mostrarGastoWeb(idElemento, gasto){
+    const gastoElement = document.createElement('div');
+    gastoElement.classList.add('gasto');        
+    //descripcion
+    const descripcionElement = document.createElement('div');
+    descripcionElement.classList.add('gasto-descripcion');
+    descripcionElement.textContent = gasto.descripcion;
+    gastoElement.appendChild(descripcionElement);
+    //fecha
+    const fechaElement = document.createElement('div');
+    fechaElement.classList.add('gasto-fecha');
+    fechaElement.textContent = new Date(gasto.fecha).toLocaleDateString('en-us', {year:"numeric", month:"numeric", day:"numeric"});
+    gastoElement.appendChild(fechaElement);
+    //valor
+    const valorElement = document.createElement('div');
+    valorElement.classList.add('gasto-valor');
+    valorElement.textContent = gasto.valor;
+    gastoElement.appendChild(valorElement);
+    //etiquetas
+    var etiquetasElement = document.createElement('div');
+    etiquetasElement.classList.add('gasto-etiquetas');
+    for (var g of gasto.etiquetas){
+        var etiquetaElement = document.createElement('span');
+        etiquetaElement.classList.add('gasto-etiquetas-etiqueta');
+        etiquetaElement.textContent = g;
+        etiquetasElement.appendChild(etiquetaElement);
+    }
+    
+    gastoElement.appendChild(etiquetasElement);
+    
+    var contenedor= document.getElementById(idElemento);
+    contenedor.appendChild(gastoElement);
+}
+function mostrarGastosAgrupadosWeb(idElemento,id,agrup){}
 
 export{
     mostrarDatoEnId,
