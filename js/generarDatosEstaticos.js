4cd742a4e57af28f7b0d5ccf6a0a2721ff43c62e
Jose Francisco Perez Castillo <jospercas2@alu.edu.gva.es>
2023-11-27 17:15:50 +0100

    Creo gastos con la función CrearGasto

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -8,6 +8,14 @@ gesGastos.actualizarPresupuesto(1500);
 //Muestro presupuesto en el id 'presupuesto'
 gesGastosWeb.mostrarDatoEnId("presupuesto",gesGastos.mostrarPresupuesto());
 
+//Creo nuevos gastos
+let gasto1 = new gesGastos.CrearGasto ("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = new gesGastos.CrearGasto ("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = new gesGastos.CrearGasto ("Bonobús", 18.60, "2020-05-26", "transporte");
+let gasto4 = new gesGastos.CrearGasto ("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = new gesGastos.CrearGasto ("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = new gesGastos.CrearGasto ("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
 
 
 
