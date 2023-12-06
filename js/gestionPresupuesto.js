185ca6ef9e3b1d66b7a11f571c80ddc6c6c36bff
Rodrigo Esteban Gimenez <rodgim@alu.edu.gva.es>
2023-12-06 11:14:05 +0100

    implemento funcion transformarListadoEtiquetas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -160,6 +160,10 @@ function agruparGastos(periodo, etiquetas, fechaDesde, fechaHasta) {
     }, {})
 }
 
+function transformarListadoEtiquetas(etiquetas) {
+    return etiquetas.split(/[,\.;: ]+/g);
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -174,4 +178,5 @@ export   {
     calcularBalance,
     filtrarGastos,
     agruparGastos,
+    transformarListadoEtiquetas,
 }
