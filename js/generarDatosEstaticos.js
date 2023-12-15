34b254e21c8f2bf0c7e5b79a3a5e6024f2315a3e
Amaya Zárate <amazarher@alu.edu.gva.es>
2023-12-15 19:01:11 +0100

    "Generar datos estáticos"

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,47 +1,53 @@
-/*Importar los programas /js/gestionPresupuesto y js/gestionPresupuestoWeb. Puedes utilizar import * as para utilizar un nombre de módulo que agrupe las funciones exportadas por cada fichero.*/
-import * as gestionPresupuestoWeb from './js/gestionPresupuestoWeb.js';
-import * as gestionPresupuesto from './js/gestionPresupuesto.js';
+/* Importar los programas /js/gestionPresupuesto y js/gestionPresupuestoWeb. Puedes utilizar import * as para utilizar un nombre de módulo que agrupe las funciones exportadas por cada fichero.*/
+import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js';
+import * as gestionPresupuesto from './gestionPresupuesto.js';
 
-//Actualizar el presupuesto a 1500€ (función actualizarPresupuesto).
+// Actualizar el presupuesto a 1500€ (función actualizarPresupuesto).
 gestionPresupuesto.actualizarPresupuesto(1500);
 
-//Mostrar el presupuesto en el div#presupuesto (funciones mostrarPresupuesto y mostrarDatoEnId).
+// Mostrar el presupuesto en el div#presupuesto (funciones mostrarPresupuesto y mostrarDatoEnId).
+gestionPresupuestoWeb.mostrarDatoEnId("presupuesto", gestionPresupuesto.mostrarPresupuesto());
 
-/*Crear los siguientes gastos (función crearGasto):
-    ("Compra carne", 23.44, "2021-10-06", "casa", "comida")
-    ("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida")
-    ("Bonobús", 18.60, "2020-05-26", "transporte")
-    ("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina")
-    ("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros")
-    ("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros")
-*/
+// Crear los siguientes gastos (función crearGasto):
+let gastos = [
+    { descripcion: "Compra carne", valor: 23.44, fecha: "2021-10-06", etiquetas: ["casa", "comida"] },
+    { descripcion: "Compra fruta y verdura", valor: 14.25, fecha: "2021-09-06", etiquetas: ["supermercado", "comida"] },
+    { descripcion: "Bonobús", valor: 18.60, fecha: "2020-05-26", etiquetas: ["transporte"] },
+    { descripcion: "Gasolina", valor: 60.42, fecha: "2021-10-08", etiquetas: ["transporte", "gasolina"] },
+    { descripcion: "Seguro hogar", valor: 206.45, fecha: "2021-09-26", etiquetas: ["casa", "seguros"] },
+    { descripcion: "Seguro coche", valor: 195.78, fecha: "2021-10-06", etiquetas: ["transporte", "seguros"] }
+  ];
 
-//Añadir los gastos creados (función anyadirGasto).
+// Añadir los gastos creados (función anyadirGasto).
+gestionPresupuesto.anyadirGasto(gastos);
 
-//Mostrar los gastos totales en div#gastos-totales (funciones calcularTotalGastos y mostrarDatoEnId).
+// Mostrar los gastos totales en div#gastos-totales (funciones calcularTotalGastos y mostrarDatoEnId).
+gestionPresupuestoWeb.mostrarDatoEnId("gastos-totales", gestionPresupuesto.calcularTotalGastos());
 
-//Mostrar el balance total en div#balance-total (funciones calcularBalance y mostrarDatoEnId).
+// Mostrar el balance total en div#balance-total (funciones calcularBalance y mostrarDatoEnId).
+gestionPresupuestoWeb.mostrarDatoEnId("balance-total", gestionPresupuesto.calcularBalance());
 
-//Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos 
-//y mostrarGastoWeb).
+// Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos y mostrarGastoWeb).
+gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-completo", gestionPresupuesto.listarGastos());
 
-//Mostrar el listado de gastos realizados en septiembre de 2021 en div#listado-gastos-filtrado-1
-// (funciones filtrarGastos y mostrarGastoWeb).
+// Mostrar el listado de gastos realizados en septiembre de 2021 en div#listado-gastos-filtrado-1 (funciones filtrarGastos y mostrarGastoWeb).
+gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-1", gestionPresupuesto.filtrarGastos("fechaDesde: 2021-09-01, fechaHasta: 2021-09-30"));
 
-//Mostrar el listado de gastos de más de 50€ en div#listado-gastos-filtrado-2 
-//(funciones filtrarGastos y mostrarGastoWeb).
+// Mostrar el listado de gastos de más de 50€ en div#listado-gastos-filtrado-2 (funciones filtrarGastos y mostrarGastoWeb).
+gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-2", gestionPresupuesto.filtrarGastos("valorMinimo: 50"));
 
-//Mostrar el listado de gastos de más de 200€ con etiqueta seguros en div#listado-gastos-filtrado-3 
-//(funciones filtrarGastos y mostrarGastoWeb).
+// Mostrar el listado de gastos de más de 200€ con etiqueta seguros en div#listado-gastos-filtrado-3 (funciones filtrarGastos y mostrarGastoWeb).
+gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-3", gestionPresupuesto.filtrarGastos('valorMinimo: 200'));
 
-//Mostrar el listado de gastos que tengan las etiquetas comida o transporte de menos de 50€ en 
-//div#listado-gastos-filtrado-4 (funciones filtrarGastos y mostrarGastoWeb).
+// Mostrar el listado de gastos que tengan las etiquetas comida o transporte de menos de 50€ en 
+// div#listado-gastos-filtrado-4 (funciones filtrarGastos y mostrarGastoWeb).
+gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-4", gestionPresupuesto.filtrarGastos('valorMaximo: 50, etiquetasTiene: comida, transporte'));
 
-//Mostrar el total de gastos agrupados por día en div#agrupacion-dia (funciones agruparGastos
-// y mostrarGastosAgrupadosWeb).
+// Mostrar el total de gastos agrupados por día en div#agrupacion-dia (funciones agruparGastos y mostrarGastosAgrupadosWeb).
+//gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-dia", gestionPresupuesto.agruparGastos("dia"));
 
-//Mostrar el total de gastos agrupados por mes en div#agrupacion-mes (funciones agruparGastos 
-//y mostrarGastosAgrupadosWeb).
+// Mostrar el total de gastos agrupados por mes en div#agrupacion-mes (funciones agruparGastos y mostrarGastosAgrupadosWeb).
+//gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-mes", gestionPresupuesto.agruparGastos("mes"));
 
-//Mostrar el total de gastos agrupados por año en div#agrupacion-anyo (funciones agruparGastos
-//y mostrarGastosAgrupadosWeb).
+// Mostrar el total de gastos agrupados por año en div#agrupacion-anyo (funciones agruparGastos y mostrarGastosAgrupadosWeb).
+//gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-anyo", gestionPresupuesto.agruparGastos("anyo"));
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,11 +1,14 @@
+import * as gestionPresupuesto from "./gestionPresupuesto.js";
+
 //-------------------------------------------> FUNCIONES <-------------------------------------
 /*Función de dos parámetros que se encargará de escribir el valor (texto) en el elemento HTML con 
   id idElemento indicado:
     - idElemento - Hará referencia al id del elemento HTML donde se insertará el resultado en formato
       texto.
     - valor - El valor a mostrar.*/
-function mostrarDatoEnId (id, valor){
-
+function mostrarDatoEnId (idElemento, valor){
+  let elemento = document.getElementById(idElemento);
+  elemento.innerText = valor;
 }
 
 /*Función de dos parámetros que se encargará de añadir dentro del elemento HTML con id idElemento
@@ -13,7 +16,8 @@ function mostrarDatoEnId (id, valor){
     - idElemento - Hará referencia al id del elemento HTML donde se insertará el conjunto de
       estructuras HTML que se crearán para cada gasto.
     - gasto - Objeto gasto*/
-function mostrarGastoWeb (){
+function mostrarGastoWeb (idElemento, gasto){
+let elemento =document.getElementById(idElemento);
 
 }
 /*Función de tres parámetros que se encargará de crear dentro del elemento HTML con id idElemento
@@ -24,8 +28,8 @@ function mostrarGastoWeb (){
       (ejecución de la función agruparGastos desarrollada en la práctica anterior).
                           (((((agrup = {"2021-09": 5, "2021-10": 39})))))
     - periodo - Período temporal por el que se habrá realizado la agrupación (mes, dia o anyo)*/
-   function mostrarGastosAgrupadosWeb(){
-
+   function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
+    
    }
 
 export{
