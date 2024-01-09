6f59133f1c6426897dbe7638ca6661bacd13eb6f
Guille <guillermina888@gmail.com>
2024-01-09 20:14:14 +0100

    Coloco función en archivo correcto

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -320,31 +320,7 @@ function filtrarGastosWeb() {
 let botonFiltradoFormGasto = new filtrarGastosWeb();
 document.getElementById("formulario-filtrado").addEventListener("submit", botonFiltradoFormGasto);
 
-function cargarGastos(gastosAlmacenamiento) {
-    // gastosAlmacenamiento es un array de objetos "planos"
-    // No tienen acceso a los métodos creados con "CrearGasto":
-    // "anyadirEtiquetas", "actualizarValor",...
-    // Solo tienen guardadas sus propiedades: descripcion, valor, fecha y etiquetas
 
-    // Reseteamos la variable global "gastos"
-    gastos = [];
-    // Procesamos cada gasto del listado pasado a la función
-    for (let g of gastosAlmacenamiento) {
-        // Creamos un nuevo objeto mediante el constructor
-        // Este objeto tiene acceso a los métodos "anyadirEtiquetas", "actualizarValor",...
-        // Pero sus propiedades (descripcion, valor, fecha y etiquetas) están sin asignar
-        let gastoRehidratado = new CrearGasto();
-        // Copiamos los datos del objeto guardado en el almacenamiento
-        // al gasto rehidratado
-        // https://es.javascript.info/object-copy#cloning-and-merging-object-assign
-        Object.assign(gastoRehidratado, g);
-        // Ahora "gastoRehidratado" tiene las propiedades del gasto
-        // almacenado y además tiene acceso a los métodos de "CrearGasto"
-
-        // Añadimos el gasto rehidratado a "gastos"
-        gastos.push(gastoRehidratado)
-    }
-}
 
 export {
     mostrarDatoEnId,
@@ -359,5 +335,5 @@ export {
     nuevoGastoWebFormulario,
     EditarHandleFormulario,
     filtrarGastosWeb,
-    cargarGastos,
+   
 };
