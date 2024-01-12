e8b61ecc750ebaa78711f693f675f973cf01ace0
Amaya Zárate <amazarher@alu.edu.gva.es>
2024-01-12 12:07:34 +0100

    "Eventos completado sin funcionar"

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -34,12 +34,42 @@ function mostrarGastoWeb (idElemento, gasto){
     // Agrega las etiquetas del gasto si existen
     if (gasto.etiquetas && gasto.etiquetas.length > 0) {
       for (let e in gasto.etiquetas){
+        // Eventos para los span de etiquetas (no crearemos botón de borrar: se producirá si el usuario hace clic encima de una etiqueta): 
         estructuraHTML += `
           <span class="gasto-etiquetas-etiqueta">
             ${gasto.etiquetas}
           </span>`;
       }
     }
+    // Crear un nuevo objeto a partir de la función constructora BorrarEtiquetasHandle.
+    let borrarEtiquetasHandle = new BorrarEtiquetasHandle(gasto);
+    // Establecer la propiedad gasto del objeto creado al objeto gasto (recuerda que el objeto gasto es un parámetro pasado a la función mostrarGastoWeb).
+    borrarEtiquetasHandle.gasto = gasto;
+    // Establecer la propiedad etiqueta del objeto creado al texto de la etiqueta que se esté procesando (seguramente este valor lo tendrás disponible dentro del bucle que se encarga de pintar un elemento span para cada etiqueta).
+    borrarEtiquetasHandle.etiqueta = gasto.etiquetas;
+    //Añadir el objeto recién creado como objeto manejador del evento click al span de la etiqueta.
+    document.getElementById("gasto-etiquetas-etiqueta").addEventListener("click", borrarEtiquetasHandle);
+    //
+    // Crear Botones con manejador de eventos:
+    estructuraHTML += `
+    <button class="gasto-editar" type="button">Editar</button>
+    <button class="gasto-borrar" type="button">Borrar</button>`;
+    // Crear un nuevo objeto a partir de la función constructora EditarHandle.
+    let editarHandle = new EditarHandle(gasto);
+    // Establecer la propiedad gasto del objeto creado al objeto gasto (recuerda que el objeto gasto es un parámetro pasado a la función mostrarGastoWeb).
+    editarHandle.gasto = gasto;
+    // Añadir el objeto recién creado como objeto manejador del evento click al botón Editar recién creado.
+    let btnEditar = document.getElementById("gasto-editar");
+    btnEditar.addEventListener("click", editarHandle);
+    //
+    // Crear un nuevo objeto a partir de la función constructora BorrarHandle.
+    let borrarHandle = new BorrarHandle(gasto);
+    // Establecer la propiedad gasto del objeto creado al objeto gasto (recuerda que el objeto gasto es un parámetro pasado a la función mostrarGastoWeb).
+    borrarHandle.gasto = gasto;
+    // Añadir el objeto recién creado como objeto manejador del evento click al botón Borrar recién creado.
+    let btnBorrar = document.getElementById("gasto-borrar");
+    btnBorrar.addEventListener("click", BorrarHandle);
+    //
     estructuraHTML += `
         </div>
       </div>`;
@@ -89,9 +119,12 @@ function repintar(){
   mostrarDatoEnId("balance-total", gestionPresupuesto.calcularBalance());
   // Borrar el contenido de div#listado-gastos-completo, para que el paso siguiente no duplique la información.
   // Puedes utilizar innerHTML para borrar el contenido de dicha capa.
-  document.getElementById("listado-gastos.completo").innerHTML = "";
+  document.getElementById("listado-gastos-completo").innerHTML = "";
   // Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos y mostrarGastoWeb)
-  mostrarGastoWeb("listado-gastos-completo", gestionPresupuesto.listarGastos());
+  let listargasto = gestionPresupuesto.listarGastos();
+  for(let gasto of listargasto){
+    mostrarGastoWeb("listado-gastos-completo", gasto);
+  }
 }
 //
 /* Función actualizarPresupuestoWeb y botón actualizarpresupuesto. 
@@ -102,7 +135,7 @@ function actualizarPresupuestoWeb (){
   let presupuestoActualizado = prompt("Introduzca su presupuesto:");
   // Convertir el valor a número (recuerda que prompt siempre devuelve un string).
   // Actualicar el presupuesto (función actualizarPresupuesto)
-  gestionPresupuesto.actualizarPresupuesto(Number(presupuestoActualizado));
+  gestionPresupuesto.actualizarPresupuesto(parseFloat(presupuestoActualizado));
   // Llamar a la función repintar para que se muestre la información actualizada en el archivo HTML. 
   // Recuerda que actualizar el presupuesto provoca cambios en el balance, por lo que al ejecutar repintar se actualizarán ambos campos.
   repintar();
@@ -125,13 +158,13 @@ function nuevoGastoWeb (){
   let fecha = prompt("Introduzca la fecha:");
   let etiquetas = prompt("Introduzca las categorías, separadas por comas:");
   // Convertir el valor a número (recuerda que prompt siempre devuelve un string).
-  let val = Number(valor);
+  let val = parseFloat(valor);
   // Convertir la cadena de texto de etiquetas devuelta por prompt a un array.
   let etiq = [etiquetas];
   // Crear un nuevo gasto (función crearGasto). ¡Ojo con la manera de pasar el parámetro ~etiquetas~!
-  let gasto = gestionPresupuesto.CrearGasto(descripcion, val, fecha.toLocaleDateString, etiq);
+  let gasto = new gestionPresupuesto.CrearGasto(descripcion, val, fecha.toLocaleDateString, ...etiq);
   // Añadir el gasto a la lista (función anyadirGasto).
-  gestionPresupuesto.anyadirGasto(gasto);//.......................................................................!!!!!!
+  gestionPresupuesto.anyadirGasto(gasto);
   // Llamar a la función repintar para que se muestre la lista con el nuevo gasto.
   repintar();
 }
@@ -139,7 +172,74 @@ function nuevoGastoWeb (){
    Para ello habrá que obtener el elemento botón correspondiente previamente.*/
 let btnanyadirgasto = document.getElementById("anyadirgasto");
 btnanyadirgasto.addEventListener("click", nuevoGastoWeb);
-
+//
+/* Esta función se utilizará como objeto manejador de eventos para editar un gasto.
+   La función EditarHandle será una función constructora que definirá exclusivamente un método llamado handleEvent.
+   Cuando creemos un objeto basado en su prototipo, asignaremos a dicho objeto una propiedad llamada gasto, que será una
+   referencia al gasto que estemos editando.*/
+function EditarHandle (gasto){
+  this.gasto = gasto;
+  // El código de la función handleEvent podrá hacer referencia a dicho gasto a través de this.gasto, 
+  // ya que es una propiedad del objeto. Esta función realizará las siguientes tareas:
+  this.handleEvent = function () {
+    // Pedir al usuario la información necesaria para editar el gasto mediante sucesivas preguntas con prompt. 
+    // Por simplicidad, de momento no se comprobará la validez de dichos datos. La fecha vendrá dada en formato
+    // internacional (yyyy-mm-dd) y las etiquetas se introducirán en un único cuadro de texto como una lista separada
+    // por comas (por ejemplo, etiqueta1,etiqueta2,etiqueta3). Recuerda que prompt admite como segundo parámetro el valor
+    // por defecto del cuadro de diálogo, por lo que puedes proporcionar el valor actual de cada propiedad del gasto.
+    let descripcion = prompt("Introduzca la nueva descripción:", this.gasto.descripcion);
+    let valor = prompt("Introduzca el nuevo valor:", this.gasto.valor);
+    let fecha = prompt("Introduzca la nueva fecha:", this.gasto.fecha);
+    let etiquetas = prompt("Introduzca las nuevas categorías, separadas por comas:", this.gasto.etiquetas);
+    // Convertir el valor a número (recuerda que prompt siempre devuelve un string).
+    let val = parseFloat(valor);
+    // Convertir la cadena de texto de etiquetas devuelta por prompt a un array.
+    let etiq = etiquetas.split(',');
+    // Actualizar las propiedades del gasto (disponible mediante this.gasto), mediante las funciones actualizarValor,
+    // actualizarDescripcion, actualizarFecha y anyadirEtiquetas.
+    this.gasto.actualizarDescripcion(descripcion);
+    this.gasto.actualizarValor(val);
+    this.gasto.actualizarFecha(new Date(fecha));
+    this.gasto.anyadirEtiquetas(etiq);
+    // Llamar a la función repintar para que se muestre la lista de gastos con los datos actualizados de la edición.
+    repintar();
+  }
+}
+//
+/* Función BorrarHandle. Esta función se utilizará como objeto manejador de eventos para borrar un gasto.
+La función BorrarHandle será una función constructora que definirá exclusivamente un método llamado handleEvent.
+Cuando creemos un objeto basado en su prototipo, asignaremos a dicho objeto una propiedad llamada gasto,
+que será una referencia al gasto que estemos editando. El código de la función handleEvent podrá hacer referencia
+a dicho gasto a través de this.gasto, ya que es una propiedad del objeto. Esta función realizará las siguientes tareas:*/
+function BorrarHandle(gasto){
+  this.gasto = gasto;
+  // Borrar el gasto asociado. Para ello utilizará la función borrarGasto y como parámetro utilizará el id del gasto seleccionado,
+  // disponible en this.gasto.
+  this.handleEvent = function(){
+    gestionPresupuesto.borrarGasto(this.gasto.id);
+  }
+  // Llamar a la función repintar para que se muestre la lista actualizada de gastos.
+repintar();
+}
+//
+/* Función BorrarEtiquetasHandle. Esta función se utilizará como objeto manejador de eventos para borrar etiquetas de un gasto.
+El funcionamiento de esta función es muy parecido a la anterior, con la excepción de su funcionamiento interno.
+La función BorrarEtiquetasHandle será una función constructora que definirá exclusivamente un método llamado handleEvent. 
+Cuando creemos un objeto basado en su prototipo, asignaremos a dicho objeto una propiedad llamada gasto, que será una referencia
+al gasto que estemos editando y una propiedad llamada etiqueta, que hará referencia a la etiqueta que se pretenda eliminar. 
+El código de la función handleEvent podrá hacer referencia a dicho gasto a través de this.gasto y a this.etiqueta, ya que 
+son propiedades del objeto. Esta función realizará las siguientes tareas:*/
+function BorrarEtiquetasHandle (gasto, etiqueta){
+  this.gasto = gasto;
+  this.etiqueta = etiqueta;
+  // Borrar la etiqueta seleccionada del gasto asociado. Para ello utilizará la función borrarEtiquetas del gasto asociado 
+  // (this.gasto) y como parámetro utilizará la etiqueta seleccionada, disponible en this.etiqueta.
+  this.handleEvent = function(){
+    this.gasto.gestionPresupuesto.borrarEtiquetas(this.etiqueta);
+  }
+  // Llamar a la función repintar para que se muestre la lista actualizada de gastos.
+  repintar();
+}
 //
 // Exportar funciones
 export{
