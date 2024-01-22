6adc752813ccec79a8ecd0c5086d8f912e10a2b2
Diego <dievalsie@alu.edu.gva.es>
2024-01-22 16:50:56 +0100

    Comienzo localStorage. Funcion guardar gastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -254,6 +254,20 @@ function transformarListadoEtiquetas(etiquetas){
 }
 
 
+function cargarGastos(arrayGastos){
+    gastos = [];
+
+    for (let g of arrayGastos){
+
+        let gastoRehidratado = new CrearGasto();
+
+        Object.assign(gastoRehidratado, g);
+
+        gastos.push(gastoRehidratado);
+    }
+}
+
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -268,5 +282,6 @@ export   {
     calcularBalance,
     filtrarGastos,
     agruparGastos,
-    transformarListadoEtiquetas
+    transformarListadoEtiquetas,
+    cargarGastos
 }
