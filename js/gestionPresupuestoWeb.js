0e0951ac98869a31066b396cd10ed33fe2f45221
AICDAW2223 <adribacan@alu.edu.gva.es>
2024-02-15 13:40:09 +0100

    Librerias externas/mostrarGastosAgrupadosWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -512,6 +512,7 @@ function cargarGastosWeb(){
 function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo){
 
     let contenedor = document.getElementById(idElemento);
+    contenedor.innerHTML = "";
 
     let divAgrup =document.createElement("div");
     divAgrup.className="agrupacion";
@@ -548,7 +549,65 @@ function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo){
         }  
         }
         
+// Estilos
+divP.style.width = "33%";
+divP.style.display = "inline-block";
+// Crear elemento <canvas> necesario para crear la gráfica
+// https://www.chartjs.org/docs/latest/getting-started/
+let chart = document.createElement("canvas");
+// Variable para indicar a la gráfica el período temporal del eje X
+// En función de la variable "periodo" se creará la variable "unit" (anyo -> year; mes -> month; dia -> day)
+let unit = "";
+switch (periodo) {
+case "anyo":
+    unit = "year";
+    break;
+case "mes":
+    unit = "month";
+    break;
+case "dia":
+default:
+    unit = "day";
+    break;
+}
 
+// Creación de la gráfica
+// La función "Chart" está disponible porque hemos incluido las etiquetas <script> correspondientes en el fichero HTML
+const myChart = new Chart(chart.getContext("2d"), {
+    // Tipo de gráfica: barras. Puedes cambiar el tipo si quieres hacer pruebas: https://www.chartjs.org/docs/latest/charts/line.html
+    type: 'bar',
+    data: {
+        datasets: [
+            {
+                // Título de la gráfica
+                label: `Gastos por ${periodo}`,
+                // Color de fondo
+                backgroundColor: "#555555",
+                // Datos de la gráfica
+                // "agrup" contiene los datos a representar. Es uno de los parámetros de la función "mostrarGastosAgrupadosWeb".
+                data: agrup
+            }
+        ],
+    },
+    options: {
+        scales: {
+            x: {
+                // El eje X es de tipo temporal
+                type: 'time',
+                time: {
+                    // Indicamos la unidad correspondiente en función de si utilizamos días, meses o años
+                    unit: unit
+                }
+            },
+            y: {
+                // Para que el eje Y empieza en 0
+                beginAtZero: true
+            }
+        }
+    }
+});
+// Añadimos la gráfica a la capa
+contenedor.append(chart);
  return contenedor;   
 
 }
