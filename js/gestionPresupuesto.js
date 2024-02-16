2d8c1ed7ec962d27ffeffaa3dd0c2835bef2a4df
Patricia Romero Marquez <patricia.romarquez@gmail.com>
2024-02-16 14:48:25 +0100

    creo funcion transformarListadoEtiquetas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -256,6 +256,19 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas ) {
     }
 }
 
+//f(x) transformarListadoEtiquetas
+
+function transformarListadoEtiquetas(etiquetasTiene)
+{
+    let regexp = /\w+/g;
+
+    let resultado = etiquetasTiene.match(regexp);
+
+    return resultado;
+}
+
+
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -269,5 +282,6 @@ export   {
     calcularTotalGastos,
     calcularBalance,
     filtrarGastos,
-    agruparGastos
+    agruparGastos,
+    transformarListadoEtiquetas
 }
