a69453ec69eba06d6b383ea3d2137749369cc7bf
Pablo de Madaria Pascual <pablodemadaria@gmail.com>
2024-10-15 17:25:27 +0200

    Metodo actualizarValor

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -35,6 +35,10 @@ function CrearGasto(descripcion, valor) {
     this.actualizarDescripcion = function(nuevaDescripcion){
         this.descripcion = nuevaDescripcion;
     }
+
+    this.actualizarValor = function(nuevoValor){
+        (nuevoValor>0 && !nuevoValor.isNaN)?(this.valor = nuevoValor):(null);
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
