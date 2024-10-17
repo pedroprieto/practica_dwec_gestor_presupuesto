65958caf95700be9333381fff48b2d1867e11c48
Cathaysa Navarro <catnavben@alu.edu.gva.es>
2024-10-17 09:41:32 +0200

    fundamento de JS I

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,25 +1,48 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+//Cathaysa Navarro Benítez.
 
-// TODO: Variable global
+let presupuesto = 0;
 
+let dinero = prompt("Introduce la cantidad de dinero para el presupuesto");
+actualizarPresupuesto(dinero);
 
-function actualizarPresupuesto() {
-    // TODO
+mostrarPresupuesto();
+
+function actualizarPresupuesto(dinero) {
+  if (dinero >= 0) {
+    presupuesto = dinero;
+    return presupuesto;
+  } else {
+    alert("La cantidad introducida no es válida");
+    return -1;
+  }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+  console.log("Tu presupuesto actual es de " + presupuesto + " euros");
 }
 
-function CrearGasto() {
-    // TODO
-}
+function CrearGasto(descripcion, valor) {
+  this.descripcion = descripcion;
+  this.valor = typeof valor === "number" && valor >= 0 ? valor : 0;
 
+  this.mostrarGasto = function () {
+    console.log(
+      `Gasto correspondiente a ${this.descripcion} con valor de ${this.valor} euros`
+    );
+  };
+
+  this.actualizarDescripcion = function (nuevaDescripcion) {
+    this.descripcion = nuevaDescripcion;
+  };
+
+  this.actualizarValor = function (nuevoValor) {
+    if (typeof valor === "number" && valor >= 0) {
+      this.valor = nuevoValor;
+    }
+  };
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
-    mostrarPresupuesto,
-    actualizarPresupuesto,
-    CrearGasto
-}
+export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto };
