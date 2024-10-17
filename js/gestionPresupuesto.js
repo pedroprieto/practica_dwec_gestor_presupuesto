e43802b7abd861405be892c2ca51b7e9806fd10a
Cathaysa Navarro <catnavben@alu.edu.gva.es>
2024-10-17 10:51:19 +0200

    Creación listas y funciones vacías, Fundamento de JS II

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,6 +1,7 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 //Cathaysa Navarro Benítez.
-
+let gastos = [];
+let idGasto = 0;
 let presupuesto = 0;
 
 let dinero = prompt("Introduce la cantidad de dinero para el presupuesto");
@@ -42,7 +43,26 @@ function CrearGasto(descripcion, valor) {
     }
   };
 }
+
+function listarGastos() {}
+
+function anyadirGasto() {}
+
+function borrarGasto() {}
+
+function calcularTotalGastos() {}
+
+function calcularBalance() {}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto };
+export {
+  mostrarPresupuesto,
+  actualizarPresupuesto,
+  CrearGasto,
+  listarGastos,
+  anyadirGasto,
+  borrarGasto,
+  calcularTotalGastos,
+  calcularBalance,
+};
