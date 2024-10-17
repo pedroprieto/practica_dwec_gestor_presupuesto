a8f22fe39142882f426761c8711f027b9de1bfd2
Cathaysa Navarro <catnavben@alu.edu.gva.es>
2024-10-17 13:01:33 +0200

    Integración de nuevos métodos objeto Gasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -4,10 +4,22 @@ let gastos = [];
 let idGasto = 0;
 let presupuesto = 0;
 
-let dinero = prompt("Introduce la cantidad de dinero para el presupuesto");
-actualizarPresupuesto(dinero);
+//let dinero = prompt("Introduce la cantidad de dinero para el presupuesto");
+
+actualizarPresupuesto(300);
 
 mostrarPresupuesto();
+//pruebas para la función CrearGasto.
+let gasto1 = new CrearGasto("Gasto 1");
+let gasto2 = new CrearGasto("Gasto 2", 23.55);
+let gasto3 = new CrearGasto("Gasto 3", 23.55, "2021-10-06T13:10");
+let gasto4 = new CrearGasto("Gasto 4", 23.55, "2021-10-06T13:10", "casa");
+let gasto5 = new CrearGasto("Gasto 5", 23.55, "2021-10-06T13:10", "casa", "supermercado");
+let gasto6 = new CrearGasto("Gasto 6", 23.55, "2021-10-06T13:10", "casa", "supermercado", "comida");
+
+gasto6.mostrarGasto();
+
+console.log(gasto6);
 
 function actualizarPresupuesto(dinero) {
   if (dinero >= 0) {
@@ -22,14 +34,27 @@ function actualizarPresupuesto(dinero) {
 function mostrarPresupuesto() {
   console.log("Tu presupuesto actual es de " + presupuesto + " euros");
 }
-
-function CrearGasto(descripcion, valor) {
+//FUNCIÓN CREAR GASTO:
+function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
   this.descripcion = descripcion;
+  //comprobación de si el valor tiene un tipo correcto.
   this.valor = typeof valor === "number" && valor >= 0 ? valor : 0;
 
+  //verificación de la fecha
+  if (fecha && !isNaN(Date.parse(fecha))) {
+    this.fecha = Date.parse(fecha);
+  } else {
+    this.fecha = Date.now();
+  }
+  //el array de etiquetas comienza vacío
+  this.etiquetas = etiquetas.length > 0 ? etiquetas : [];
+
+  //mostrar todos los datos introducidos por pantalla
   this.mostrarGasto = function () {
     console.log(
-      `Gasto correspondiente a ${this.descripcion} con valor de ${this.valor} euros`
+      `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €, fecha: ${new Date(
+        this.fecha
+      ).toLocaleString()}, etiquetas: ${this.etiquetas.join(", ")}`
     );
   };
 
@@ -38,21 +63,61 @@ function CrearGasto(descripcion, valor) {
   };
 
   this.actualizarValor = function (nuevoValor) {
-    if (typeof valor === "number" && valor >= 0) {
+    if (typeof nuevoValor === "number" && nuevoValor >= 0) {
       this.valor = nuevoValor;
     }
   };
+
+  this.anyadirEtiquetas = function (...nuevasEtiquetas) {
+    this.etiquetas.push(...nuevasEtiquetas);
+  };
+
+  this.mostrarGastoCompleto = function () {};
 }
 
-function listarGastos() {}
+//Función listar gastos
+function listarGastos() {
+  return gastos;
+}
 
-function anyadirGasto() {}
+//Función añadir gastos
+function anyadirGasto(gasto) {
+  gasto.id = idGasto;
 
-function borrarGasto() {}
+  idGasto++;
 
-function calcularTotalGastos() {}
+  idGasto.push(gasto);
+}
 
-function calcularBalance() {}
+//Función borrar gastos.
+function borrarGasto(id) {
+  for (i = 0; i < gastos.length; i++) {
+    if (gastos[i].id === id) {
+      gastos.splice(i, 1); //Elimina el gasto de la posición i
+
+      console.log(`Gasto con ID ${id} ha sido eliminado`);
+      return;
+    }
+  }
+  console.log(`El gasto con el ID ${id} no ha sido encontrado`); //comprobar si funciona.
+}
+
+//función Calcular el total de los gastos
+function calcularTotalGastos() {
+  let total = 0;
+  for (i = 0; i < gastos.length; i++) {
+    total += gastos[i].valor;
+  }
+  return total;
+}
+
+//Función calcular balance presupuestario
+function calcularBalance() {
+  let computo = calcularTotalGastos() - presupuesto;
+
+  console.log(`El balance es de ${computo} euros`);
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
