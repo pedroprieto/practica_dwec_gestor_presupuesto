50b142240a62a0c4ded24e3fcdc94fccef452a4f
Arias93 <blanca.daw93@gmail.com>
2024-10-18 20:40:55 +0200

    Código parte Fundamentos de JavaScript I - BLANCA ARIAS

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,49 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+let presupuesto = 0;
 
-// TODO: Variable global
-
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if (valor >= 0){
+        presupuesto=valor;
+        return presupuesto;
+    }
+    else{ 
+        console.log("Has introducido un valor negativo");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    this.descripcion=descripcion;
+
+    if (valor>0){
+        this.valor=valor;
+    }
+    else{
+        this.valor=0;
+    }
+
+    //metodos:
+
+    this.mostrarGasto = function(){
+        return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
+    }
+
+    this.actualizarDescripcion = function (descripcion){
+        this.descripcion=descripcion;
+    }
+
+    this.actualizarValor = function (nuevoValor){
+        if (nuevoValor>=0)
+        {
+            this.valor=nuevoValor;
+        }
+        else{
+            console.log("Has introducido un valor negativo");
+        }
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
